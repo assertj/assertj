0868928face3021e8ab4fa7933fac35411e96d13
Alex Ruiz <alex.ruiz.05@gmail.com>
2010-09-26 20:45:58 -0700

    Added more tests.

---
+++ src/test/java/org/fest/assertions/api/CollectionAssert_as_with_description_Test.java
@@ -0,0 +1,58 @@
+/*
+ * Created on Sep 26, 2010
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright @2010 the original author or authors.
+ */
+package org.fest.assertions.api;
+
+import static java.util.Collections.emptyList;
+import static junit.framework.Assert.*;
+import static org.fest.assertions.test.ExpectedException.none;
+import static org.fest.assertions.test.FailureMessages.descriptionIsNull;
+
+import org.fest.assertions.description.*;
+import org.fest.assertions.test.ExpectedException;
+import org.junit.*;
+
+/**
+ * Tests for <code>{@link CollectionAssert#describedAs(Description)}</code>
+ *
+ * @author Yvonne Wang
+ * @author Alex Ruiz
+ */
+public class CollectionAssert_as_with_description_Test {
+
+  @Rule public ExpectedException thrown = none();
+
+  private CollectionAssert assertions;
+  private Description d;
+
+  @Before public void setUp() {
+    assertions = new CollectionAssert(emptyList());
+    d = new TextDescription("who's the more foolish: the fool, or the fool who follows him?");
+  }
+
+  @Test public void should_set_description() {
+    assertions.describedAs(d);
+    assertEquals(d.value(), assertions.descriptionText());
+  }
+
+  @Test public void should_return_this() {
+    CollectionAssert a = assertions.as(d);
+    assertSame(assertions, a);
+  }
+
+  @Test public void should_throw_error_if_description_is_null() {
+    thrown.expectNullPointerException(descriptionIsNull());
+    assertions.as((Description) null);
+  }
+}
