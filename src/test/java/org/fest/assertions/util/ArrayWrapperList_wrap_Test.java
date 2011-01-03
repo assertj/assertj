ae24a2e72ed8eee63631ac29925aa6c8b6c50e99
Alex Ruiz <alex.ruiz.05@gmail.com>
2011-01-03 06:08:19 -0800

    Updated copyright year.

--- src/test/java/org/fest/assertions/util/ArrayWrapperList_wrap_Test.java
+++ src/test/java/org/fest/assertions/util/ArrayWrapperList_wrap_Test.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * Copyright @2010 the original author or authors.
+ * Copyright @2010-2011 the original author or authors.
  */
 package org.fest.assertions.util;
 
