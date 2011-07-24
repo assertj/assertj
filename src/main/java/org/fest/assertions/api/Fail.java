31abbdb563dc6656c5e2fa601a1f6e5e715cc5e8
Joel Costigliola <joel.costigliola@gmail.com>
2011-07-24 18:34:14 +0200

    fixes FEST-387 fail() overloaded version when expected exception didn't occur

--- src/main/java/org/fest/assertions/api/Fail.java
+++ src/main/java/org/fest/assertions/api/Fail.java
@@ -1,24 +1,22 @@
 /*
  * Created on Mar 19, 2007
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- *
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
  * Copyright @2007-2011 the original author or authors.
  */
 package org.fest.assertions.api;
 
-
 /**
  * Common failures.
- *
+ * 
  * @author Alex Ruiz
  * @author Yvonne Wang
  * @author Joel Costigliola
@@ -28,16 +26,15 @@ public final class Fail {
   /**
    * Fails with the given message.
    * @param failureMessage error message.
-   * @return the thrown {@code AssertionError} .
+   * @return the thrown {@code AssertionError}.
    * @throws AssertionError with the given message.
    */
   public static void fail(String failureMessage) {
     throw new AssertionError(failureMessage);
   }
-  
+
   /**
-   * Throws an <code>{@link AssertionError}</code> with the given message and with the <code>{@link Throwable}</code>
-   * that caused the failure.
+   * Throws an {@link AssertionError} with the given message and with the {@link Throwable} that caused the failure.
    * @param failureMessage the description of the failed assertion. It can be {@code null}.
    * @param realCause cause of the error.
    */
@@ -45,6 +42,15 @@ public final class Fail {
     AssertionError error = new AssertionError(failureMessage);
     error.initCause(realCause);
     throw error;
+  }
+
+  /**
+   * Throws an {@link AssertionError} with a message explaining that an expection of type exceptionClass was expected to be thrown.
+   * @param exceptionClass the class exception that was expected to be thrown.
+   */
+  public static void failBecauseExceptionWasNotThrown(Class<? extends Exception> exceptionClass) {
+    String message = String.format("Expected %s to be thrown", exceptionClass.getSimpleName());
+    throw new AssertionError(message);
   }
 
   /**
