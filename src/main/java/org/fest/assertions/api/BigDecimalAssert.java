d45f007b7c8ded0f7e6b222cf4fb5328bdc1e105
Joel Costigliola <joel.costigliola@gmail.com>
2012-05-14 23:17:45 +0200

    github#16 : BigDecimalAssert : add isEqualTo(String) and isEqualByComparingTo(String)

--- src/main/java/org/fest/assertions/api/BigDecimalAssert.java
+++ src/main/java/org/fest/assertions/api/BigDecimalAssert.java
@@ -1,15 +1,15 @@
 /*
  * Created on Feb 8, 2011
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
+ * 
  * Copyright @2011 the original author or authors.
  */
 package org.fest.assertions.api;
@@ -19,14 +19,15 @@ import java.util.Comparator;
 
 import org.fest.assertions.core.NumberAssert;
 import org.fest.assertions.internal.BigDecimals;
-import org.fest.util.*;
+import org.fest.util.ComparatorBasedComparisonStrategy;
+import org.fest.util.VisibleForTesting;
 
 /**
  * Assertion methods for <code>{@link BigDecimal}</code>s.
  * <p>
  * To create an instance of this class, invoke <code>{@link Assertions#assertThat(BigDecimal)}</code>.
  * </p>
- *
+ * 
  * @author David DIDIER
  * @author Ted M. Young
  * @author Yvonne Wang
@@ -36,7 +37,8 @@ import org.fest.util.*;
 public class BigDecimalAssert extends AbstractUnevenComparableAssert<BigDecimalAssert, BigDecimal> implements
     NumberAssert<BigDecimal> {
 
-  @VisibleForTesting BigDecimals bigDecimals = BigDecimals.instance();
+  @VisibleForTesting
+  BigDecimals bigDecimals = BigDecimals.instance();
 
   protected BigDecimalAssert(BigDecimal actual) {
     super(actual, BigDecimalAssert.class);
@@ -66,13 +68,29 @@ public class BigDecimalAssert extends AbstractUnevenComparableAssert<BigDecimalA
     return myself;
   }
 
+  /**
+   * Same as {@link AbstractAssert#isEqualTo(Object) isEqualTo(BigDecimal)} but takes care of converting given String to
+   * {@link BigDecimal} for you.
+   */
+  public BigDecimalAssert isEqualTo(String expected) {
+    return super.isEqualTo(new BigDecimal(expected));
+  }
+
+  /**
+   * Same as {@link AbstractUnevenComparableAssert#isEqualByComparingTo(Comparable) isEqualByComparingTo(BigDecimal)}
+   * but takes care of converting given String to {@link BigDecimal} for you.
+   */
+  public BigDecimalAssert isEqualByComparingTo(String expected) {
+    return super.isEqualByComparingTo(new BigDecimal(expected));
+  }
+
   @Override
   public BigDecimalAssert usingComparator(Comparator<?> customComparator) {
     super.usingComparator(customComparator);
     this.bigDecimals = new BigDecimals(new ComparatorBasedComparisonStrategy(customComparator));
     return myself;
   }
-  
+
   @Override
   public BigDecimalAssert usingDefaultComparator() {
     super.usingDefaultComparator();
