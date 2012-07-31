79d575848a833ffc08825ae243b58e9eaf0f3930
olim7t <omichallat+github@gmail.com>
2012-07-31 19:29:19 +0200

    Migrate ObjectAssert tests.

---
+++ src/test/java/org/fest/assertions/api/object/ObjectAssert_isLenientEqualsToByIgnoringFields_Test.java
@@ -0,0 +1,43 @@
+/*
+ * 
+ * Created on Apr 9, 2012
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * 
+ * Copyright @2012 the original author or authors.
+ */
+package org.fest.assertions.api.object;
+
+import static org.mockito.Mockito.verify;
+
+import org.fest.assertions.api.ObjectAssert;
+import org.fest.assertions.api.ObjectAssertTest;
+import org.fest.assertions.test.Jedi;
+
+/**
+ * Tests for <code>{@link ObjectAssert#isLenientEqualsToByIgnoringFields(Object, String...)}</code>.
+ * 
+ * @author Nicolas François
+ * @author Mikhail Mazursky
+ */
+public class ObjectAssert_isLenientEqualsToByIgnoringFields_Test extends ObjectAssertTest {
+
+  private Jedi other = new Jedi("Yoda", "Blue");
+
+  @Override
+  protected ObjectAssert<Jedi> invoke_api_method() {
+    return assertions.isLenientEqualsToByIgnoringFields(other, "lightSaberColor");
+  }
+
+  @Override
+  protected void verify_internal_effects() {
+    verify(objects).assertIsLenientEqualsToByIgnoringFields(getInfo(assertions), getActual(assertions), other, "lightSaberColor");
+  }
+}
