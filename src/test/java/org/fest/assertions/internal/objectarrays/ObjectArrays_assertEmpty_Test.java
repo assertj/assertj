6cfc9e4f9a34978ecff7476c3c37dfbc2f8020a4
Joel Costigliola <joel.costigliola@gmail.com>
2012-08-05 18:30:48 +0200

    github #86 tests : org.fest.assertions.internal contain too much classes moving ObjectArrays tests to org.fest.assertions.internal.objectarrays

---
+++ src/test/java/org/fest/assertions/internal/objectarrays/ObjectArrays_assertEmpty_Test.java
@@ -0,0 +1,62 @@
+/*
+ * Created on Nov 25, 2010
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * 
+ * Copyright @2010-2011 the original author or authors.
+ */
+package org.fest.assertions.internal.objectarrays;
+
+import static org.fest.assertions.error.ShouldBeEmpty.shouldBeEmpty;
+import static org.fest.assertions.test.FailureMessages.actualIsNull;
+import static org.fest.assertions.test.ObjectArrayFactory.emptyArray;
+import static org.fest.assertions.test.TestData.someInfo;
+import static org.fest.assertions.test.TestFailures.failBecauseExpectedAssertionErrorWasNotThrown;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+
+import org.fest.assertions.core.AssertionInfo;
+import org.fest.assertions.internal.ObjectArrays;
+import org.fest.assertions.internal.ObjectArraysBaseTest;
+
+/**
+ * Tests for <code>{@link ObjectArrays#assertEmpty(AssertionInfo, Object[])}</code>.
+ * 
+ * @author Alex Ruiz
+ * @author Joel Costigliola
+ */
+public class ObjectArrays_assertEmpty_Test extends ObjectArraysBaseTest {
+
+  @Test
+  public void should_fail_if_actual_is_null() {
+    thrown.expectAssertionError(actualIsNull());
+    arrays.assertEmpty(someInfo(), null);
+  }
+
+  @Test
+  public void should_fail_if_actual_is_not_empty() {
+    AssertionInfo info = someInfo();
+    Character[] actual = { 'a', 'b' };
+    try {
+      arrays.assertEmpty(info, actual);
+    } catch (AssertionError e) {
+      verify(failures).failure(info, shouldBeEmpty(actual));
+      return;
+    }
+    failBecauseExpectedAssertionErrorWasNotThrown();
+  }
+
+  @Test
+  public void should_pass_if_actual_is_empty() {
+    arrays.assertEmpty(someInfo(), emptyArray());
+  }
+}
---
+++ src/test/java/org/fest/assertions/internal/objectarrays/ObjectArrays_assertHasSameSizeAs_with_Iterable_Test.java
@@ -0,0 +1,74 @@
+/*
+ * Created on Apr 27, 2012
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ * 
+ * Copyright @2010-2012 the original author or authors.
+ */
+package org.fest.assertions.internal.objectarrays;
+
+import static org.fest.assertions.error.ShouldHaveSameSizeAs.shouldHaveSameSizeAs;
+import static org.fest.assertions.test.FailureMessages.actualIsNull;
+import static org.fest.assertions.test.TestData.someInfo;
+import static org.fest.assertions.test.TestFailures.failBecauseExpectedAssertionErrorWasNotThrown;
+import static org.fest.util.Arrays.array;
+import static org.fest.util.Collections.list;
+
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import org.fest.assertions.core.AssertionInfo;
+import org.fest.assertions.internal.ObjectArrays;
+import org.fest.assertions.internal.ObjectArraysBaseTest;
+
+/**
+ * Tests for <code>{@link ObjectArrays#assertHasSameSizeAs(AssertionInfo, Object[], Iterable)}</code>.
+ * 
+ * @author Nicolas François
+ * @author Joel Costigliola
+ */
+public class ObjectArrays_assertHasSameSizeAs_with_Iterable_Test extends ObjectArraysBaseTest {
+
+  @Test
+  public void should_fail_if_actual_is_null() {
+    thrown.expectAssertionError(actualIsNull());
+    arrays.assertHasSameSizeAs(someInfo(), null, list("Solo", "Leia"));
+  }
+
+  @Test
+  public void should_fail_if_other_is_null() {
+    thrown.expectNullPointerException("The iterable to look for should not be null");
+    String[] actual = array("Solo", "Leia");
+    Iterable<?> other = null;
+    arrays.assertHasSameSizeAs(someInfo(), actual, other);
+  }
+
+  @Test
+  public void should_fail_if_actual_size_is_not_equal_to_other_size() {
+    AssertionInfo info = someInfo();
+    String[] actual = array("Yoda");
+    List<String> other = list("Solo", "Leia");
+    try {
+      arrays.assertHasSameSizeAs(info, actual, other);
+    } catch (AssertionError e) {
+      verify(failures).failure(info, shouldHaveSameSizeAs(actual, actual.length, other.size()));
+      return;
+    }
+    failBecauseExpectedAssertionErrorWasNotThrown();
+  }
+
+  @Test
+  public void should_pass_if_actual_has_same_size_as_other() {
+    arrays.assertHasSameSizeAs(someInfo(), array("Solo", "Leia"), list("Solo", "Leia"));
+  }
+}
