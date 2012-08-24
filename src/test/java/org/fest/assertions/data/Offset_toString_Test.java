213582665f666640bad28b2deeefc20acc6de0f9
Alex Ruiz <alruiz@google.com>
2012-08-23 23:41:05 -0700

    CLOSED - task 103: Remove dependencies on AWT https://github.com/alexruiz/fest-assert-2.x/issues/issue/103

--- src/test/java/org/fest/assertions/data/Offset_toString_Test.java
+++ src/test/java/org/fest/assertions/data/Offset_toString_Test.java
@@ -1,16 +1,16 @@
 /*
  * Created on Oct 30, 2010
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- * 
- * Copyright @2010-2011 the original author or authors.
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright @2010-2012 the original author or authors.
  */
 package org.fest.assertions.data;
 
@@ -20,12 +20,11 @@ import static org.fest.assertions.data.Offset.offset;
 import org.junit.*;
 
 /**
- * Tests for <code>{@link Offset#toString()}</code>.
- * 
+ * Tests for {@link Offset#toString()}.
+ *
  * @author Alex Ruiz
  */
 public class Offset_toString_Test {
-
   private static Offset<Integer> offset;
 
   @BeforeClass
