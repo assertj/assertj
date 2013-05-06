26a85ae791acccc272e22a75809f4e28b1c61548
William Delanoue <william.delanoue@deveryware.net>
2013-05-06 23:10:36 +0200

    move functions to Comparables

--- src/main/java/org/assertj/core/internal/Numbers.java
+++ src/main/java/org/assertj/core/internal/Numbers.java
@@ -1,20 +1,18 @@
 /*
  * Created on Oct 20, 2010
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  * 
  * Copyright @2010-2011 the original author or authors.
  */
 package org.assertj.core.internal;
-
-import static org.assertj.core.error.ShouldBeBetween.shouldBeBetween;
 
 import org.assertj.core.api.AssertionInfo;
 
@@ -39,6 +37,7 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
   /**
    * Asserts that the actual value is equal to zero.<br>
    * It does not rely on the custom comparisonStrategy (if one is set).
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @throws AssertionError if the actual value is {@code null}.
@@ -51,6 +50,7 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
   /**
    * Asserts that the actual value is not equal to zero.<br>
    * It does not rely on the custom comparisonStrategy (if one is set).
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @throws AssertionError if the actual value is {@code null}.
@@ -62,6 +62,7 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
 
   /**
    * Asserts that the actual value is negative.
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @throws AssertionError if the actual value is {@code null}.
@@ -73,6 +74,7 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
 
   /**
    * Asserts that the actual value is positive.
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @throws AssertionError if the actual value is {@code null}.
@@ -84,6 +86,7 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
 
   /**
    * Asserts that the actual value is not negative.
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @throws AssertionError if the actual value is {@code null}.
@@ -95,6 +98,7 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
 
   /**
    * Asserts that the actual value is not positive.
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @throws AssertionError if the actual value is {@code null}.
@@ -104,9 +108,9 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
     assertLessThanOrEqualTo(info, actual, zero());
   }
 
-
   /**
    * Asserts that the actual value is in [start, end] range (start included, end included).
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @throws AssertionError if the actual value is {@code null}.
@@ -116,11 +120,12 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
    * @throws AssertionError if the actual value is not in [start, end] range.
    */
   public void assertIsBetween(AssertionInfo info, NUMBER actual, NUMBER start, NUMBER end) {
-	  assertIsBetween(info, actual, start, end, true, true);
+    assertIsBetween(info, actual, start, end, true, true);
   }
-  
+
   /**
    * Asserts that the actual value is in ]start, end[ range (start excluded, end excluded).
+   * 
    * @param info contains information about the assertion.
    * @param actual the actual value.
    * @param start the start value (exclusive), expected not to be null.
@@ -131,36 +136,6 @@ public abstract class Numbers<NUMBER extends Comparable<NUMBER>> extends Compara
    * @throws AssertionError if the actual value is not in ]start, end[ range.
    */
   public void assertIsStrictlyBetween(AssertionInfo info, NUMBER actual, NUMBER start, NUMBER end) {
-	  assertIsBetween(info, actual, start, end, false, false);
-  }
-
-  private void assertIsBetween(AssertionInfo info, NUMBER actual, NUMBER start, NUMBER end, boolean inclusiveStart, boolean inclusiveEnd) {
-    assertNotNull(info, actual);
-    startParameterIsNotNull(start);
-    endParameterIsNotNull(end);
-    boolean checkLowerBoundaryRange = inclusiveStart ? comparisonStrategy.isLessThanOrEqualTo(start, actual) : comparisonStrategy.isLessThan(start, actual);
-    boolean checkUpperBoundaryRange = inclusiveEnd ? comparisonStrategy.isLessThanOrEqualTo(actual, end) : comparisonStrategy.isLessThan(actual, end);
-    if (checkLowerBoundaryRange && checkUpperBoundaryRange) return;
-    throw failures.failure(info, shouldBeBetween(actual, start, end, inclusiveStart, inclusiveEnd, comparisonStrategy));
-  }
-  
-  /**
-   * used to check that the start of range to compare actual number to is not null, in that case throws a
-   * {@link NullPointerException} with an explicit message
-   * @param start the start number to check
-   * @throws a {@link NullPointerException} with an explicit message if the given start value is null
-   */
-  private static void startParameterIsNotNull(Object start) {
-    if (start == null) throw new NullPointerException("The start range to compare actual with should not be null");
-  }
-
-  /**
-   * used to check that the end of range to compare actual number to is not null, in that case throws a
-   * {@link NullPointerException} with an explicit message
-   * @param end the end number to check
-   * @throws a {@link NullPointerException} with an explicit message if the given end value is null
-   */
-  private static void endParameterIsNotNull(Object end) {
-    if (end == null) throw new NullPointerException("The end range to compare actual with should not be null");
+    assertIsBetween(info, actual, start, end, false, false);
   }
 }
