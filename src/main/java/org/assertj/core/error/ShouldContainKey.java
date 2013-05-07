29971ed6e213ee58f37189a1d889eaee5d6380dc
William Delanoue <william.delanoue@deveryware.net>
2013-05-07 16:32:55 +0200

    add containsKeys to MapAssert

--- src/main/java/org/assertj/core/error/ShouldContainKey.java
+++ src/main/java/org/assertj/core/error/ShouldContainKey.java
@@ -1,18 +1,22 @@
 /*
  * Created on Jun 3, 2012
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  * 
  * Copyright @2010-2012 the original author or authors.
  */
 package org.assertj.core.error;
+
+import java.util.Set;
+
+import org.assertj.core.util.Strings;
 
 /**
  * Creates an error message indicating that an assertion that verifies a map contains a key..
@@ -23,14 +27,16 @@ public class ShouldContainKey extends BasicErrorMessageFactory {
 
   /**
    * Creates a new </code>{@link ShouldContainKey}</code>.
+   * 
    * @param actual the actual value in the failed assertion.
    * @return the created {@code ErrorMessageFactory}.
    */
-  public static ErrorMessageFactory shouldContainKey(Object actual, Object key) {
+  public static <K> ErrorMessageFactory shouldContainKey(Object actual, Set<K> key) {
     return new ShouldContainKey(actual, key);
   }
 
-  private ShouldContainKey(Object actual, Object key) {
-    super("\nExpecting:\n <%s>\nto contain key:\n <%s>", actual, key);
+  private <K> ShouldContainKey(Object actual, Set<K> key) {
+    // new StringBuilder to avoid "ToString.quote" in message formatting.. better solution ?
+    super("\nExpecting:\n <%s>\nto contain key%s:\n <%s>", actual, new StringBuilder(key.size() == 1 ? "": "s"), new StringBuilder(Strings.join(key).with(", ", "'")));
   }
 }
--- src/test/java/org/assertj/core/util/Strings_join_Test.java
+++ src/test/java/org/assertj/core/util/Strings_join_Test.java
@@ -1,14 +1,14 @@
 /*
  * Created on Sep 22, 2006
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  * 
  * Copyright @2006-2011 the original author or authors.
  */
@@ -16,6 +16,7 @@ package org.assertj.core.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.rules.ExpectedException.none;
+import static org.assertj.core.util.Lists.newArrayList;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -45,5 +46,15 @@ public class Strings_join_Test {
   @Test
   public void should_join_using_delimeter() {
     assertEquals("Luke|Leia|Han", Strings.join("Luke", "Leia", "Han").with("|"));
+  }
+
+  @Test
+  public void should_join_using_delimeter_and_escape() {
+    assertEquals("'Luke'|'Leia'|'Han'", Strings.join("Luke", "Leia", "Han").with("|", "'"));
+  }
+
+  @Test
+  public void should_join_using_iterable_delimeter_and_escape() {
+    assertEquals("'Luke'|'Leia'|'Han'", Strings.join(newArrayList("Luke", "Leia", "Han")).with("|", "'"));
   }
 }
