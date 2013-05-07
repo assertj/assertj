79315cb061f307441683f3f96e94498c2054b890
Mikhail Mazursky <ash2kk@gmail.com>
2013-05-07 18:56:18 +0600

    Fix more findbugs warnings

--- src/test/java/org/assertj/core/api/objectarray/ObjectArrayAssert_doNothave_Test.java
+++ src/test/java/org/assertj/core/api/objectarray/ObjectArrayAssert_doNothave_Test.java
@@ -31,7 +31,7 @@ import org.junit.Before;
  */
 public class ObjectArrayAssert_doNothave_Test extends ObjectArrayAssertBaseTest {
 
-  private static Condition<Object> condition;
+  private Condition<Object> condition;
 
   @Before
   public void before() {
