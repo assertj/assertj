088ca3812ad844b1bb3b599b06227c026f35015d
William Delanoue <william.delanoue@deveryware.net>
2013-06-02 23:11:38 +0200

    copyright

--- src/main/java/org/assertj/core/groups/FieldsOrPropertiesExtractor.java
+++ src/main/java/org/assertj/core/groups/FieldsOrPropertiesExtractor.java
@@ -1,3 +1,17 @@
+/*
+ * Created on Jan 28, 2011
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this Throwable except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ *
+ * Copyright @2011 the original author or authors.
+ */
 package org.assertj.core.groups;
 
 import static java.lang.String.format;
@@ -85,7 +99,7 @@ public class FieldsOrPropertiesExtractor {
       throw new IllegalArgumentException("The names of the fields/properties to read should not be empty");
     if (objects == null)
       throw new IllegalArgumentException("The objects to extract fields/properties from should not be null");
-    // convert objects to a list to ensure consistent iteration order in extracteds fields/properties
+    // convert objects to a list to ensure consistent iteration order in extracted fields/properties
     List<Object> objectsAsList = newArrayList(objects);
     List<Tuple> extractedTuples = buildTuples(objectsAsList.size());
     for (String fieldOrPropertyName : fieldsOrPropertiesNames) {
