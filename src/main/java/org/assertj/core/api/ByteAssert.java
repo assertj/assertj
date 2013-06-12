17f6564a78eb0ceb0605937c2c3277fccbffcc61
Mikhail Mazursky <ash2kk@gmail.com>
2013-06-12 17:11:03 +0600

    Make ByteAssert extendable

--- src/main/java/org/assertj/core/api/ByteAssert.java
+++ src/main/java/org/assertj/core/api/ByteAssert.java
@@ -14,14 +14,8 @@
  */
 package org.assertj.core.api;
 
-import java.util.Comparator;
-
-import org.assertj.core.internal.*;
-import org.assertj.core.util.VisibleForTesting;
-
-
 /**
- * Assertion methods for bytes.
+ * Assertion methods for {@link Byte}s.
  * <p>
  * To create an instance of this class, invoke <code>{@link Assertions#assertThat(Byte)}</code> or
  * <code>{@link Assertions#assertThat(byte)}</code>.
@@ -34,154 +28,9 @@ import org.assertj.core.util.VisibleForTesting;
  * @author Mikhail Mazursky
  * @author Nicolas François
  */
-public class ByteAssert extends AbstractComparableAssert<ByteAssert, Byte> implements NumberAssert<ByteAssert, Byte> {
-
-  @VisibleForTesting
-  Bytes bytes = Bytes.instance();
+public class ByteAssert extends AbstractByteAssert<ByteAssert> {
 
   protected ByteAssert(Byte actual) {
     super(actual, ByteAssert.class);
-  }
-
-  /**
-   * Verifies that the actual value is equal to the given one.
-   * @param expected the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is not equal to the given one.
-   */
-  public ByteAssert isEqualTo(byte expected) {
-    bytes.assertEqual(info, actual, expected);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is not equal to the given one.
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is equal to the given one.
-   */
-  public ByteAssert isNotEqualTo(byte other) {
-    bytes.assertNotEqual(info, actual, other);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isZero() {
-    bytes.assertIsZero(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isNotZero() {
-    bytes.assertIsNotZero(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isPositive() {
-    bytes.assertIsPositive(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isNegative() {
-    bytes.assertIsNegative(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isNotNegative() {
-    bytes.assertIsNotNegative(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isNotPositive() {
-    bytes.assertIsNotPositive(info, actual);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is less than the given one.
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is equal to or greater than the given one.
-   */
-  public ByteAssert isLessThan(byte other) {
-    bytes.assertLessThan(info, actual, other);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is less than or equal to the given one.
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is greater than the given one.
-   */
-  public ByteAssert isLessThanOrEqualTo(byte other) {
-    bytes.assertLessThanOrEqualTo(info, actual, other);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is greater than the given one.
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is equal to or less than the given one.
-   */
-  public ByteAssert isGreaterThan(byte other) {
-    bytes.assertGreaterThan(info, actual, other);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is greater than or equal to the given one.
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is less than the given one.
-   */
-  public ByteAssert isGreaterThanOrEqualTo(byte other) {
-    bytes.assertGreaterThanOrEqualTo(info, actual, other);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isBetween(Byte start, Byte end) {
-	bytes.assertIsBetween(info, actual, start, end);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ByteAssert isStrictlyBetween(Byte start, Byte end) {
-	bytes.assertIsStrictlyBetween(info, actual, start, end);
-    return this;
-  }
-
-  @Override
-  public ByteAssert usingComparator(Comparator<? super Byte> customComparator) {
-    super.usingComparator(customComparator);
-    this.bytes = new Bytes(new ComparatorBasedComparisonStrategy(customComparator));
-    return myself;
-  }
-
-  @Override
-  public ByteAssert usingDefaultComparator() {
-    super.usingDefaultComparator();
-    this.bytes = Bytes.instance();
-    return myself;
   }
 }
