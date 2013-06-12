056dddb9243b00e54c0d6dadea34cfc7965a724f
Mikhail Mazursky <ash2kk@gmail.com>
2013-06-12 20:47:29 +0600

    Make ShortAssert extendable

--- src/main/java/org/assertj/core/api/ShortAssert.java
+++ src/main/java/org/assertj/core/api/ShortAssert.java
@@ -14,13 +14,8 @@
  */
 package org.assertj.core.api;
 
-import java.util.Comparator;
-
-import org.assertj.core.internal.*;
-import org.assertj.core.util.VisibleForTesting;
-
 /**
- * Assertion methods for shorts.
+ * Assertion methods for {@link Short}s.
  * <p>
  * To create an instance of this class, invoke <code>{@link Assertions#assertThat(Short)}</code> or
  * <code>{@link Assertions#assertThat(short)}</code>.
@@ -33,160 +28,9 @@ import org.assertj.core.util.VisibleForTesting;
  * @author Mikhail Mazursky
  * @author Nicolas François
  */
-public class ShortAssert extends AbstractComparableAssert<ShortAssert, Short> implements NumberAssert<ShortAssert, Short> {
-
-  @VisibleForTesting
-  Shorts shorts = Shorts.instance();
+public class ShortAssert extends AbstractShortAssert<ShortAssert> {
 
   protected ShortAssert(Short actual) {
     super(actual, ShortAssert.class);
-  }
-
-  /**
-   * Verifies that the actual value is equal to the given one.
-   * 
-   * @param expected the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is not equal to the given one.
-   */
-  public ShortAssert isEqualTo(short expected) {
-    shorts.assertEqual(info, actual, expected);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is not equal to the given one.
-   * 
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is equal to the given one.
-   */
-  public ShortAssert isNotEqualTo(short other) {
-    shorts.assertNotEqual(info, actual, other);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isZero() {
-    shorts.assertIsZero(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isNotZero() {
-    shorts.assertIsNotZero(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isPositive() {
-    shorts.assertIsPositive(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isNegative() {
-    shorts.assertIsNegative(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isNotNegative() {
-    shorts.assertIsNotNegative(info, actual);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isNotPositive() {
-    shorts.assertIsNotPositive(info, actual);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is less than the given one.
-   * 
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is equal to or greater than the given one.
-   */
-  public ShortAssert isLessThan(short other) {
-    shorts.assertLessThan(info, actual, other);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is less than or equal to the given one.
-   * 
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is greater than the given one.
-   */
-  public ShortAssert isLessThanOrEqualTo(short other) {
-    shorts.assertLessThanOrEqualTo(info, actual, other);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is greater than the given one.
-   * 
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is equal to or less than the given one.
-   */
-  public ShortAssert isGreaterThan(short other) {
-    shorts.assertGreaterThan(info, actual, other);
-    return this;
-  }
-
-  /**
-   * Verifies that the actual value is greater than or equal to the given one.
-   * 
-   * @param other the given value to compare the actual value to.
-   * @return {@code this} assertion object.
-   * @throws AssertionError if the actual value is {@code null}.
-   * @throws AssertionError if the actual value is less than the given one.
-   */
-  public ShortAssert isGreaterThanOrEqualTo(short other) {
-    shorts.assertGreaterThanOrEqualTo(info, actual, other);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isBetween(Short start, Short end) {
-    shorts.assertIsBetween(info, actual, start, end);
-    return this;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public ShortAssert isStrictlyBetween(Short start, Short end) {
-    shorts.assertIsStrictlyBetween(info, actual, start, end);
-    return this;
-  }
-
-  @Override
-  public ShortAssert usingComparator(Comparator<? super Short> customComparator) {
-    super.usingComparator(customComparator);
-    this.shorts = new Shorts(new ComparatorBasedComparisonStrategy(customComparator));
-    return myself;
-  }
-
-  @Override
-  public ShortAssert usingDefaultComparator() {
-    super.usingDefaultComparator();
-    this.shorts = Shorts.instance();
-    return myself;
   }
 }
