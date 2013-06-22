5f84a8ea45fc6ecbe71ec02e12925b5b9c287a39
Joel Costigliola <joel.costigliola@gmail.com>
2013-06-22 17:53:42 +0200

    #9 : Ease extending char[] assertions with AbstractCharArrayAssert

--- src/main/java/org/assertj/core/api/AbstractCharArrayAssert.java
+++ src/main/java/org/assertj/core/api/AbstractCharArrayAssert.java
@@ -4,18 +4,18 @@ import java.util.Comparator;
 
 import org.assertj.core.data.Index;
 import org.assertj.core.internal.ComparatorBasedComparisonStrategy;
-import org.assertj.core.internal.LongArrays;
+import org.assertj.core.internal.CharArrays;
 import org.assertj.core.util.VisibleForTesting;
 
-public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<S>>
-    extends AbstractAssert<S, long[]>
-    implements EnumerableAssert<AbstractLongArrayAssert<S>, Long>,
-    ArraySortedAssert<AbstractLongArrayAssert<S>, Long> {
+public abstract class AbstractCharArrayAssert<S extends AbstractCharArrayAssert<S>>
+    extends AbstractAssert<S, char[]>
+    implements EnumerableAssert<AbstractCharArrayAssert<S>, Character>,
+    ArraySortedAssert<AbstractCharArrayAssert<S>, Character> {
 
   @VisibleForTesting
-  protected LongArrays arrays = LongArrays.instance();
+  protected CharArrays arrays = CharArrays.instance();
 
-  public AbstractLongArrayAssert(long[] actual, Class<?> selfType) {
+  public AbstractCharArrayAssert(char[] actual, Class<?> selfType) {
     super(actual, selfType);
   }
 
@@ -69,7 +69,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws AssertionError if the actual array is {@code null}.
    * @throws AssertionError if the actual array does not contain the given values.
    */
-  public S contains(long... values) {
+  public S contains(char... values) {
     arrays.assertContains(info, actual, values);
     return myself;
   }
@@ -85,7 +85,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws AssertionError if the actual array does not contain the given values, i.e. the actual array contains some
    *           or none of the given values, or the actual array contains more values than the given ones.
    */
-  public S containsOnly(long... values) {
+  public S containsOnly(char... values) {
     arrays.assertContainsOnly(info, actual, values);
     return myself;
   }
@@ -97,12 +97,12 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * 
    * <pre>
    * // assertion will pass
-   * assertThat(new long[] { 1, 2, 3 }).containsOnlyOnce(1, 2);
+   * assertThat(new char[] { 'a', 'b', 'c' }).containsOnlyOnce('a', 'b');
    * 
    * // assertions will fail
-   * assertThat(new long[] { 1, 2, 1 }).containsOnlyOnce(1);
-   * assertThat(new long[] { 1, 2, 3 }).containsOnlyOnce(4);
-   * assertThat(new long[] { 1, 2, 3, 3 }).containsOnlyOnce(0, 1, 2, 3, 4, 5);
+   * assertThat(new char[] { 'a', 'b', 'a' }).containsOnlyOnce('a');
+   * assertThat(new char[] { 'a', 'b', 'c' }).containsOnlyOnce('d');
+   * assertThat(new char[] { 'a', 'b', 'c', 'c' }).containsOnlyOnce(0, 'a', 'b', 'c', 'd', 'e');
    * </pre>
    * 
    * @param values the given values.
@@ -113,7 +113,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws AssertionError if the actual group does not contain the given values, i.e. the actual group contains some
    *           or none of the given values, or the actual group contains more than once these values.
    */
-  public S containsOnlyOnce(long... values) {
+  public S containsOnlyOnce(char... values) {
     arrays.assertContainsOnlyOnce(info, actual, values);
     return myself;
   }
@@ -127,7 +127,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws AssertionError if the given array is {@code null}.
    * @throws AssertionError if the actual array does not contain the given sequence.
    */
-  public S containsSequence(long... sequence) {
+  public S containsSequence(char... sequence) {
     arrays.assertContainsSequence(info, actual, sequence);
     return myself;
   }
@@ -144,7 +144,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    *           the actual array.
    * @throws AssertionError if the actual array does not contain the given value at the given index.
    */
-  public S contains(long value, Index index) {
+  public S contains(char value, Index index) {
     arrays.assertContains(info, actual, value, index);
     return myself;
   }
@@ -159,7 +159,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws AssertionError if the actual array is {@code null}.
    * @throws AssertionError if the actual array contains any of the given values.
    */
-  public S doesNotContain(long... values) {
+  public S doesNotContain(char... values) {
     arrays.assertDoesNotContain(info, actual, values);
     return myself;
   }
@@ -174,7 +174,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws NullPointerException if the given {@code Index} is {@code null}.
    * @throws AssertionError if the actual array contains the given value at the given index.
    */
-  public S doesNotContain(long value, Index index) {
+  public S doesNotContain(char value, Index index) {
     arrays.assertDoesNotContain(info, actual, value, index);
     return myself;
   }
@@ -193,7 +193,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
 
   /**
    * Verifies that the actual array starts with the given sequence of values, without any other values between them.
-   * Similar to <code>{@link #containsSequence(long...)}</code>, but it also verifies that the first element in the
+   * Similar to <code>{@link #containsSequence(char...)}</code>, but it also verifies that the first element in the
    * sequence is also first element of the actual array.
    * 
    * @param sequence the sequence of values to look for.
@@ -203,14 +203,14 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws AssertionError if the actual array is {@code null}.
    * @throws AssertionError if the actual array does not start with the given sequence.
    */
-  public S startsWith(long... sequence) {
+  public S startsWith(char... sequence) {
     arrays.assertStartsWith(info, actual, sequence);
     return myself;
   }
 
   /**
    * Verifies that the actual array ends with the given sequence of values, without any other values between them.
-   * Similar to <code>{@link #containsSequence(long...)}</code>, but it also verifies that the last element in the
+   * Similar to <code>{@link #containsSequence(char...)}</code>, but it also verifies that the last element in the
    * sequence is also last element of the actual array.
    * 
    * @param sequence the sequence of values to look for.
@@ -220,7 +220,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * @throws AssertionError if the actual array is {@code null}.
    * @throws AssertionError if the actual array does not end with the given sequence.
    */
-  public S endsWith(long... sequence) {
+  public S endsWith(char... sequence) {
     arrays.assertEndsWith(info, actual, sequence);
     return myself;
   }
@@ -234,22 +234,22 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
 
   /** {@inheritDoc} */
   @Override
-  public S isSortedAccordingTo(Comparator<? super Long> comparator) {
+  public S isSortedAccordingTo(Comparator<? super Character> comparator) {
     arrays.assertIsSortedAccordingToComparator(info, actual, comparator);
     return myself;
   }
 
   /** {@inheritDoc} */
   @Override
-  public S usingElementComparator(Comparator<? super Long> customComparator) {
-    this.arrays = new LongArrays(new ComparatorBasedComparisonStrategy(customComparator));
+  public S usingElementComparator(Comparator<? super Character> customComparator) {
+    this.arrays = new CharArrays(new ComparatorBasedComparisonStrategy(customComparator));
     return myself;
   }
 
   /** {@inheritDoc} */
   @Override
   public S usingDefaultElementComparator() {
-    this.arrays = LongArrays.instance();
+    this.arrays = CharArrays.instance();
     return myself;
   }
 
@@ -259,13 +259,13 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    * Example :
    * 
    * <pre>
-   * long[] longs = { 1, 2, 3 };
+   * char[] chars = { 'a', 'b', 'c' };
    * 
    * // assertion will pass
-   * assertThat(longs).containsExactly(1, 2, 3);
+   * assertThat(chars).containsExactly('a', 'b', 'c');
    * 
    * // assertion will fail as actual and expected orders differ.
-   * assertThat(longs).containsExactly(2, 1, 3);
+   * assertThat(chars).containsExactly('b', 'a', 'c');
    * </pre>
    * 
    * @param values the given values.
@@ -276,7 +276,7 @@ public abstract class AbstractLongArrayAssert<S extends AbstractLongArrayAssert<
    *           contains some or none of the given values, or the actual group contains more values than the given ones
    *           or values are the same but the order is not.
    */
-  public S containsExactly(long... values) {
+  public S containsExactly(char... values) {
     objects.assertEqual(info, actual, values);
     return myself;
   }
