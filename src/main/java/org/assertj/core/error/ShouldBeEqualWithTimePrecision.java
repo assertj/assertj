3df7085e4398f9beebd902286eb1058ba954006a
Joel Costigliola <joel.costigliola@gmail.com>
2013-10-17 23:00:49 +0200

    Fixes #118 : Use same error message as assertj-joda-time for consistency

--- src/main/java/org/assertj/core/error/ShouldBeEqualWithTimePrecision.java
+++ src/main/java/org/assertj/core/error/ShouldBeEqualWithTimePrecision.java
@@ -1,13 +1,16 @@
 /*
  * Created on Aug 5, 2010
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the
  * License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ * language
  * governing permissions and limitations under the License.
  * 
  * Copyright @2010-2011 the original author or authors.
@@ -18,13 +21,13 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Creates an <code>{@link AssertionError}</code> indicating that an assertion that verifies that two dates (with max precision) are equals failed.
+ * Creates an <code>{@link AssertionError}</code> indicating that an assertion that verifies that two dates  are equals
+ * up to a given precision failed.
  *
  * @author William Delanoue
+ * @author Joel Costigliola
  */
 public class ShouldBeEqualWithTimePrecision extends BasicErrorMessageFactory {
-
-  private static final String EXPECTED_BUT_WAS_MESSAGE = "\nExpecting:\n <%s>\nto be equal to:\n <%s>\nbut was not using max precision of <%s>.";
 
   /**
    * Creates a new <code>{@link org.assertj.core.error.ShouldBeEqualWithTimePrecision}</code>.
@@ -39,7 +42,26 @@ public class ShouldBeEqualWithTimePrecision extends BasicErrorMessageFactory {
   }
 
   private ShouldBeEqualWithTimePrecision(Date actual, Date expected, TimeUnit precision) {
-    super(EXPECTED_BUT_WAS_MESSAGE, actual, expected, precision.name());
+    super(buildErrorMessageTemplate(precision), actual, expected);
+  }
+
+  private static String buildErrorMessageTemplate(final TimeUnit precision) {
+    String fields = "";
+    String lastField = "";
+    if (precision.equals(TimeUnit.HOURS)) {
+      lastField = "day";
+    } else if (precision.equals(TimeUnit.MINUTES)) {
+      fields = ", day";
+      lastField = "hour";
+    } else if (precision.equals(TimeUnit.SECONDS)) {
+      fields = ", day, hour";
+      lastField = "minute";
+    } else if (precision.equals(TimeUnit.MILLISECONDS)) {
+      fields = ", day, hour, minute";
+      lastField = "second";
+    }
+    return "\nExpecting:\n  <%s>\nto have same year, month" + fields + " and " + lastField + " as:\n  <%s>\nbut had " +
+             "not.";
   }
 
 }
---
+++ src/test/java/org/assertj/core/error/ShouldBeEqualWithTimePrecision_create_Test.java
@@ -0,0 +1,70 @@
+package org.assertj.core.error;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.error.ShouldBeEqualWithTimePrecision.shouldBeEqual;
+import static org.assertj.core.util.Dates.parseDatetimeWithMs;
+
+import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import org.assertj.core.description.TextDescription;
+
+
+/**
+ * Tests for <code>{@link ShouldBeEqualWithTimePrecision#create(org.assertj.core.description.Description)}</code>.
+ *
+ * @author Joel Costigliola
+ */
+public class ShouldBeEqualWithTimePrecision_create_Test {
+
+  @Test
+  public void should_create_error_message_ignoring_millisseconds() throws ParseException {
+    ErrorMessageFactory factory = shouldBeEqual(parseDatetimeWithMs("2011-01-01T05:00:00.000"),
+                                                parseDatetimeWithMs("2011-01-01T06:05:17.003"), TimeUnit.MILLISECONDS);
+
+    String message = factory.create(new TextDescription("Test"));
+    assertThat(message).isEqualTo("[Test] \nExpecting:\n  <2011-01-01T05:00:00>\n" +
+                                    "to have same year, month, day, hour, minute and second as:\n  " +
+                                    "<2011-01-01T06:05:17>\n" +
+                                    "but had not.");
+  }
+
+  @Test
+  public void should_create_error_message_ignoring_seconds() throws ParseException {
+    ErrorMessageFactory factory = shouldBeEqual(parseDatetimeWithMs("2011-01-01T05:00:00.000"),
+                                                parseDatetimeWithMs("2011-01-01T06:05:17.003"), TimeUnit.SECONDS);
+
+    String message = factory.create(new TextDescription("Test"));
+    assertThat(message).isEqualTo("[Test] \nExpecting:\n  <2011-01-01T05:00:00>\n" +
+                                    "to have same year, month, day, hour and minute as:\n  " +
+                                    "<2011-01-01T06:05:17>\n" +
+                                    "but had not.");
+  }
+
+  @Test
+  public void should_create_error_message_ignoring_miinutes() throws ParseException {
+    ErrorMessageFactory factory = shouldBeEqual(parseDatetimeWithMs("2011-01-01T05:00:00.000"),
+                                                parseDatetimeWithMs("2011-01-01T06:05:17.003"), TimeUnit.MINUTES);
+
+    String message = factory.create(new TextDescription("Test"));
+    assertThat(message).isEqualTo("[Test] \nExpecting:\n  <2011-01-01T05:00:00>\n" +
+                                    "to have same year, month, day and hour as:\n  " +
+                                    "<2011-01-01T06:05:17>\n" +
+                                    "but had not.");
+  }
+
+  @Test
+  public void should_create_error_message_ignoring_hours() throws ParseException {
+    ErrorMessageFactory factory = shouldBeEqual(parseDatetimeWithMs("2011-01-01T05:00:00.000"),
+                                                parseDatetimeWithMs("2011-01-01T06:05:17.003"), TimeUnit.HOURS);
+
+    String message = factory.create(new TextDescription("Test"));
+    assertThat(message).isEqualTo("[Test] \nExpecting:\n  <2011-01-01T05:00:00>\n" +
+                                    "to have same year, month and day as:\n  " +
+                                    "<2011-01-01T06:05:17>\n" +
+                                    "but had not.");
+  }
+
+}
