b80724e5eedf45d61924e6e0dcbc51148add462c
Gregor Zeitlinger <gregor.zeitlinger@txtr.com>
2013-12-10 09:34:20 +0100

    make an abstract base class for softly and soft assertions

--- src/test/java/org/assertj/core/api/SoftlyTest.java
+++ src/test/java/org/assertj/core/api/SoftlyTest.java
@@ -1,17 +1,48 @@
 package org.assertj.core.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
 import org.assertj.core.util.Lists;
-import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runners.model.MultipleFailureException;
 
 public class SoftlyTest {
 
-    @Rule
+    //we cannot make it a rule here, because we need to test the failure without this test failing!
+    //@Rule
     public final Softly softly = new Softly();
 
     @Test
-    public void testSoftly() throws Exception {
+    public void testSuccess() throws Throwable {
         softly.assertThat(1).isEqualTo(1);
         softly.assertThat(Lists.newArrayList(1, 2)).containsOnly(1, 2);
+        softly.collector.verify();
+    }
+
+    @Test
+    public void testFail() throws Throwable {
+        try {
+            softly.assertThat(1).isEqualTo(1);
+            softly.assertThat(1).isEqualTo(2);
+            softly.assertThat(Lists.newArrayList(1, 2)).containsOnly(1, 3);
+            softly.collector.verify();
+            fail("Should not reach here");
+        } catch (MultipleFailureException e) {
+            List<Throwable> failures = e.getFailures();
+            assertThat(failures).hasSize(2);
+            assertThat(failures.get(0).getMessage()).isEqualTo("expected:<[2]> but was:<[1]>");
+            assertThat(failures.get(1).getMessage()).isEqualTo("\n" +
+                "Expecting:\n" +
+                " <[1, 2]>\n" +
+                "to contain only:\n" +
+                " <[1, 3]>\n" +
+                "elements not found:\n" +
+                " <[3]>\n" +
+                "and elements not expected:\n" +
+                " <[2]>\n");
+        }
     }
 }
