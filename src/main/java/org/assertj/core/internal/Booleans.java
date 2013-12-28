mariuszs/assertj-core

Issue #148 - Standard Java, Binary and Hexadecimal representation of objects

src/main/java/org/assertj/core/internal/Booleans.java
@@ -57,7 +57,7 @@ public class Booleans {
   public void assertEqual(AssertionInfo info, Boolean actual, boolean expected) {
     assertNotNull(info, actual);
     if (actual == expected) return;
-    throw failures.failure(info, shouldBeEqual(actual, expected));
+    throw failures.failure(info, shouldBeEqual(actual, expected, info.presentation()));
   }
 
   /**

src/test/java/org/assertj/core/error/ShouldBeSubsetOf_create_Test.java
@@ -18,15 +18,15 @@ import static junit.framework.Assert.assertEquals;
 import static org.assertj.core.error.ShouldBeSubsetOf.shouldBeSubsetOf;
 import static org.assertj.core.util.Lists.*;
 
-import org.assertj.core.description.Description;
 import org.assertj.core.description.TextDescription;
 import org.assertj.core.internal.ComparatorBasedComparisonStrategy;
+import org.assertj.core.presentation.StandardPresentation;
 import org.assertj.core.util.CaseInsensitiveStringComparator;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Tests for <code>{@link ShouldBeSubsetOf#create(Description)}</code>.
+ * Tests for <code>{@link ShouldBeSubsetOf#create(org.assertj.core.description.Description, org.assertj.core.presentation.Presentation)}</code>.
  * 
  * @author Maciej Jaskowski
  */
@@ -41,7 +41,7 @@ public class ShouldBeSubsetOf_create_Test {
 
   @Test
   public void should_create_error_message() {
-    String message = factory.create(new TextDescription("Test"));
+    String message = factory.create(new TextDescription("Test"), new StandardPresentation());
     assertEquals(
         "[Test] \nExpecting :\n <[\"Yoda\", \"Luke\"]>\nto be subset of\n <[\"Han\", \"Luke\"]>\nbut found those extra elements:\n <[\"Yoda\"]>",
         message);
@@ -51,7 +51,7 @@ public class ShouldBeSubsetOf_create_Test {
   public void should_create_error_message_with_custom_comparison_strategy() {
     ErrorMessageFactory factory = shouldBeSubsetOf(newArrayList("Yoda", "Luke"), newArrayList("Han", "Luke"),
         newArrayList("Yoda"), new ComparatorBasedComparisonStrategy(CaseInsensitiveStringComparator.instance));
-    String message = factory.create(new TextDescription("Test"));
+    String message = factory.create(new TextDescription("Test"), new StandardPresentation());
     assertEquals(
         "[Test] \nExpecting according to 'CaseInsensitiveStringComparator' comparator:\n <[\"Yoda\", \"Luke\"]>\nto be subset of\n <[\"Han\", \"Luke\"]>\nbut found those extra elements:\n <[\"Yoda\"]>",
         message);

src/test/java/org/assertj/core/error/ShouldHaveComparableElementsAccordingToComparator_create_Test.java
@@ -19,16 +19,14 @@ import static junit.framework.Assert.assertEquals;
 import static org.assertj.core.error.ShouldBeSorted.shouldHaveComparableElementsAccordingToGivenComparator;
 import static org.assertj.core.util.Arrays.array;
 
-import org.assertj.core.description.Description;
-import org.assertj.core.error.ErrorMessageFactory;
-import org.assertj.core.error.ShouldBeSorted;
 import org.assertj.core.internal.TestDescription;
+import org.assertj.core.presentation.StandardPresentation;
 import org.assertj.core.util.CaseInsensitiveStringComparator;
 import org.junit.*;
 
 
 /**
- * Tests for <code>{@link ShouldBeSorted#create(Description)}</code>.
+ * Tests for <code>{@link ShouldBeSorted#create(org.assertj.core.description.Description, org.assertj.core.presentation.Presentation)}</code>.
  * 
  * @author Joel Costigliola
  */
@@ -43,7 +41,7 @@ public class ShouldHaveComparableElementsAccordingToComparator_create_Test {
 
   @Test
   public void should_create_error_message() {
-    String message = factory.create(new TestDescription("Test"));
+    String message = factory.create(new TestDescription("Test"), new StandardPresentation());
     assertEquals(
         "[Test] \nsome elements are not mutually comparable according to 'CaseInsensitiveStringComparator' comparator in group:\n<[\"b\", \"c\", \"a\"]>",
         message);

src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertHasSameSizeAs_with_Iterable_Test.java
@@ -63,7 +63,8 @@ public class ObjectArrays_assertHasSameSizeAs_with_Iterable_Test extends ObjectA
     try {
       arrays.assertHasSameSizeAs(info, actual, other);
     } catch (AssertionError e) {
-      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length, other.size()).create(null));
+      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length, other.size())
+          .create(null, info.presentation()));
       return;
     }
     failBecauseExpectedAssertionErrorWasNotThrown();

src/test/java/org/assertj/core/internal/strings/Strings_assertHasSameSizeAs_with_Array_Test.java
@@ -23,7 +23,6 @@ import static org.assertj.core.util.FailureMessages.actualIsNull;
 import static org.mockito.Mockito.verify;
 
 import org.assertj.core.api.AssertionInfo;
-import org.assertj.core.api.Assertions;
 import org.assertj.core.internal.Strings;
 import org.assertj.core.internal.StringsBaseTest;
 import org.junit.Test;
@@ -50,7 +49,8 @@ public class Strings_assertHasSameSizeAs_with_Array_Test extends StringsBaseTest
     try {
       strings.assertHasSameSizeAs(info, actual, other);
     } catch (AssertionError e) {
-      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length(), other.length).create(null));
+      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length(), other.length)
+          .create(null, info.presentation()));
       return;
     }
     failBecauseExpectedAssertionErrorWasNotThrown();

assertj/assertj

src/main/java/org/assertj/core/internal/Booleans.java
@@ -57,7 +57,7 @@ public class Booleans {
   public void assertEqual(AssertionInfo info, Boolean actual, boolean expected) {
     assertNotNull(info, actual);
     if (actual == expected) return;
-    throw failures.failure(info, shouldBeEqual(actual, expected));
+    throw failures.failure(info, shouldBeEqual(actual, expected, info.representation()));
   }
 
   /**

src/test/java/org/assertj/core/error/ShouldBeSubsetOf_create_Test.java
@@ -18,15 +18,15 @@ import static junit.framework.Assert.assertEquals;
 import static org.assertj.core.error.ShouldBeSubsetOf.shouldBeSubsetOf;
 import static org.assertj.core.util.Lists.*;
 
-import org.assertj.core.description.Description;
 import org.assertj.core.description.TextDescription;
 import org.assertj.core.internal.ComparatorBasedComparisonStrategy;
+import org.assertj.core.presentation.StandardRepresentation;
 import org.assertj.core.util.CaseInsensitiveStringComparator;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Tests for <code>{@link ShouldBeSubsetOf#create(Description)}</code>.
+ * Tests for <code>{@link ShouldBeSubsetOf#create(org.assertj.core.description.Description, org.assertj.core.presentation.Representation)}</code>.
  * 
  * @author Maciej Jaskowski
  */
@@ -41,7 +41,7 @@ public class ShouldBeSubsetOf_create_Test {
 
   @Test
   public void should_create_error_message() {
-    String message = factory.create(new TextDescription("Test"));
+    String message = factory.create(new TextDescription("Test"), new StandardRepresentation());
     assertEquals(
         "[Test] \nExpecting :\n <[\"Yoda\", \"Luke\"]>\nto be subset of\n <[\"Han\", \"Luke\"]>\nbut found those extra elements:\n <[\"Yoda\"]>",
         message);
@@ -51,7 +51,7 @@ public class ShouldBeSubsetOf_create_Test {
   public void should_create_error_message_with_custom_comparison_strategy() {
     ErrorMessageFactory factory = shouldBeSubsetOf(newArrayList("Yoda", "Luke"), newArrayList("Han", "Luke"),
         newArrayList("Yoda"), new ComparatorBasedComparisonStrategy(CaseInsensitiveStringComparator.instance));
-    String message = factory.create(new TextDescription("Test"));
+    String message = factory.create(new TextDescription("Test"), new StandardRepresentation());
     assertEquals(
         "[Test] \nExpecting according to 'CaseInsensitiveStringComparator' comparator:\n <[\"Yoda\", \"Luke\"]>\nto be subset of\n <[\"Han\", \"Luke\"]>\nbut found those extra elements:\n <[\"Yoda\"]>",
         message);

src/test/java/org/assertj/core/error/ShouldHaveComparableElementsAccordingToComparator_create_Test.java
@@ -19,16 +19,14 @@ import static junit.framework.Assert.assertEquals;
 import static org.assertj.core.error.ShouldBeSorted.shouldHaveComparableElementsAccordingToGivenComparator;
 import static org.assertj.core.util.Arrays.array;
 
-import org.assertj.core.description.Description;
-import org.assertj.core.error.ErrorMessageFactory;
-import org.assertj.core.error.ShouldBeSorted;
 import org.assertj.core.internal.TestDescription;
+import org.assertj.core.presentation.StandardRepresentation;
 import org.assertj.core.util.CaseInsensitiveStringComparator;
 import org.junit.*;
 
 
 /**
- * Tests for <code>{@link ShouldBeSorted#create(Description)}</code>.
+ * Tests for <code>{@link ShouldBeSorted#create(org.assertj.core.description.Description, org.assertj.core.presentation.Representation)}</code>.
  * 
  * @author Joel Costigliola
  */
@@ -43,7 +41,7 @@ public class ShouldHaveComparableElementsAccordingToComparator_create_Test {
 
   @Test
   public void should_create_error_message() {
-    String message = factory.create(new TestDescription("Test"));
+    String message = factory.create(new TestDescription("Test"), new StandardRepresentation());
     assertEquals(
         "[Test] \nsome elements are not mutually comparable according to 'CaseInsensitiveStringComparator' comparator in group:\n<[\"b\", \"c\", \"a\"]>",
         message);

src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertHasSameSizeAs_with_Iterable_Test.java
@@ -63,7 +63,8 @@ public class ObjectArrays_assertHasSameSizeAs_with_Iterable_Test extends ObjectA
     try {
       arrays.assertHasSameSizeAs(info, actual, other);
     } catch (AssertionError e) {
-      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length, other.size()).create(null));
+      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length, other.size())
+          .create(null, info.representation()));
       return;
     }
     failBecauseExpectedAssertionErrorWasNotThrown();

src/test/java/org/assertj/core/internal/strings/Strings_assertHasSameSizeAs_with_Array_Test.java
@@ -23,7 +23,6 @@ import static org.assertj.core.util.FailureMessages.actualIsNull;
 import static org.mockito.Mockito.verify;
 
 import org.assertj.core.api.AssertionInfo;
-import org.assertj.core.api.Assertions;
 import org.assertj.core.internal.Strings;
 import org.assertj.core.internal.StringsBaseTest;
 import org.junit.Test;
@@ -50,7 +49,8 @@ public class Strings_assertHasSameSizeAs_with_Array_Test extends StringsBaseTest
     try {
       strings.assertHasSameSizeAs(info, actual, other);
     } catch (AssertionError e) {
-      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length(), other.length).create(null));
+      assertThat(e).hasMessage(shouldHaveSameSizeAs(actual, actual.length(), other.length)
+          .create(null, info.representation()));
       return;
     }
     failBecauseExpectedAssertionErrorWasNotThrown();