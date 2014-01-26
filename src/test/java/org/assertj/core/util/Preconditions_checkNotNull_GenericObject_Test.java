35318ef2de97d8bd15f40613f4aa9a4fc4c5974c
croesch <christianroesch@gmx.net>
2014-01-26 21:21:36 +0100

    #159 added tests for existing methods.

---
+++ src/test/java/org/assertj/core/util/Preconditions_checkNotNull_GenericObject_Test.java
@@ -0,0 +1,47 @@
+/*
+ * Created on Apr 29, 2007
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
+ * Copyright @2007-2012 the original author or authors.
+ */
+package org.assertj.core.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.test.ExpectedException.none;
+
+import org.assertj.core.test.ExpectedException;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Tests for <code>{@link Preconditions#checkNotNull(Object)}</code>.
+ * 
+ * @author Christian Rösch
+ */
+public class Preconditions_checkNotNull_GenericObject_Test {
+  @Rule
+  public ExpectedException thrown = none();
+
+  @Test
+  public void should_throw_nullpointerexception_if_object_is_null() {
+    thrown.expect(NullPointerException.class);
+    Object object = null;
+    Preconditions.checkNotNull(object);
+  }
+
+  @Test
+  public void should_return_object_if_it_is_not_null_nor_empty() {
+    String object = "4711";
+    String result = Preconditions.checkNotNull(object);
+
+    assertThat(result).isEqualTo(object);
+  }
+}
