41b081af270c813778644f229bcc25f81119a0f3
mhaligowski <mhaligowski@gmail.com>
2014-07-31 01:17:44 +0200

    Javadoc for Extractor interface

--- src/main/java/org/assertj/core/api/iterable/Extractor.java
+++ src/main/java/org/assertj/core/api/iterable/Extractor.java
@@ -1,5 +1,17 @@
 package org.assertj.core.api.iterable;
 
+import org.assertj.core.api.ListAssert;
+import org.assertj.core.api.ObjectArrayAssert;
+
+/**
+ * Function converting an element to another element. Used in {@link ListAssert#extracting(Extractor)} and
+ * {@link ObjectArrayAssert#extracting(Extractor)}.
+ * 
+ * @author Mateusz Haligowski
+ *
+ * @param <F> type of element from which the conversion happens
+ * @param <T> target element type
+ */
 public interface Extractor<F, T> {
   T extract(F input);
 }
