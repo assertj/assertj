c0963ef9ed0a63958f1149b4c51c912fb26999a1
Joel Costigliola <joel.costigliola@gmail.com>
2014-10-04 17:34:14 +1300

    simplify Tuple and add more tests

--- src/main/java/org/assertj/core/groups/Tuple.java
+++ src/main/java/org/assertj/core/groups/Tuple.java
@@ -1,22 +1,23 @@
 /*
  * Created on Jan 28, 2011
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this Throwable except in compliance with the
- * License. You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this Throwable except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- *
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * 
  * Copyright @2011 the original author or authors.
  */
 package org.assertj.core.groups;
 
+import static java.util.Collections.addAll;
+import static org.assertj.core.util.Collections.format;
 import static org.assertj.core.util.Lists.newArrayList;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Tuple {
@@ -24,52 +25,42 @@ public class Tuple {
   private final List<Object> datas = newArrayList();
 
   public Tuple(Object... values) {
-    java.util.Collections.addAll(datas, values);
+	addAll(datas, values);
   }
 
   public void addData(Object data) {
-    datas.add(data);
+	datas.add(data);
   }
 
   public Object[] toArray() {
-    return datas.toArray();
+	return datas.toArray();
   }
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((datas == null) ? 0 : datas.hashCode());
-    return result;
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + datas.hashCode();
+	return result;
   }
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
-    Tuple other = (Tuple) obj;
-    if (datas == null) {
-      if (other.datas != null) return false;
-    } else if (!datas.equals(other.datas)) return false;
-    return true;
+	if (this == obj) return true;
+	if (obj == null) return false;
+	if (getClass() != obj.getClass()) return false;
+	Tuple other = (Tuple) obj;
+	// datas can't be null
+	return datas.equals(other.datas);
   }
 
   @Override
   public String toString() {
-    return java.util.Arrays.toString(toArray());
-  }
-
-  public static List<Tuple> buildTuples(int n) {
-    List<Tuple> tuples = new ArrayList<Tuple>(n);
-    for (int i = 0; i < n; i++) {
-      tuples.add(new Tuple());
-    }
-    return tuples;
+	return format(datas, "(", ")");
   }
 
   public static Tuple tuple(Object... values) {
-    return new Tuple(values);
+	return new Tuple(values);
   }
 
 }
---
+++ src/test/java/org/assertj/core/api/Tuple_Test.java
@@ -0,0 +1,43 @@
+package org.assertj.core.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Arrays.array;
+
+import org.assertj.core.groups.Tuple;
+import org.junit.Test;
+
+public class Tuple_Test {
+
+  @Test
+  public void should_create_tuple() {
+    Tuple tuple = new Tuple("Yoda", 800, "Jedi");
+    assertThat(tuple).isEqualTo(new Tuple("Yoda", 800, "Jedi"));
+  }
+
+  @Test
+  public void should_create_empty_tuple() {
+	Tuple tuple = new Tuple();
+	assertThat(tuple).isEqualTo(new Tuple());
+  }
+  
+  @Test
+  public void add_an_element_to_a_tuple() {
+	Tuple tuple = new Tuple("Yoda", 800);
+	tuple.addData("Jedi");
+	assertThat(tuple).isEqualTo(new Tuple("Yoda", 800, "Jedi"));
+  }
+  
+  @SuppressWarnings("unchecked")
+  @Test
+  public void convert_tuple_to_an_array() {
+	Tuple tuple = new Tuple("Yoda", 800, "Jedi");
+	assertThat(tuple.toArray()).isEqualTo(array("Yoda", 800, "Jedi"));
+  }
+  
+  @Test
+  public void tuple_representation() {
+	Tuple tuple = new Tuple("Yoda", 800, "Jedi");
+	assertThat(tuple.toString()).isEqualTo("(\"Yoda\", 800, \"Jedi\")");
+  }
+  
+}
