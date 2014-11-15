f79dd69813cbcc81e16010573e3840bcdd7b72d2
Alan Rothkopf <arothkopf@usa.com>
2014-11-15 17:06:49 +0100

    Fixed windows-specific unit test failures

--- src/test/java/org/assertj/core/error/ShouldHaveAtLeastOneElementOfType_create_Test.java
+++ src/test/java/org/assertj/core/error/ShouldHaveAtLeastOneElementOfType_create_Test.java
@@ -1,15 +1,15 @@
 /*
  * Created on Jan 28, 2011
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * Copyright @2014 the original author or authors.
  */
 package org.assertj.core.error;
@@ -28,28 +28,28 @@ import org.junit.Test;
  */
 public class ShouldHaveAtLeastOneElementOfType_create_Test {
 
-  @Test
-  public void should_create_error_message_for_iterable() {
-	ErrorMessageFactory factory = shouldHaveAtLeastOneElementOfType(newArrayList("Yoda", "Luke"), Long.class);
-	String message = factory.create(new TextDescription("Test"), new StandardRepresentation());
-	assertThat(message).isEqualTo("[Test] \n"
-	                              + "Expecting:\n"
-	                              + "  <[\"Yoda\", \"Luke\"]>\n"
-	                              + "to have at least one element of type:\n"
-	                              + "  <java.lang.Long>\n"
-	                              + "but had none.");
-  }
+	@Test
+	public void should_create_error_message_for_iterable() {
+		ErrorMessageFactory factory = shouldHaveAtLeastOneElementOfType(newArrayList("Yoda", "Luke"), Long.class);
+		String message = factory.create(new TextDescription("Test"), new StandardRepresentation());
+		assertThat(message).isEqualTo("[Test] " + System.getProperty("line.separator")
+				+ "Expecting:" + System.getProperty("line.separator")
+				+ "  <[\"Yoda\", \"Luke\"]>" + System.getProperty("line.separator")
+				+ "to have at least one element of type:" + System.getProperty("line.separator")
+				+ "  <java.lang.Long>" + System.getProperty("line.separator")
+				+ "but had none.");
+	}
 
-  @Test
-  public void should_create_error_message_for_array() {
-	ErrorMessageFactory factory = shouldHaveAtLeastOneElementOfType(array("Yoda", "Luke"), Long.class);
-	String message = factory.create(new TextDescription("Test"), new StandardRepresentation());
-	assertThat(message).isEqualTo("[Test] \n"
-	                              + "Expecting:\n"
-	                              + "  <[\"Yoda\", \"Luke\"]>\n"
-	                              + "to have at least one element of type:\n"
-	                              + "  <java.lang.Long>\n"
-	                              + "but had none.");
-  }
+	@Test
+	public void should_create_error_message_for_array() {
+		ErrorMessageFactory factory = shouldHaveAtLeastOneElementOfType(array("Yoda", "Luke"), Long.class);
+		String message = factory.create(new TextDescription("Test"), new StandardRepresentation());
+		assertThat(message).isEqualTo("[Test] " + System.getProperty("line.separator")
+				+ "Expecting:" + System.getProperty("line.separator")
+				+ "  <[\"Yoda\", \"Luke\"]>" + System.getProperty("line.separator")
+				+ "to have at least one element of type:" + System.getProperty("line.separator")
+				+ "  <java.lang.Long>" + System.getProperty("line.separator")
+				+ "but had none.");
+	}
 
 }
