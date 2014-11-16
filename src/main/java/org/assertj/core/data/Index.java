f9847e6a43ee739c7344abb5e73cd88a86fe3f9e
Joel Costigliola <joel.costigliola@gmail.com>
2014-11-16 18:00:46 +1300

    Update licence for main java sources

--- src/main/java/org/assertj/core/data/Index.java
+++ src/main/java/org/assertj/core/data/Index.java
@@ -1,6 +1,4 @@
-/*
- * Created on Oct 26, 2010
- *
+/**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  *
@@ -10,7 +8,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * Copyright @2010-2012 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  */
 package org.assertj.core.data;
 
--- src/main/java/org/assertj/core/util/Objects.java
+++ src/main/java/org/assertj/core/util/Objects.java
@@ -1,6 +1,4 @@
-/*
- * Created on Jun 2, 2006
- *
+/**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  *
@@ -10,7 +8,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * Copyright @2006-2012 the original author or authors.
+ * Copyright 2012-2014 the original author or authors.
  */
 package org.assertj.core.util;
 
