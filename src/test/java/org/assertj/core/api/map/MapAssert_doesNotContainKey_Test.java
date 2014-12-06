cf36befd2d3bccc46074a10ca781cc8e0acc827d
Paul Doran <dorzey@gmail.com>
2014-12-06 23:52:32 +0000

    Added tests for MapAssert

--- src/test/java/org/assertj/core/api/map/MapAssert_doesNotContainKey_Test.java
+++ src/test/java/org/assertj/core/api/map/MapAssert_doesNotContainKey_Test.java
@@ -34,6 +34,6 @@ public class MapAssert_doesNotContainKey_Test extends MapAssertBaseTest {
 
   @Override
   protected void verify_internal_effects() {
-    verify(maps).assertDoesNotContainKey(getInfo(assertions), getActual(assertions), "key1");
+    verify(maps).assertDoesNotContainKeys(getInfo(assertions), getActual(assertions), "key1");
   }
 }
--- src/test/java/org/assertj/core/api/map/MapAssert_doesNotcontainKeys_Test.java
+++ src/test/java/org/assertj/core/api/map/MapAssert_doesNotcontainKeys_Test.java
@@ -1,13 +1,16 @@
 /*
  * Created on Jun 3, 2012
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the
  * License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+ * language
  * governing permissions and limitations under the License.
  * 
  * Copyright @2010-2012 the original author or authors.
@@ -20,23 +23,22 @@ import static org.mockito.Mockito.verify;
 import org.assertj.core.api.MapAssert;
 import org.assertj.core.api.MapAssertBaseTest;
 
-
 /**
  * Tests for <code>{@link org.assertj.core.api.MapAssert#containsKey(Object)}</code>.
  *
  * @author dorzey
  */
-public class MapAssert_containsKeys_Test extends MapAssertBaseTest {
+public class MapAssert_doesNotContainKeys_Test extends MapAssertBaseTest {
 
   final Object[] keys = array("key1", "key2");
 
   @Override
   protected MapAssert<Object, Object> invoke_api_method() {
-    return assertions.containsKeys("key1", "key2");
+	return assertions.doesNotContainKeys("key1", "key2");
   }
 
   @Override
   protected void verify_internal_effects() {
-    verify(maps).assertContainsKeys(getInfo(assertions), getActual(assertions), keys);
+	verify(maps).assertDoesNotContainKeys(getInfo(assertions), getActual(assertions), keys);
   }
 }
