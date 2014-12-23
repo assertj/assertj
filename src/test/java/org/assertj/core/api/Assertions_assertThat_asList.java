008fc2ba3bba14929bf0152f23c076df94c74b19
Tim Perry <pimterry@gmail.com>
2014-12-23 09:46:38 +0000

    Rename asX test to asList, and rename test var too for clarity

--- src/test/java/org/assertj/core/api/Assertions_assertThat_asList.java
+++ src/test/java/org/assertj/core/api/Assertions_assertThat_asList.java
@@ -16,16 +16,15 @@ import org.assertj.core.test.ExpectedException;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.test.ExpectedException.none;
 
 /**
- * Tests for Assert.asX() methods
+ * Tests for Assert.asList() methods
  */
-public class Assertions_assertThat_asX {
+public class Assertions_assertThat_asList {
 
     @Rule
     public ExpectedException thrown = none();
@@ -38,10 +37,10 @@ public class Assertions_assertThat_asX {
 
     @Test
     public void should_fail_list_asserts_on_non_list_objects_even_with_asList() {
-        Object nonListAsObject = new Object();
+        Object nonList = new Object();
 
         thrown.expectAssertionError("an instance of:\n <java.util.List>\nbut was instance of:\n <java.lang.Object>");
-        assertThat(nonListAsObject).asList().isSorted();
+        assertThat(nonList).asList().isSorted();
     }
 
 }
