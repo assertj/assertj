09e3549af40eb15de0b2d7a9ce369490030d729f
Lovro Pandzic <lovro.pandzic@gmail.com>
2015-01-07 18:55:53 +0100

    Add comparable assert to BDDAssertions

--- src/test/java/org/assertj/core/api/BDDAssertions_then_Test.java
+++ src/test/java/org/assertj/core/api/BDDAssertions_then_Test.java
@@ -57,6 +57,26 @@ public class BDDAssertions_then_Test {
   }
 
   @Test
+  public void should_delegate_to_assert_comparable() throws Exception {
+
+    class IntBox implements Comparable<IntBox> {
+
+      private final Integer number;
+
+      IntBox(Integer number) {
+        this.number = number;
+      }
+
+      @Override
+      public int compareTo(IntBox o) {
+        return number.compareTo(o.number);
+      }
+    }
+
+    then(new IntBox(1)).isLessThan(new IntBox(2));
+  }
+
+  @Test
   public void then_Iterable() {
     Iterable<String> iterable = Arrays.asList("1");
     then(iterable).contains("1");
