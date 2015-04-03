832fddc01716eaf4229f73776ac5b494314fb378
Alexander Bischof <alexanderxbischof@gmail.com>
2015-04-04 00:18:19 +0200

    #356 Created packages and structures with hasScheme

---
+++ src/main/java/org/assertj/core/api/AbstractUrlAssert.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2015 the original author or authors.
+ */
+package org.assertj.core.api;
+
+import org.assertj.core.internal.Urls;
+import org.assertj.core.util.VisibleForTesting;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * Base class for all implementations of assertions for {@link URL}s.
+ *
+ * @param <S> the "self" type of this assertion class. Please read &quot;<a href="http://bit.ly/anMa4g"
+ *          target="_blank">Emulating 'self types' using Java Generics to simplify fluent API implementation</a>&quot;
+ *          for more details.
+ *
+ * @see java.net.URL
+ *
+ * @author Alexander Bischof
+ */
+public abstract class AbstractUrlAssert<S extends AbstractUrlAssert<S>> extends AbstractAssert<S, URL> {
+
+  @VisibleForTesting
+  protected Urls urls = Urls.instance();
+
+  protected AbstractUrlAssert(final URL actual, final Class<?> selfType) {
+	super(actual, selfType);
+  }
+
+  /**
+   * Verifies that the actual {@code URL} has the expected scheme.
+   * <p>
+   * This assertion will succeed:
+   *
+   * <pre><code class='java'>
+   * assertThat(new URL("http://helloworld.org").hasSchemeEqualsTo("http")
+   * </code></pre>
+   *
+   * Whereas this assertion will fail:
+   *
+   * <pre><code class='java'>
+   * assertThat(new URL("ftp://helloworld.org").hasSchemeEqualsTo("http")
+   * </code></pre>
+   *
+   * @param expected the expected length of the actual {@code CharSequence}.
+   * @return {@code this} assertion object.
+   * @throws AssertionError if the actual length is not equal to the expected length.
+   */
+  public S hasScheme(String expected) {
+	urls.assertHasScheme(info, actual, expected);
+	return myself;
+  }
+}
---
+++ src/main/java/org/assertj/core/internal/Urls.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2015 the original author or authors.
+ */
+package org.assertj.core.internal;
+
+import org.assertj.core.api.AssertionInfo;
+import org.assertj.core.util.UrlsException;
+import org.assertj.core.util.VisibleForTesting;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static java.lang.String.format;
+import static org.assertj.core.error.ShouldBeEqual.shouldBeEqual;
+import static org.assertj.core.internal.Comparables.assertNotNull;
+
+/**
+ * Core assertion class for {@link java.net.URL} assertions
+ */
+public class Urls {
+
+  private static final Urls INSTANCE = new Urls();
+
+  @VisibleForTesting
+  Failures failures = Failures.instance();
+
+  public static Urls instance() {
+	return INSTANCE;
+  }
+
+  Urls() {
+  }
+
+  public void assertHasScheme(final AssertionInfo info, final URL actual, final String expected) {
+	assertNotNull(info, actual);
+
+  try {
+    if (!actual.toURI().getScheme().equals(expected))
+        throw failures.failure(info, shouldBeEqual(actual, expected, info.representation()));
+    }catch(URISyntaxException e){
+      throw new UrlsException(format("Unable to parse URI reference:<%s>", actual), e);
+  }
+  }
+}
