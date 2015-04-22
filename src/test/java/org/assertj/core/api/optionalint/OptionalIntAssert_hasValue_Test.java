630818399a427ed09a986ccfaa1593b5d2c69e5c
Alexander Bischof <alexanderxbischof@gmail.com>
2015-04-23 01:40:33 +0200

    Added OptionalIntAssert, AbstractOptionalIntAssert with tests

---
+++ src/test/java/org/assertj/core/api/optionalint/OptionalIntAssert_hasValue_Test.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.core.api.optionalint;
+
+import org.assertj.core.api.BaseTest;
+import org.junit.Test;
+
+import java.util.OptionalInt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.error.OptionalIntShouldHaveValue.shouldHaveValue;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+
+public class OptionalIntAssert_hasValue_Test extends BaseTest {
+
+    @Test
+    public void should_fail_when_OptionalInt_is_null() throws Exception {
+        thrown.expectAssertionError(actualIsNull());
+
+        assertThat((OptionalInt) null).hasValue(10);
+    }
+
+    @Test
+    public void should_pass_if_OptionalInt_has_expected_value() throws Exception {
+        assertThat(OptionalInt.of(10)).hasValue(10);
+    }
+
+    @Test
+    public void should_fail_if_OptionalInt_does_not_have_expected_value() throws Exception {
+        OptionalInt actual = OptionalInt.of(5);
+        int expectedValue = 10;
+
+        thrown.expectAssertionError(shouldHaveValue(actual, expectedValue).create());
+
+        assertThat(actual).hasValue(expectedValue);
+    }
+
+    @Test
+    public void should_fail_if_OptionalInt_is_empty() throws Exception {
+        int expectedValue = 10;
+
+        thrown.expectAssertionError(shouldHaveValue(expectedValue).create());
+
+        assertThat(OptionalInt.empty()).hasValue(expectedValue);
+    }
+}
