7d393b179992a0ee8a7d369dfe761c00134abb20
Alban Dericbourg <alban@dericbourg.net>
2015-06-18 22:34:39 +0200

    Add assertion to check Optional value type.

---
+++ src/test/java/org/assertj/core/error/OptionalShouldContainInstanceOf_create_Test.java
@@ -0,0 +1,17 @@
+package org.assertj.core.error;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.error.OptionalShouldContainInstanceOf.shouldContainInstanceOf;
+
+public class OptionalShouldContainInstanceOf_create_Test {
+
+  @Test
+  public void should_create_error_message_with_expected_type() {
+    String errorMessage = shouldContainInstanceOf(Optional.empty(), Object.class).create();
+    assertThat(errorMessage).isEqualTo("\nExpecting Optional to contain a value of type java.lang.Object.");
+  }
+}
