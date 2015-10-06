4deb2484105127ebbd75aadda194932a8f111d63
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-10-06 11:55:41 +0200

    Backport assertThatThrownBy() and catchThrowable() methods

---
+++ src/test/java/org/assertj/core/api/throwable/ThrowableAssert_built_from_ThrowingCallable_Test.java
@@ -0,0 +1,60 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.core.api.throwable;
+
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Fail.failBecauseExceptionWasNotThrown;
+
+public class ThrowableAssert_built_from_ThrowingCallable_Test {
+
+  @Test
+  public void should_build_ThrowableAssert_with_runtime_exception_thrown_by_callable_code() {
+		// check that actual exception is the one thrown by ThrowingCallable()#run
+		assertThatThrownBy(new ThrowingCallable() {
+			@Override
+			public void call() {
+				throw new IllegalArgumentException("something was wrong");
+			}
+		}).isInstanceOf(IllegalArgumentException.class).hasMessage("something was wrong");
+  }
+
+  @Test
+  public void should_build_ThrowableAssert_with_throwable_thrown_by_callable_code() {
+		assertThatThrownBy(new ThrowingCallable() {
+			@Override
+			public void call() throws Exception {
+				throw new Exception("something was wrong");
+			}
+		}).isInstanceOf(Exception.class).hasMessage("something was wrong");
+  }
+
+  @Test
+  public void should_fail_if_nothing_is_thrown_by_callable_code() {
+		try {
+			assertThatThrownBy(new ThrowingCallable() {
+				@Override
+				public void call() {
+					// no exception
+				}
+			});
+		} catch (AssertionError e) {
+			assertThat(e).hasMessage("Expecting code to raise a throwable.");
+			return;
+		}
+		failBecauseExceptionWasNotThrown(AssertionError.class);
+  }
+}
