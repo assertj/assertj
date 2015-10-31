288650a12fef433998e713b8643bad33ca5cf374
gabga <info@gabga.com>
2015-10-31 15:12:28 +0100

    Did a cleanup and added licence comments.

--- src/test/java/org/assertj/core/internal/maps/Maps_assertEqual_Test.java
+++ src/test/java/org/assertj/core/internal/maps/Maps_assertEqual_Test.java
@@ -1,20 +1,18 @@
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * <p/>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * <p/>
+ *
  * Copyright 2012-2015 the original author or authors.
  */
 package org.assertj.core.internal.maps;
 
 import org.assertj.core.api.AssertionInfo;
-import org.assertj.core.data.MapEntry;
-import org.assertj.core.error.ShouldBe;
 import org.assertj.core.error.ShouldBeEqual;
 import org.assertj.core.internal.ComparisonStrategy;
 import org.assertj.core.internal.Maps;
@@ -31,7 +29,6 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.error.ShouldBeEqual.shouldBeEqual;
 import static org.assertj.core.test.TestData.someInfo;
 import static org.assertj.core.test.TestFailures.failBecauseExpectedAssertionErrorWasNotThrown;
 import static org.mockito.Matchers.eq;
