292f0ef574fc05d1dbd2de2996a80c3534450087
Joel Costigliola <joel.costigliola@gmail.com>
2015-10-31 21:00:29 +1300

    Document assertThatThrownBy limitation when using a test description

--- src/main/java/org/assertj/core/api/Assertions.java
+++ src/main/java/org/assertj/core/api/Assertions.java
@@ -1,13 +1,10 @@
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
  * Copyright 2012-2015 the original author or authors.
  */
 package org.assertj.core.api;
@@ -289,8 +286,7 @@ public class Assertions {
    * @param actual the path to test
    * @return the created assertion object
    */
-  public static AbstractPathAssert<?> assertThat(Path actual)
-  {
+  public static AbstractPathAssert<?> assertThat(Path actual) {
     return new PathAssert(actual);
   }
 
@@ -599,6 +595,16 @@ public class Assertions {
    *   
    * }).isInstanceOf(Exception.class)
    *   .hasMessageContaining("boom");</code></pre>
+   * 
+   * If the provided {@link ThrowingCallable} does not raise an exception, an error is immediately raised, 
+   * in that case the test description provided with {@link AbstractAssert#as(String, Object...) as(String, Object...)} is not honored. 
+   * To use a test description, use {@link #catchThrowable(ThrowingCallable) catchThrowable} as shown below.  
+   * <pre><code class='java'> // assertion will fail but "display me" won't appear in the error 
+   * assertThatThrownBy(() -> { // do nothing }).as("display me").isInstanceOf(Exception.class);
+   * 
+   * // assertion will fail AND "display me" will appear in the error
+   * Throwable thrown = catchThrowable(() -> { // do nothing });
+   * assertThat(thrown).as("display me").isInstanceOf(Exception.class); </code></pre>
    *
    * @param shouldRaiseThrowable The {@link ThrowingCallable} or lambda with the code that should raise the throwable.
    * @return The captured exception or <code>null</code> if none was raised by the callable.
