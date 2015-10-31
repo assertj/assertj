14faeb1aabce5027d3af9fda93bbdfe3a55e2ce0
gabga <info@gabga.com>
2015-10-31 13:17:04 +0100

    Differentiated the tests in Maps for unequal map entries and unequal map entry values.

--- src/test/java/org/assertj/core/internal/maps/Maps_assertEqual_Test.java
+++ src/test/java/org/assertj/core/internal/maps/Maps_assertEqual_Test.java
@@ -13,19 +13,21 @@
 package org.assertj.core.internal.maps;
 
 import org.assertj.core.api.AssertionInfo;
-import org.assertj.core.internal.ComparisonStrategy;
-import org.assertj.core.internal.Maps;
-import org.assertj.core.internal.MapsBaseTest;
-import org.assertj.core.internal.StandardComparisonStrategy;
+import org.assertj.core.internal.*;
 import org.assertj.core.presentation.MapRepresentation;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.data.MapEntry.entry;
 import static org.assertj.core.error.ShouldBeEqual.shouldBeEqual;
+import static org.assertj.core.test.Maps.mapOf;
 import static org.assertj.core.test.TestData.someInfo;
 import static org.assertj.core.test.TestFailures.failBecauseExpectedAssertionErrorWasNotThrown;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -34,31 +36,48 @@ import static org.mockito.Mockito.verify;
  * @author Vojislav Marinkovic
  */
 public class Maps_assertEqual_Test extends MapsBaseTest {
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+        actual = new HashMap<String, String>();
+        actual.put("a", "1");
+        actual.put("b", "2");
+        actual.put("c", "3");
+    }
 
     @Test
     public void should_pass_if_maps_are_equal() {
-        Map<String, Integer> actual = new HashMap<String, Integer>();
-        actual.put("a", 1);
-        actual.put("b", 2);
-        actual.put("c", 3);
-        Map<String, Integer> expected = new HashMap<String, Integer>();
-        expected.put("a", 1);
-        expected.put("b", 2);
-        expected.put("c", 3);
+        Map<String, String> expected = new HashMap<String, String>();
+        expected.put("a", "1");
+        expected.put("b", "2");
+        expected.put("c", "3");
         maps.assertEqual(someInfo(), actual, expected, StandardComparisonStrategy.instance());
     }
 
     @Test
-    public void should_fail_if_maps_are_not_equal() {
+    public void should_fail_if_map_entry_values_are_not_equal() {
         AssertionInfo info = someInfo();
-        Map<String, Integer> expected = new HashMap<String, Integer>();
-        expected.put("a", 1);
-        expected.put("b", 2);
-        expected.put("c", 3);
-        Map<String, Integer> actual = new HashMap<String, Integer>();
-        actual.put("a", 1);
-        actual.put("b", 2);
-        actual.put("c", 4);
+        Map<String, String> expected = new HashMap<String, String>();
+        expected.put("a", "1");
+        expected.put("b", "2");
+        expected.put("c", "4");
+        try {
+            maps.assertEqual(someInfo(), actual, expected, StandardComparisonStrategy.instance());
+        } catch (AssertionError e) {
+            verify(failures).failure(info, shouldBeEqual(actual, expected, new MapRepresentation()));
+            return;
+        }
+        failBecauseExpectedAssertionErrorWasNotThrown();
+    }
+
+    @Test
+    public void should_fail_if_map_entry_keys_are_not_equal() {
+        AssertionInfo info = someInfo();
+        Map<String, String> expected = new HashMap<String, String>();
+        expected.put("a", "1");
+        expected.put("b", "2");
+        expected.put("d", "3");
         try {
             maps.assertEqual(someInfo(), actual, expected, StandardComparisonStrategy.instance());
         } catch (AssertionError e) {
