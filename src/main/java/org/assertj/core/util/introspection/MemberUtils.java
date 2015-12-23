fc33afa470e3d30dd6b19e7faff1bd9ee85dad90
Pascal Schumacher <pascalschumacher@gmx.net>
2015-12-23 10:29:16 +0100

    clean-up: remove @since, and @version javadoc tags, remove duplicated apache license header

--- src/main/java/org/assertj/core/util/introspection/MemberUtils.java
+++ src/main/java/org/assertj/core/util/introspection/MemberUtils.java
@@ -12,23 +12,6 @@
  */
 package org.assertj.core.util.introspection;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
@@ -39,9 +22,6 @@ import java.lang.reflect.Modifier;
  * Contains common code for working with Methods/Constructors, extracted and
  * refactored from <code>MethodUtils</code> when it was imported from Commons
  * BeanUtils.
- *
- * @since 2.5
- * @version $Id: MemberUtils.java 1143537 2011-07-06 19:30:22Z joehni $
  */
 abstract class MemberUtils {
 
