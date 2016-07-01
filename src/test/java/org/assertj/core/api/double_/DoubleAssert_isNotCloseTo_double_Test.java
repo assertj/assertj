307a7fd9c195f1d7df7f63a6f81586ecbdf6a7e9
chris.arnott@softwire.com <chris.arnott@softwire.com>
2016-07-01 09:31:49 +0100

    664: Add api tests.

---
+++ src/test/java/org/assertj/core/api/double_/DoubleAssert_isNotCloseTo_double_Test.java
@@ -0,0 +1,41 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2016 the original author or authors.
+ */
+package org.assertj.core.api.double_;
+
+import org.assertj.core.api.DoubleAssert;
+import org.assertj.core.api.DoubleAssertBaseTest;
+import org.assertj.core.data.Offset;
+
+import static org.assertj.core.data.Offset.offset;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Tests for <code>{@link DoubleAssert#isNotCloseTo(Double, Offset)}</code>.
+ *
+ * @author Chris Arnott
+ */
+public class DoubleAssert_isNotCloseTo_double_Test extends DoubleAssertBaseTest {
+
+  private final Offset<Double> offset = offset(5.0);
+  private final Double value = 8.0;
+
+  @Override
+  protected DoubleAssert invoke_api_method() {
+    return assertions.isNotCloseTo(value, offset);
+  }
+
+  @Override
+  protected void verify_internal_effects() {
+    verify(doubles).assertIsNotCloseTo(getInfo(assertions), getActual(assertions), value, offset);
+  }
+}
---
+++ src/test/java/org/assertj/core/api/integer_/IntegerAssert_isNotCloseTo_integer_Test.java
@@ -0,0 +1,41 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2016 the original author or authors.
+ */
+package org.assertj.core.api.integer_;
+
+import org.assertj.core.api.IntegerAssert;
+import org.assertj.core.api.IntegerAssertBaseTest;
+import org.assertj.core.data.Offset;
+
+import static org.assertj.core.data.Offset.offset;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Tests for <code>{@link IntegerAssert#isNotCloseTo(Integer, Offset)}</code>.
+ *
+ * @author Chris Arnott
+ */
+public class IntegerAssert_isNotCloseTo_integer_Test extends IntegerAssertBaseTest {
+
+  private final Offset<Integer> offset = offset(5);
+  private final Integer value = 8;
+
+  @Override
+  protected IntegerAssert invoke_api_method() {
+    return assertions.isNotCloseTo(value, offset);
+  }
+
+  @Override
+  protected void verify_internal_effects() {
+    verify(integers).assertIsNotCloseTo(getInfo(assertions), getActual(assertions), value, offset);
+  }
+}
