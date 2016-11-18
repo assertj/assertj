6dbd94db6295d7dc336e8a028eeb3edca9bd31ed
Gaël Lhez <gael.lhez@gmail.com>
2016-11-18 21:55:41 +0100

    Added shortcut for assertThatExceptionOfType to exception:
      NullPointerException
      IllegalArgumentException
      InvalidStateException
      IOException
    
    Signed-off-by: Gaël Lhez <gael.lhez@gmail.com>

--- src/test/java/org/assertj/core/api/Assertions_assertThatExceptionOfType_Test.java
+++ src/test/java/org/assertj/core/api/Assertions_assertThatExceptionOfType_Test.java
@@ -14,38 +14,80 @@ package org.assertj.core.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIOException;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Supplier;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class Assertions_assertThatExceptionOfType_Test {
+
+  private final Supplier<ThrowableTypeAssert<? extends Exception>> assertionGenerator;
+  private final Class<? extends Exception> exceptionType;
+  private final Supplier<? extends Exception> exceptionGenerator;
+
+  public Assertions_assertThatExceptionOfType_Test(Supplier<ThrowableTypeAssert<? extends Exception>> assertionGenerator,
+                                                   Class<? extends Exception> exceptionType,
+                                                   Supplier<? extends Exception> exceptionBuilder) {
+    this.assertionGenerator = assertionGenerator;
+    this.exceptionType = exceptionType;
+    this.exceptionGenerator = exceptionBuilder;
+  }
+
+  private static <E> Supplier<E> s(Supplier<E> supplier) {
+    return supplier;
+  }
+
+  @Parameters
+  public static Iterable<? extends Object> data() {
+    return Arrays.asList(
+      new Object[] {
+        s(() -> assertThatExceptionOfType(UnsupportedOperationException.class)),
+        UnsupportedOperationException.class,
+        s(() -> new UnsupportedOperationException())  
+      },
+      new Object[] {s(() -> assertThatNullPointerException()), NullPointerException.class, s(() -> new NullPointerException("value"))},
+      new Object[] {s(() -> assertThatIllegalArgumentException()), IllegalArgumentException.class, s(() -> new IllegalArgumentException("arg"))},
+      new Object[] {s(() -> assertThatIllegalStateException()), IllegalStateException.class, s(() -> new IllegalStateException("state"))},
+      new Object[] {s(() -> assertThatIOException()), IOException.class, s(() -> new IOException("io"))}      
+    );
+  }
 
   @Test
   public void should_create_ExpectThrowableAssert() {
-    ThrowableTypeAssert<NullPointerException> assertions = assertThatExceptionOfType(NullPointerException.class);
+    ThrowableTypeAssert<? extends Exception> assertions = assertionGenerator.get();
     assertThat(assertions).isNotNull();
   }
 
   @Test
   public void should_pass_ExceptionType() {
-    Class<NullPointerException> exceptionType = NullPointerException.class;
-    ThrowableTypeAssert<NullPointerException> assertions = assertThatExceptionOfType(exceptionType);
+    ThrowableTypeAssert<? extends Exception> assertions = assertionGenerator.get();
     assertThat(assertions.expectedThrowableType).isSameAs(exceptionType);
   }
 
   @Test
   public void should_create_ChainedThrowableAssert() {
-    ThrowableAssertAlternative<NullPointerException> assertions = assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> {
-      throw new NullPointerException("boom!");
+    ThrowableAssertAlternative<? extends Exception> assertions = assertionGenerator.get().isThrownBy(() -> {
+      throw exceptionGenerator.get();
     });
     assertThat(assertions).isNotNull();
   }
 
   @Test
   public void should_pass_thrown_exception_to_ChainedThrowableAssert() {
-    NullPointerException nullPointerException = new NullPointerException("boom!");
-    ThrowableAssertAlternative<NullPointerException> assertions = assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> {
-      throw nullPointerException;
+    Exception exception = exceptionGenerator.get();
+    ThrowableAssertAlternative<? extends Exception> assertions = assertionGenerator.get().isThrownBy(() -> {
+      throw exception;
     });
-    assertThat(assertions.actual).isSameAs(nullPointerException);
+    assertThat(assertions.actual).isSameAs(exception);
   }
 }
