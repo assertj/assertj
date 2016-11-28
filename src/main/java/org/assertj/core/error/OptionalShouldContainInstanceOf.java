f7642e6e470dcbdd62873a658340ddbdace5410b
Eirik Lygre <eirik.lygre@acando.no>
2016-11-28 12:24:47 +0100

    Add detail level from AbstractOptionalAssert.containsInstanceOf().

--- src/main/java/org/assertj/core/error/OptionalShouldContainInstanceOf.java
+++ src/main/java/org/assertj/core/error/OptionalShouldContainInstanceOf.java
@@ -1,16 +1,18 @@
 /**
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
+ * <p>
  * Copyright 2012-2016 the original author or authors.
  */
 package org.assertj.core.error;
+
+import java.util.Optional;
 
 import static java.lang.String.format;
 
@@ -21,18 +23,25 @@ import static java.lang.String.format;
  */
 public class OptionalShouldContainInstanceOf extends BasicErrorMessageFactory {
 
-  private OptionalShouldContainInstanceOf(Object optional, Class<?> clazz) {
-    super(format("%nExpecting %s to contain a value of type %s.", optional.getClass().getSimpleName(), clazz.getName()));
+  private OptionalShouldContainInstanceOf(String message) {
+    super(message);
   }
 
   /**
    * Indicates that a value should be present in an empty {@link java.util.Optional}.
    *
-   * @param optional Optional to be checked.
+   * @param value Optional to be checked.
    * @return an error message factory.
    * @throws java.lang.NullPointerException if optional is null.
    */
-  public static OptionalShouldContainInstanceOf shouldContainInstanceOf(Object optional, Class<?> clazz) {
-    return new OptionalShouldContainInstanceOf(optional, clazz);
+  public static OptionalShouldContainInstanceOf shouldContainInstanceOf(Object value, Class<?> clazz) {
+    Optional optional = (Optional) value;
+    if (optional.isPresent()) {
+      return new OptionalShouldContainInstanceOf(format("%nExpecting:%n <%s>%nto contain a value that is an instance of:%n <%s>%nbut did contain instance of:%n <%s>",
+        optional.getClass().getSimpleName(), clazz.getName(), optional.get().getClass().getName()));
+    } else {
+      return new OptionalShouldContainInstanceOf(format("%nExpecting:%n <%s>%nto contain a value that is an instance of:%n <%s>%nbut was empty",
+        optional.getClass().getSimpleName(), clazz.getName()));
+    }
   }
 }
