adf26a918a3c99e22db1e53109d0974e0633b80a
Pascal Schumacher <pascalschumacher@gmx.net>
2016-12-11 23:13:25 +0100

    use ExpectedException#expectIllegalArgumentException

--- src/test/java/org/assertj/core/api/objectarray/ObjectArrayAssert_extractingResultOf_Test.java
+++ src/test/java/org/assertj/core/api/objectarray/ObjectArrayAssert_extractingResultOf_Test.java
@@ -68,8 +68,7 @@ public class ObjectArrayAssert_extractingResultOf_Test {
 
   @Test
   public void should_throw_error_if_no_method_with_given_name_can_be_extracted() {
-    thrown.expect(IllegalArgumentException.class);
-    thrown.expectMessage("Can't find method 'unknown' in class FluentJedi.class. Make sure public method exists and accepts no arguments!");
+    thrown.expectIllegalArgumentException("Can't find method 'unknown' in class FluentJedi.class. Make sure public method exists and accepts no arguments!");
     assertThat(jedis).extractingResultOf("unknown");
   }
 
