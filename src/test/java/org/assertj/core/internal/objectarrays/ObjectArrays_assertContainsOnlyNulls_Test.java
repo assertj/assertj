bd065f89c4602579906226cc03513e3ef4460eb9
epeee <epeee@users.noreply.github.com>
2017-10-23 21:18:38 +0200

    make use of assertj-parent-pom v2.1.7 and generate license headers (SLASHSTAR_STYLE)

--- src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertContainsOnlyNulls_Test.java
+++ src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertContainsOnlyNulls_Test.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  *
@@ -10,7 +10,6 @@
  *
  * Copyright 2012-2017 the original author or authors.
  */
-
 package org.assertj.core.internal.objectarrays;
 
 import static org.assertj.core.error.ShouldContainOnlyNulls.shouldContainOnlyNulls;
