c7f1f282d0f9b610891415150a7a5869ba055ef9
Joel Costigliola <joel.costigliola@gmail.com>
2017-11-11 18:50:13 +1300

    Fix warnings

--- src/test/java/org/assertj/core/api/charsequence/CharSequenceAssert_isJavaBlank_Test.java
+++ src/test/java/org/assertj/core/api/charsequence/CharSequenceAssert_isJavaBlank_Test.java
@@ -12,10 +12,10 @@
  */
 package org.assertj.core.api.charsequence;
 
+import static org.mockito.Mockito.verify;
+
 import org.assertj.core.api.CharSequenceAssert;
 import org.assertj.core.api.CharSequenceAssertBaseTest;
-
-import static org.mockito.Mockito.verify;
 
 /**
  * Tests for <code>{@link CharSequenceAssert#isJavaBlank()}</code>.
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.verify;
  */
 public class CharSequenceAssert_isJavaBlank_Test extends CharSequenceAssertBaseTest {
 
+  @SuppressWarnings("deprecation")
   @Override
   protected CharSequenceAssert invoke_api_method() {
     return assertions.isJavaBlank();
