bd359b62a681cbf77827d906644093c6c8824d5a
Joel Costigliola <joel.costigliola@gmail.com>
2017-12-17 19:15:45 +1300

    Document String assertions containsSequence breaking changes.

--- src/main/java/org/assertj/core/api/AbstractCharSequenceAssert.java
+++ src/main/java/org/assertj/core/api/AbstractCharSequenceAssert.java
@@ -529,6 +529,9 @@ public abstract class AbstractCharSequenceAssert<SELF extends AbstractCharSequen
   /**
    * Verifies that the actual {@code CharSequence} contains the given sequence of values <b>in the given order without any other values between them</b>.
    * <p>
+   * <b>Breaking change since 2.9.0</b>: in previous versions this assertion behaved like {@link #containsSubsequence(Iterable) containsSubsequence} 
+   * and allowed other values between the sequence values.  
+   * <p>
    * Example:
    * <pre><code class='java'> String book = &quot;{ 'title':'A Game of Thrones', 'author':'George Martin'}&quot;;
    *
@@ -557,6 +560,9 @@ public abstract class AbstractCharSequenceAssert<SELF extends AbstractCharSequen
    * Verifies that the actual {@code CharSequence} contains all the values of the given Iterable <b>in the Iterable
    * iteration order without any other values between them</b>.
    * <p>
+   * <b>Breaking change since 2.9.0</b>: in previous versions this assertion behaved like {@link #containsSubsequence(CharSequence...) containsSubsequence} 
+   * and allowed other values between the sequence values.  
+   * <p>
    * Example:
    * <pre><code class='java'> String book = &quot;{ 'title':'A Game of Thrones', 'author':'George Martin'}&quot;;
    *
@@ -582,8 +588,8 @@ public abstract class AbstractCharSequenceAssert<SELF extends AbstractCharSequen
   }
 
   /**
-   * Verifies that the actual {@code CharSequence} contains all the given values <b>in the given order
-   * (possibly with other values between them)</b>.
+   * Verifies that the actual {@code CharSequence} contains all the given values <b>in the given order,
+   * possibly with other values between them</b>.
    * <p>
    * Example:
    * <pre><code class='java'> String book = &quot;{ 'title':'A Game of Thrones', 'author':'George Martin'}&quot;;
