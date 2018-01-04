cc178abc5871bc34525113550373ffd90e303034
Joel Costigliola <joel.costigliola@gmail.com>
2018-01-04 16:15:48 +1300

    Update javadoc licence section

--- src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertContains_Test.java
+++ src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertContains_Test.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  *
@@ -8,7 +8,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  */
 package org.assertj.core.internal.objectarrays;
 
