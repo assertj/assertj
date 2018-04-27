b6d684700712f956792babc336ef8f636cb2b4b7
Joel Costigliola <joel.costigliola@gmail.com>
2018-04-27 23:30:26 +1200

    Add Streams class to easily convert an Iterable to a Stream.

---
+++ src/main/java/org/assertj/core/util/Streams.java
@@ -0,0 +1,24 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2018 the original author or authors.
+ */
+package org.assertj.core.util;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class Streams {
+
+  public static <T> Stream<T> stream(Iterable<T> actual) {
+    return StreamSupport.stream(actual.spliterator(), false);
+
+  }
+}
