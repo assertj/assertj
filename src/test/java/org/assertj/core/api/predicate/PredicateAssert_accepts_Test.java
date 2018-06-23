9f2318704d0839a172a9c6ef757b8c66664c4868
Lars Hvile <lars@8-bm.com>
2018-06-23 19:46:34 +0200

    Fix if-else logic in PredicateAssert
    Both accepts() & rejects() invoked the predicate
    twice if given a single value because of broken
    if-else logic.

--- src/test/java/org/assertj/core/api/predicate/PredicateAssert_accepts_Test.java
+++ src/test/java/org/assertj/core/api/predicate/PredicateAssert_accepts_Test.java
@@ -17,7 +17,11 @@ import static org.assertj.core.error.ElementsShouldMatch.elementsShouldMatch;
 import static org.assertj.core.error.ShouldAccept.shouldAccept;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
 import static org.assertj.core.util.Lists.newArrayList;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.function.Predicate;
 
@@ -77,6 +81,16 @@ public class PredicateAssert_accepts_Test extends PredicateAssertBaseTest {
     Predicate<String> predicate = val -> val.equals("something");
 
     assertThat(predicate).accepts("something");
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void should_pass_and_only_invoke_predicate_once_for_single_value() {
+    Predicate<Object> predicate = mock(Predicate.class);
+    when(predicate.test(any())).thenReturn(true);
+
+    assertThat(predicate).accepts("something");
+    verify(predicate, times(1)).test("something");
   }
 
 
