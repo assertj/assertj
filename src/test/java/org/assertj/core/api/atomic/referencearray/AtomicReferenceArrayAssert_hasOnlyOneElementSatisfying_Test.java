f93172714fee98c583b8af9771b3be3ecf0bbfb3
Vladimir Chernikov <j-chernikow@ya.ru>
2018-08-26 13:29:10 +0300

    Used list in place of newArrayList

--- src/test/java/org/assertj/core/api/atomic/referencearray/AtomicReferenceArrayAssert_hasOnlyOneElementSatisfying_Test.java
+++ src/test/java/org/assertj/core/api/atomic/referencearray/AtomicReferenceArrayAssert_hasOnlyOneElementSatisfying_Test.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2018 the original author or authors.
+ */
 package org.assertj.core.api.atomic.referencearray;
 
 import org.assertj.core.api.AtomicReferenceArrayAssert;
@@ -7,7 +19,7 @@ import org.mockito.Mock;
 
 import java.util.function.Consumer;
 
-import static org.assertj.core.util.Lists.newArrayList;
+import static org.assertj.core.util.Lists.list;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -33,6 +45,6 @@ public class AtomicReferenceArrayAssert_hasOnlyOneElementSatisfying_Test extends
 
   @Override
   protected void verify_internal_effects() {
-    verify(iterables).assertHasOnlyOneElementSatisfying(info(), newArrayList(internalArray()), consumer);
+    verify(iterables).assertHasOnlyOneElementSatisfying(info(), list(internalArray()), consumer);
   }
 }
