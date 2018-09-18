cb7821bb7093b4faa194c03f9722797b61327c3b
sparsick <sparsick@web.de>
2018-09-18 09:24:45 +0200

    Add hasSizeGreaterThan, hasSizeLessThanOrEqualTo, hasSizeGreaterThanOrEqualTo and hasSizeGreaterThan to AbstractCharSequenceAssert Fixes #1319 Authors Georg Berky and Sandra Parsick

--- src/main/java/org/assertj/core/internal/Strings.java
+++ src/main/java/org/assertj/core/internal/Strings.java
@@ -30,16 +30,20 @@ import static org.assertj.core.error.ShouldBeLowerCase.shouldBeLowerCase;
 import static org.assertj.core.error.ShouldBeNullOrEmpty.shouldBeNullOrEmpty;
 import static org.assertj.core.error.ShouldBeSubstring.shouldBeSubstring;
 import static org.assertj.core.error.ShouldBeUpperCase.shouldBeUpperCase;
-import static org.assertj.core.error.ShouldContainOneOrMoreWhitespaces.shouldContainOneOrMoreWhitespaces;
 import static org.assertj.core.error.ShouldContainCharSequence.shouldContain;
 import static org.assertj.core.error.ShouldContainCharSequence.shouldContainIgnoringCase;
 import static org.assertj.core.error.ShouldContainCharSequenceOnlyOnce.shouldContainOnlyOnce;
+import static org.assertj.core.error.ShouldContainOneOrMoreWhitespaces.shouldContainOneOrMoreWhitespaces;
 import static org.assertj.core.error.ShouldContainOnlyDigits.shouldContainOnlyDigits;
 import static org.assertj.core.error.ShouldContainOnlyWhitespaces.shouldContainOnlyWhitespaces;
 import static org.assertj.core.error.ShouldContainPattern.shouldContainPattern;
 import static org.assertj.core.error.ShouldContainSequenceOfCharSequence.shouldContainSequence;
 import static org.assertj.core.error.ShouldContainSubsequenceOfCharSequence.shouldContainSubsequence;
 import static org.assertj.core.error.ShouldEndWith.shouldEndWith;
+import static org.assertj.core.error.ShouldHaveSizeGreaterThan.shouldHaveSizeGreaterThan;
+import static org.assertj.core.error.ShouldHaveSizeGreaterThanOrEqualTo.shouldHaveSizeGreaterThanOrEqualTo;
+import static org.assertj.core.error.ShouldHaveSizeLessThan.shouldHaveSizeLessThan;
+import static org.assertj.core.error.ShouldHaveSizeLessThanOrEqualTo.shouldHaveSizeLessThanOrEqualTo;
 import static org.assertj.core.error.ShouldMatchPattern.shouldMatch;
 import static org.assertj.core.error.ShouldNotBeBlank.shouldNotBeBlank;
 import static org.assertj.core.error.ShouldNotBeEmpty.shouldNotBeEmpty;
@@ -295,6 +299,74 @@ public class Strings {
   public void assertHasSize(AssertionInfo info, CharSequence actual, int expectedSize) {
     assertNotNull(info, actual);
     checkSizes(actual, actual.length(), expectedSize, info);
+  }
+
+  /**
+   * Asserts that the size of the given {@code CharSequence} is less than the expected size
+   *
+   * @param info contains information about the assertion.
+   * @param actual the given {@code CharSequence}.
+   * @param expectedMaxSizeExcluded the expected max size of {@code actual}
+   * @throws AssertionError if the given {@code CharSequence} is {@code null}.
+   * @throws AssertionError if the size of the given {@code CharSequence} is equal to or greater than the expected max size
+   */
+  public void assertHasSizeLessThan(AssertionInfo info, CharSequence actual, int expectedMaxSizeExcluded) {
+    assertNotNull(info, actual);
+
+    if (actual.length() >= expectedMaxSizeExcluded) {
+      throw failures.failure(info, shouldHaveSizeLessThan(actual, actual.length(), expectedMaxSizeExcluded));
+    }
+  }
+
+  /**
+   * Asserts that the size of the given {@code CharSequence} is less than or equal to the expected size
+   *
+   * @param info contains information about the assertion.
+   * @param actual the given {@code CharSequence}.
+   * @param expectedMaxSizeIncluded the expected max size of {@code actual}
+   * @throws AssertionError if the given {@code CharSequence} is {@code null}.
+   * @throws AssertionError if the size of the given {@code CharSequence} is greater than the expected max size
+   */
+  public void assertHasSizeLessThanOrEqualTo(AssertionInfo info, CharSequence actual, int expectedMaxSizeIncluded) {
+    assertNotNull(info, actual);
+
+    if (actual.length() > expectedMaxSizeIncluded) {
+      throw failures.failure(info, shouldHaveSizeLessThanOrEqualTo(actual, actual.length(), expectedMaxSizeIncluded));
+    }
+  }
+
+  /**
+   * Asserts that the size of the given {@code CharSequence} is greater than the expected size
+   *
+   * @param info contains information about the assertion.
+   * @param actual the given {@code CharSequence}.
+   * @param expectedMinSizeExcluded the expected min size of {@code actual}
+   * @throws AssertionError if the given {@code CharSequence} is {@code null}.
+   * @throws AssertionError if the size of the given {@code CharSequence} is equal to or less than the expected max size
+   */
+  public void assertHasSizeGreaterThan(AssertionInfo info, CharSequence actual, int expectedMinSizeExcluded) {
+    assertNotNull(info, actual);
+
+    if (actual.length() <= expectedMinSizeExcluded) {
+      throw failures.failure(info, shouldHaveSizeGreaterThan(actual, actual.length(), expectedMinSizeExcluded));
+    }
+  }
+
+  /**
+   * Asserts that the size of the given {@code CharSequence} is greater than or equal to the expected size
+   *
+   * @param info contains information about the assertion.
+   * @param actual the given {@code CharSequence}.
+   * @param expectedMinSizeIncluded the expected min size of {@code actual}
+   * @throws AssertionError if the given {@code CharSequence} is {@code null}.
+   * @throws AssertionError if the size of the given {@code CharSequence} is greater than the expected max size
+   */
+  public void assertHasSizeGreaterThanOrEqualTo(AssertionInfo info, CharSequence actual, int expectedMinSizeIncluded) {
+    assertNotNull(info, actual);
+
+    if (actual.length() < expectedMinSizeIncluded) {
+      throw failures.failure(info, shouldHaveSizeGreaterThanOrEqualTo(actual, actual.length(), expectedMinSizeIncluded));
+    }
   }
 
   /**
---
+++ src/test/java/org/assertj/core/internal/strings/Strings_assertHasSizeLessThanOrEqualTo_Test.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2018 the original author or authors.
+ */
+package org.assertj.core.internal.strings;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.error.ShouldHaveSizeLessThanOrEqualTo.shouldHaveSizeLessThanOrEqualTo;
+import static org.assertj.core.test.TestData.someInfo;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+
+import org.assertj.core.api.AssertionInfo;
+import org.assertj.core.internal.Strings;
+import org.assertj.core.internal.StringsBaseTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for <code>{@link Strings#assertHasSizeLessThanOrEqualTo(AssertionInfo, CharSequence, int)} </code>.
+ *
+ * @author Sandra Parsick
+ * @author Georg Berky
+ */
+public class Strings_assertHasSizeLessThanOrEqualTo_Test extends StringsBaseTest {
+
+  @Test
+  public void should_fail_if_actual_is_null() {
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> strings.assertHasSizeLessThanOrEqualTo(someInfo(), null, 3))
+                                                   .withMessage(actualIsNull());
+  }
+
+  @Test
+  public void should_fail_if_size_of_actual_is_greater_than_expected_size() {
+    AssertionInfo info = someInfo();
+    String actual = "Han";
+
+    String errorMessage = shouldHaveSizeLessThanOrEqualTo(actual, actual.length(), 2).create();
+    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> strings.assertHasSizeLessThanOrEqualTo(info, actual, 2))
+                                                   .withMessage(errorMessage);
+  }
+
+  @Test
+  public void should_pass_if_size_of_actual_is_equal_to_expected_size() {
+    strings.assertHasSizeLessThanOrEqualTo(someInfo(), "Han", 3);
+  }
+
+  @Test
+  public void should_pass_if_size_of_actual_is_less_than_expected_size() {
+    strings.assertHasSizeLessThanOrEqualTo(someInfo(), "Han", 4);
+  }
+}
