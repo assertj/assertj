2cba38fe2de5a398afde3f634d6c75c128edb275
Erhard Pointl <epeee@users.noreply.github.com>
2019-02-25 21:40:16 +0100

    fix some more javadoc errors (#1447)

--- src/test/java/org/assertj/core/api/iterable/IterableAssert_areAtMost_Test.java
+++ src/test/java/org/assertj/core/api/iterable/IterableAssert_areAtMost_Test.java
@@ -23,7 +23,7 @@ import org.assertj.core.api.TestCondition;
 import org.junit.jupiter.api.BeforeAll;
 
 /**
- * Tests for <code>{@link AbstractIterableAssert#areAtMost(Condition, int)}</code>.
+ * Tests for <code>{@link AbstractIterableAssert#areAtMost(int, Condition)}</code>.
  * 
  * @author Nicolas François
  */
