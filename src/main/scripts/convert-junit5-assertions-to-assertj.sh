83dc122e7dcd95ebd8bd70d51c0d0b4b72c8818d
Nils Winkler <nils@nilswinkler.com>
2019-03-19 02:18:52 +0100

    Refactor macOS Conversion Script (#1456)
    * Whitespace and some ShellCheck fixes
    
    * Refactoring the replace functionality
    
    - sed options are set for GNU sed and BSD sed (macOS)
    - Moved file loop and replacement into a function to avoid code repetition
    - No more temp files!
    
    * Consolidated first convert script into a single version that works on
    both Linux and macOS (GNU and BSD sed).
    
    * Updated other scripts using the same means
    
    Also removed some leftover "$file" parameters from the previous
    refactoring step.
    
    * Renamed script file

--- src/main/scripts/convert-junit5-assertions-to-assertj.sh
+++ src/main/scripts/convert-junit5-assertions-to-assertj.sh
@@ -3,12 +3,17 @@
 function usage() {
   echo
   echo "NAME"
-  echo "convert-junit-assertions-to-assertj.sh - Converts most of JUnit assertions to AssertJ assertions"
+  echo "convert-junit5-assertions-to-assertj.sh - Converts most of JUnit 5 assertions to AssertJ assertions"
+  echo
+  echo "AUTHOR"
+  echo "this script is based on JUnit 4 script found at http://joel-costigliola.github.io/assertj/assertj-core-converting-junit-assertions-to-assertj.html
+  The changes in regexps are mostly due to the fact the order arguments changed
+  in JUnit 5 (i.e. message moved to the last position). Kudos to whoever wrote the original script!"
   echo
   echo "It is difficult to convert ALL JUnit assertions (e.g. the ones that are multiline) but it should work for most of them."
   echo
   echo "SYNOPSIS"
-  echo "convert-junit-assertions-to-assertj.sh [Pattern]"
+  echo "convert-junit5-assertions-to-assertj.sh [Pattern]"
   echo
   echo "OPTIONS"
   echo " -h --help    this help"
@@ -16,7 +21,7 @@ function usage() {
   echo "              don't forget to enclose your pattern with double quotes \"\" to avoid pattern to be expanded by your shell prematurely"
   echo
   echo "EXAMPLE"
-  echo " convert-junit-assertions-to-assertj.sh \"*IT.java\""
+  echo " convert-junit5-assertions-to-assertj.sh \"*IT.java\""
   exit 0
 }
 
@@ -45,59 +50,63 @@ function replace-in-all-files() {
 }
 
 echo ''
-echo "Converting JUnit assertions to AssertJ assertions in files matching pattern : $FILES_PATTERN"
+echo "Converting JUnit 5 assertions to AssertJ assertions in files matching pattern : $FILES_PATTERN"
 echo ''
 echo ' 1 - Replacing : assertEquals(0, myList.size()) ................. by : assertThat(myList).isEmpty()'
-replace-in-all-files 's/assertEquals(\(\".*\"\),[[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\2).as(\1).isEmpty()/g'
+replace-in-all-files 's/assertEquals([[:blank:]]*0,[[:blank:]]*\(.*\).size(),[[:blank:]]*\(\".*\"\))/assertThat(\1).as(\2).isEmpty()/g'
 replace-in-all-files 's/assertEquals([[:blank:]]*0,[[:blank:]]*\(.*\).size())/assertThat(\1).isEmpty()/g'
 
 echo ' 2 - Replacing : assertEquals(expectedSize, myList.size()) ...... by : assertThat(myList).hasSize(expectedSize)'
-replace-in-all-files 's/assertEquals(\(\".*\"\),[[:blank:]]*\([[:digit:]]*\),[[:blank:]]*\(.*\).size())/assertThat(\3).as(\1).hasSize(\2)/g'
+replace-in-all-files 's/assertEquals([[:blank:]]*\([[:digit:]]*\),[[:blank:]]*\(.*\).size(),[[:blank:]]*\(\".*\"\))/assertThat(\2).as(\3).hasSize(\1)/g'
 replace-in-all-files 's/assertEquals([[:blank:]]*\([[:digit:]]*\),[[:blank:]]*\(.*\).size())/assertThat(\2).hasSize(\1)/g'
 
 echo ' 3 - Replacing : assertEquals(expectedDouble, actual, delta) .... by : assertThat(actual).isCloseTo(expectedDouble, within(delta))'
-replace-in-all-files 's/assertEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isCloseTo(\2, within(\4))/g'
+replace-in-all-files 's/assertEquals(\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\2).as(\4).isCloseTo(\1, within(\3))/g'
 # must be done before assertEquals("description", expected, actual) -> assertThat(actual).as("description").isEqualTo(expected)
 # will only replace triplets without double quote to avoid matching : assertEquals("description", expected, actual)
 replace-in-all-files 's/assertEquals([[:blank:]]*\([^"]*\),[[:blank:]]*\([^"]*\),[[:blank:]]*\([^"]*\))/assertThat(\2).isCloseTo(\1, within(\3))/g'
 
 echo ' 4 - Replacing : assertEquals(expected, actual) ................. by : assertThat(actual).isEqualTo(expected)'
-replace-in-all-files 's/assertEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isEqualTo(\2)/g'
+replace-in-all-files 's/assertEquals(\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\2).as(\3).isEqualTo(\1)/g'
 replace-in-all-files 's/assertEquals([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isEqualTo(\1)/g'
 
+
+echo ' 4B - Replacing : assertNotEquals(expected, actual) ................. by : assertThat(actual).isNotEqualTo(expected)'
+replace-in-all-files 's/assertNotEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).as(\3).isNotEqualTo(\1)/g'
+replace-in-all-files 's/assertNotEquals([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isNotEqualTo(\1)/g'
+
 echo ' 5 - Replacing : assertArrayEquals(expectedArray, actual) ....... by : assertThat(actual).isEqualTo(expectedArray)'
-replace-in-all-files 's/assertArrayEquals(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isEqualTo(\2)/g'
+replace-in-all-files 's/assertArrayEquals(\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\2).as(\3).isEqualTo(\1)/g'
 replace-in-all-files 's/assertArrayEquals([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isEqualTo(\1)/g'
 
 echo ' 6 - Replacing : assertNull(actual) ............................. by : assertThat(actual).isNull()'
-replace-in-all-files 's/assertNull(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isNull()/g'
+replace-in-all-files 's/assertNull(\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\1).as(\2).isNull()/g'
 replace-in-all-files 's/assertNull([[:blank:]]*\(.*\))/assertThat(\1).isNull()/g'
 
 echo ' 7 - Replacing : assertNotNull(actual) .......................... by : assertThat(actual).isNotNull()'
-replace-in-all-files 's/assertNotNull(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isNotNull()/g'
+replace-in-all-files 's/assertNotNull(\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\1).as(\2).isNotNull()/g'
 replace-in-all-files 's/assertNotNull([[:blank:]]*\(.*\))/assertThat(\1).isNotNull()/g'
 
 echo ' 8 - Replacing : assertTrue(logicalCondition) ................... by : assertThat(logicalCondition).isTrue()'
-replace-in-all-files 's/assertTrue(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isTrue()/g'
+replace-in-all-files 's/assertTrue(\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\1).as(\2).isTrue()/g'
 replace-in-all-files 's/assertTrue([[:blank:]]*\(.*\))/assertThat(\1).isTrue()/g'
 
 echo ' 9 - Replacing : assertFalse(logicalCondition) .................. by : assertThat(logicalCondition).isFalse()'
-replace-in-all-files 's/assertFalse(\(\".*\"\),[[:blank:]]*\(.*\))/assertThat(\2).as(\1).isFalse()/g'
+replace-in-all-files 's/assertFalse(\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\1).as(\2).isFalse()/g'
 replace-in-all-files 's/assertFalse([[:blank:]]*\(.*\))/assertThat(\1).isFalse()/g'
 
 echo '10 - Replacing : assertSame(expected, actual) ................... by : assertThat(actual).isSameAs(expected)'
-replace-in-all-files 's/assertSame(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isSameAs(\2)/g'
+replace-in-all-files 's/assertSame(\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\2).as(\3).isSameAs(\1)/g'
 replace-in-all-files 's/assertSame([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isSameAs(\1)/g'
 
 echo '11 - Replacing : assertNotSame(expected, actual) ................ by : assertThat(actual).isNotSameAs(expected)'
-replace-in-all-files 's/assertNotSame(\(\".*\"\),[[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\3).as(\1).isNotSameAs(\2)/g'
+replace-in-all-files 's/assertNotSame(\(.*\),[[:blank:]]*\(.*\),[[:blank:]]*\(\".*\"\))/assertThat(\2).as(\3).isNotSameAs(\1)/g'
 replace-in-all-files 's/assertNotSame([[:blank:]]*\(.*\),[[:blank:]]*\(.*\))/assertThat(\2).isNotSameAs(\1)/g'
 
 echo ''
-echo '12 - Replacing JUnit static imports by AssertJ ones, at this point you will probably need to :'
+echo '12 - Replacing JUnit 5 static imports by AssertJ ones, at this point you will probably need to :'
 echo '12 --- optimize imports with your IDE to remove unused imports'
-echo '12 --- add "import static org.assertj.core.api.Assertions.within;" if you were using JUnit number assertions with deltas'
-replace-in-all-files 's/import static org.junit.Assert.assertEquals;/import static org.assertj.core.api.Assertions.assertThat;/g'
-replace-in-all-files 's/import static org.junit.Assert.fail;/import static org.assertj.core.api.Assertions.fail;/g'
-replace-in-all-files 's/import static org.junit.Assert.\*;/import static org.assertj.core.api.Assertions.*;/g'
+echo '12 --- add "import static org.assertj.core.api.Assertions.within;" if you were using JUnit 5 number assertions with deltas'
+replace-in-all-files 's/import static org.junit.jupiter.api.Assertions.fail;/import static org.assertj.core.api.Assertions.fail;/g'
+replace-in-all-files 's/import static org.junit.jupiter.api.Assertions.\*;/import static org.assertj.core.api.Assertions.*;/g'
 echo ''
