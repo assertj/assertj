ced2937f8e1647ab7893adb9fc766e64b1ab20a9
Stefano Cordio <stefano.cordio@gmail.com>
2019-06-10 19:55:46 +0200

    Split InstanceOfAssertFactories sync test into fields and methods tests

--- src/test/java/org/assertj/core/api/Assertions_sync_with_InstanceOfAssertFactories_Test.java
+++ src/test/java/org/assertj/core/api/Assertions_sync_with_InstanceOfAssertFactories_Test.java
@@ -25,7 +25,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
-import java.util.Collection;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Predicate;
@@ -35,44 +34,49 @@ import org.junit.jupiter.api.Test;
 
 class Assertions_sync_with_InstanceOfAssertFactories_Test extends BaseAssertionsTest {
 
-  private static final Class<?>[] IGNORED_ASSERT_TYPES = {
-      // ObjectArrayAssert is ignored because the comparison of the input GenericArrayTypes will always fail, since
-      // it verifies the inner TypeVariable which returns the defining Method as result of TypeVariable#getGenericDeclaration()
-      ObjectArrayAssert.class
+  private static final Class<?>[] FIELD_FACTORIES_IGNORED_TYPES = {
+      // There can be no Comparable field factory with a base type.
+      AbstractComparableAssert.class,
+      // The comparison of the input GenericArrayTypes will always fail, since it verifies the inner TypeVariable which
+      // returns the defining Method as result of TypeVariable#getGenericDeclaration().
+      ObjectArrayAssert.class,
+      // A field factory for an object is pointless.
+      ObjectAssert.class,
+  };
+
+  private static final Class<?>[] METHOD_FACTORIES_IGNORED_TYPES = {
+      // The comparison of the input GenericArrayTypes will always fail, since it verifies the inner TypeVariable which
+      // returns the defining Method as result of TypeVariable#getGenericDeclaration().
+      ObjectArrayAssert.class,
   };
 
   @Test
-  void each_standard_assertion_should_have_an_instance_of_assert_factory() {
+  void each_standard_assertion_should_have_an_instance_of_assert_factory_static_field() {
     // GIVEN
     Map<Type, Type> assertThatMethods = findAssertThatParameterAndReturnTypes();
     // WHEN
-    Map<Type, Type> factories = Stream.of(findFieldFactoryTypes(), findMethodFactoryTypes())
-                                      .map(Map::entrySet)
-                                      .flatMap(Collection::stream)
-                                      .distinct()
-                                      .collect(toMap(Entry::getKey, Entry::getValue));
+    Map<Type, Type> fieldFactories = findFieldFactoryTypes();
     // THEN
-    then(factories).containsAllEntriesOf(assertThatMethods)
-                   .hasSameSizeAs(assertThatMethods);
+    then(fieldFactories).containsAllEntriesOf(assertThatMethods)
+                        .hasSameSizeAs(assertThatMethods);
+  }
+
+  @Test
+  void each_standard_assertion_with_type_parameters_should_have_an_instance_of_assert_factory_static_method() {
+    // GIVEN
+    Map<Type, Type> assertThatMethods = findTypedAssertThatParameterAndReturnTypes();
+    // WHEN
+    Map<Type, Type> methodFactories = findMethodFactoryTypes();
+    // THEN
+    then(methodFactories).containsAllEntriesOf(assertThatMethods)
+                         .hasSameSizeAs(assertThatMethods);
   }
 
   private Map<Type, Type> findAssertThatParameterAndReturnTypes() {
-    return Stream.of(findMethodsWithName(Assertions.class, "assertThat", ignoredReturnTypes()))
-                 .map(method -> entry(normalize(genericParameterType(method)), normalize(method.getGenericReturnType())))
+    return Stream.of(findMethodsWithName(Assertions.class, "assertThat", ignoredReturnTypes(FIELD_FACTORIES_IGNORED_TYPES)))
+                 .map(this::toParameterAndReturnTypeEntry)
                  .filter(not(this::isPrimitiveTypeKey))
                  .collect(toMap(Entry::getKey, Entry::getValue));
-  }
-
-  private Class<?>[] ignoredReturnTypes() {
-    return Stream.of(SPECIAL_IGNORED_RETURN_TYPES, IGNORED_ASSERT_TYPES)
-                 .flatMap(Stream::of)
-                 .toArray(Class[]::new);
-  }
-
-  private Type genericParameterType(Method method) {
-    Type[] parameterTypes = method.getGenericParameterTypes();
-    assertThat(parameterTypes).hasSize(1);
-    return parameterTypes[0];
   }
 
   private <K, V> boolean isPrimitiveTypeKey(Entry<K, V> entry) {
@@ -82,21 +86,60 @@ class Assertions_sync_with_InstanceOfAssertFactories_Test extends BaseAssertions
     return false;
   }
 
+  private Map<Type, Type> findTypedAssertThatParameterAndReturnTypes() {
+    return Stream.of(findMethodsWithName(Assertions.class, "assertThat", ignoredReturnTypes(METHOD_FACTORIES_IGNORED_TYPES)))
+                 .filter(this::hasTypeParameters)
+                 .map(this::toParameterAndReturnTypeEntry)
+                 .collect(toMap(Entry::getKey, Entry::getValue));
+  }
+
+  private boolean hasTypeParameters(Method method) {
+    return method.getTypeParameters().length != 0;
+  }
+
+  private Class<?>[] ignoredReturnTypes(Class<?>... ignoredAssertTypes) {
+    return Stream.of(SPECIAL_IGNORED_RETURN_TYPES, ignoredAssertTypes)
+                 .flatMap(Stream::of)
+                 .toArray(Class[]::new);
+  }
+
+  private Entry<Type, Type> toParameterAndReturnTypeEntry(Method method) {
+    return entry(normalize(genericParameterType(method)), normalize(method.getGenericReturnType()));
+  }
+
+  private Type genericParameterType(Method method) {
+    Type[] parameterTypes = method.getGenericParameterTypes();
+    assertThat(parameterTypes).hasSize(1);
+    return parameterTypes[0];
+  }
+
   private Map<Type, Type> findFieldFactoryTypes() {
     return Stream.of(InstanceOfAssertFactories.class.getFields())
                  .filter(not(Field::isSynthetic)) // Exclude $jacocoData - see #590 and jacoco/jacoco#168
                  .map(Field::getGenericType)
                  .map(this::extractTypeParameters)
-                 .filter(not(this::ignoredFactory))
+                 .filter(not(this::isIgnoredFieldFactory))
                  .collect(toMap(Entry::getKey, Entry::getValue));
+  }
+
+  private boolean isIgnoredFieldFactory(Entry<Type, Type> e) {
+    return isIgnoredFactory(e, FIELD_FACTORIES_IGNORED_TYPES);
   }
 
   private Map<Type, Type> findMethodFactoryTypes() {
     return Stream.of(InstanceOfAssertFactories.class.getMethods())
                  .map(Method::getGenericReturnType)
                  .map(this::extractTypeParameters)
-                 .filter(not(this::ignoredFactory))
+                 .filter(not(this::isIgnoredMethodFactory))
                  .collect(toMap(Entry::getKey, Entry::getValue));
+  }
+
+  private boolean isIgnoredMethodFactory(Entry<Type, Type> e) {
+    return isIgnoredFactory(e, METHOD_FACTORIES_IGNORED_TYPES);
+  }
+
+  private boolean isIgnoredFactory(Entry<Type, Type> e, Class<?>... ignoredTypes) {
+    return Stream.of(ignoredTypes).anyMatch(type -> e.getValue().equals(type));
   }
 
   private Entry<Type, Type> extractTypeParameters(Type type) {
@@ -120,12 +163,9 @@ class Assertions_sync_with_InstanceOfAssertFactories_Test extends BaseAssertions
     return type;
   }
 
+  // Borrowed from JDK 11
   private static <T> Predicate<T> not(Predicate<T> target) {
     return target.negate();
-  }
-
-  private boolean ignoredFactory(Entry<Type, Type> e) {
-    return Stream.of(IGNORED_ASSERT_TYPES).anyMatch(type -> e.getValue().equals(type));
   }
 
 }
