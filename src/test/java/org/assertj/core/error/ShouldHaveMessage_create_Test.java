44f116e62e161667894d0a236501cc163c9cd940
Erhard Pointl <epeee@users.noreply.github.com>
2019-08-18 08:17:55 +0200

    fix possible MissingFormatArgumentException in ShouldHaveMessage (#1576)
    fixes #1574

--- src/test/java/org/assertj/core/error/ShouldHaveMessage_create_Test.java
+++ src/test/java/org/assertj/core/error/ShouldHaveMessage_create_Test.java
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.Test;
 public class ShouldHaveMessage_create_Test {
 
   @Test
-  public void should_create_error_message() {
+  void should_create_error_message() {
     // GIVEN
     Exception cause = new Exception("cause");
     RuntimeException actual = new RuntimeException("error message", cause);
@@ -38,6 +38,24 @@ public class ShouldHaveMessage_create_Test {
                                        "Throwable that failed the check:%n" +
                                        "%n%s",
                                        Throwables.getStackTrace(actual));
+  }
+
+  @Test
+  void should_create_error_message_escaping_percent() {
+    // GIVEN
+    Throwable actual = new RuntimeException("%3A");
+    // WHEN
+    String errorMessage = shouldHaveMessage(actual, "expected error message").create(new TestDescription("TEST"));
+    // THEN
+    assertThat(errorMessage).isEqualTo("[TEST] %n" +
+        "Expecting message to be:%n" +
+        "  <\"expected error message\">%n" +
+        "but was:%n" +
+        "  <\"%%3A\">%n" +
+        "%n" +
+        "Throwable that failed the check:%n" +
+        "%n%s",
+      Throwables.getStackTrace(actual));
   }
 
 }
