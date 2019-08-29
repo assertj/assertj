167c63e4a901d598ea9418eef6a6e885aa7ba279
Geoffrey Arthaud <garthaud@gmail.com>
2019-08-29 12:15:06 +0200

    assertHasSizeBetween works with strings. Fixes #1586 (#1587)
    * assertHasSizeBetween works with strings. Fixes #1586

---
+++ src/test/java/org/assertj/core/internal/strings/Strings_assertHasSizeBetween_Test.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2019 the original author or authors.
+ */
+package org.assertj.core.internal.strings;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.error.ShouldHaveSizeBetween.shouldHaveSizeBetween;
+import static org.assertj.core.test.TestData.someInfo;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+
+import org.assertj.core.api.AssertionInfo;
+import org.assertj.core.internal.Strings;
+import org.assertj.core.internal.StringsBaseTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for <code>{@link Strings#assertHasSizeGreaterThan(AssertionInfo, CharSequence, int)}</code>.
+ *
+ * @author Geoffrey Arthaud
+ */
+public class Strings_assertHasSizeBetween_Test extends StringsBaseTest {
+
+  @Test
+  public void should_fail_if_actual_is_null() {
+    // GIVEN
+    String actual = null;
+    // WHEN
+    AssertionError error = expectAssertionError(() -> assertThat(actual).hasSizeBetween(4, 7));
+    // THEN
+    assertThat(error).hasMessage(actualIsNull());   
+  }
+
+  @Test
+  public void should_fail_if_size_of_actual_is_less_to_min_expected_size() {
+    // GIVEN
+    String actual = "Han";
+    // WHEN
+    AssertionError error = expectAssertionError(() -> assertThat(actual).hasSizeBetween(4, 7));
+    // THEN
+    String errorMessage = shouldHaveSizeBetween(actual, actual.length(), 4, 7).create();
+    assertThat(error).hasMessage(errorMessage);    
+  }
+
+  @Test
+  public void should_fail_if_size_of_actual_is_greater_than_max_expected_size() {
+    // GIVEN
+    String actual = "Han";
+    // WHEN
+    AssertionError error = expectAssertionError(() -> assertThat(actual).hasSizeBetween(1, 2));
+    // THEN
+    String errorMessage = shouldHaveSizeBetween(actual, actual.length(), 1, 2).create();
+    assertThat(error).hasMessage(errorMessage);    
+  }
+
+  @Test
+  public void should_pass_if_size_of_actual_is_between_sizes() {
+    // GIVEN
+    String actual = "Han";
+    // THEN
+    strings.assertHasSizeBetween(someInfo(), actual, 2, 7);
+  }
+}
