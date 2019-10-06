452dd2cb1ac7819332ea67534a228a12829b2f01
Stephen O'Rourke <storourke@gmail.com>
2019-10-07 08:52:26 +1100

    Show the stack trace for the Throwable under test when hasMessageMatching fails (#1355)

--- src/test/java/org/assertj/core/error/ShouldHaveMessageMatchingRegex_create_Test.java
+++ src/test/java/org/assertj/core/error/ShouldHaveMessageMatchingRegex_create_Test.java
@@ -12,9 +12,9 @@
  */
 package org.assertj.core.error;
 
-import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.error.ShouldHaveMessageMatchingRegex.shouldHaveMessageMatchingRegex;
+import static org.assertj.core.util.Throwables.getStackTrace;
 
 import org.assertj.core.internal.TestDescription;
 import org.junit.jupiter.api.Test;
@@ -25,16 +25,20 @@ public class ShouldHaveMessageMatchingRegex_create_Test {
   public void should_create_error_message() {
     // GIVEN
     String regex = "regex";
-    RuntimeException actual = new RuntimeException("error message");
+    RuntimeException actual = new RuntimeException("error %s message");
     // WHEN
     String errorMessage = shouldHaveMessageMatchingRegex(actual, regex).create(new TestDescription("TEST"));
     // THEN
-    assertThat(errorMessage).isEqualTo(format("[TEST] %n" +
-                                              "Expecting message:%n" +
-                                              "  <\"error message\">%n" +
-                                              "to match regex:%n" +
-                                              "  <\"regex\">%n" +
-                                              "but did not."));
+    assertThat(errorMessage).isEqualTo("[TEST] %n" +
+                                       "Expecting message:%n" +
+                                       "  <\"error %%s message\">%n" +
+                                       "to match regex:%n" +
+                                       "  <\"regex\">%n" +
+                                       "but did not.%n" +
+                                       "%n" +
+                                       "Throwable that failed the check:%n" +
+                                       "%n%s",
+                                       getStackTrace(actual));
   }
 
 }
