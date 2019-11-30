53436ecd02daffeb8988452185bfdd2f34051bb0
Stefano Cordio <stefano.cordio@gmail.com>
2019-11-30 15:25:38 +0100

    Add modular integration test

---
+++ src/it/modular-tests/src/test/java/black/box/BlackBoxTest.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2019 the original author or authors.
+ */
+package black.box;
+
+import org.assertj.core.util.Closeables;
+import org.junit.jupiter.api.Test;
+
+import java.io.Closeable;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
+
+class BlackBoxTest {
+
+  @Test
+  void issue_1656() {
+    // GIVEN
+    Closeable closeable = null;
+    // WHEN
+    Throwable thrown = catchThrowable(() -> Closeables.closeQuietly(closeable));
+    // THEN
+    then(thrown).isNull();
+  }
+
+}
