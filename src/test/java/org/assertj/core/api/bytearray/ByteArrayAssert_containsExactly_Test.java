1112bcaa1ba75afbca5474c8feba7dc60e6abf6c
Indrek Priks <indrek.opensource@gmail.com>
2020-03-06 17:29:23 +0200

    ByteArrayAssert.containsExactly(byte...) error message will mention not found and not expected items (#1801)

--- src/test/java/org/assertj/core/api/bytearray/ByteArrayAssert_containsExactly_Test.java
+++ src/test/java/org/assertj/core/api/bytearray/ByteArrayAssert_containsExactly_Test.java
@@ -12,15 +12,17 @@
  */
 package org.assertj.core.api.bytearray;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.test.ByteArrays.arrayOf;
 import static org.mockito.Mockito.verify;
 
 import org.assertj.core.api.ByteArrayAssert;
 import org.assertj.core.api.ByteArrayAssertBaseTest;
+import org.junit.jupiter.api.Test;
 
 /**
  * Tests for <code>{@link org.assertj.core.api.ByteArrayAssert#containsExactly(byte...)}</code>.
- * 
+ *
  * @author Jean-Christophe Gay
  */
 public class ByteArrayAssert_containsExactly_Test extends ByteArrayAssertBaseTest {
@@ -32,6 +34,11 @@ public class ByteArrayAssert_containsExactly_Test extends ByteArrayAssertBaseTes
 
   @Override
   protected void verify_internal_effects() {
-    verify(objects).assertEqual(getInfo(assertions), getActual(assertions), arrayOf(1, 2));
+    verify(arrays).assertContainsExactly(getInfo(assertions), getActual(assertions), arrayOf(1, 2));
+  }
+
+  @Test
+  public void invoke_api_like_user() {
+    assertThat(new byte[] { 1, 2, 3 }).containsExactly((byte) 1, (byte) 2, (byte) 3);
   }
 }
