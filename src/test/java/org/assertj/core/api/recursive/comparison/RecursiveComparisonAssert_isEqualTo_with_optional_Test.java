68dbf9eafd441a69cda42498f4340a6a9f2dfb5c
Joel Costigliola <joel.costigliola@gmail.com>
2020-03-27 22:56:30 +1300

    Add more optional types to test

--- src/test/java/org/assertj/core/api/recursive/comparison/RecursiveComparisonAssert_isEqualTo_with_optional_Test.java
+++ src/test/java/org/assertj/core/api/recursive/comparison/RecursiveComparisonAssert_isEqualTo_with_optional_Test.java
@@ -15,6 +15,9 @@ package org.assertj.core.api.recursive.comparison;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 import java.util.stream.Stream;
 
 import org.assertj.core.api.RecursiveComparisonAssert_isEqualTo_BaseTest;
@@ -34,8 +37,7 @@ public class RecursiveComparisonAssert_isEqualTo_with_optional_Test extends Recu
   }
 
   static Stream<Arguments> sameBooks() {
-    return Stream.of(Arguments.of(new BookWithOptionalCoAuthor(new Author("test")),
-                                  new BookWithOptionalCoAuthor(new Author("test"))),
+    return Stream.of(Arguments.of(new BookWithOptionalCoAuthor("test"), new BookWithOptionalCoAuthor("test")),
                      // empty optional coAuthor
                      Arguments.of(new BookWithOptionalCoAuthor(null), new BookWithOptionalCoAuthor(null)),
                      // null coAuthor
@@ -43,37 +45,51 @@ public class RecursiveComparisonAssert_isEqualTo_with_optional_Test extends Recu
   }
 
   @ParameterizedTest(name = "author 1 {0} / author 2 {1} / path {2} / value 1 {3}/ value 2 {4}")
-  @MethodSource("differentOptionalCoAuthors")
-  public void should_fail_when_comparing_different_optional_fields(Author author1, Author author2, String path, Object value1,
+  @MethodSource("differentBookWithOptionalCoAuthors")
+  public void should_fail_when_comparing_different_optional_fields(BookWithOptionalCoAuthor actual,
+                                                                   BookWithOptionalCoAuthor expected, String path, Object value1,
                                                                    Object value2) {
-    // GIVEN
-    BookWithOptionalCoAuthor actual = new BookWithOptionalCoAuthor(author1);
-    BookWithOptionalCoAuthor expected = new BookWithOptionalCoAuthor(author2);
     // WHEN
     compareRecursivelyFailsAsExpected(actual, expected);
     // THEN
     verifyShouldBeEqualByComparingFieldByFieldRecursivelyCall(actual, expected, diff(path, value1, value2));
   }
 
-  static Stream<Arguments> differentOptionalCoAuthors() {
-    Author pratchett = new Author("Terry Pratchett");
-    return Stream.of(Arguments.of(pratchett, new Author("George Martin"), "coAuthor.value.name", "Terry Pratchett",
-                                  "George Martin"),
-                     Arguments.of(pratchett, null, "coAuthor", Optional.of(pratchett), Optional.empty()),
-                     Arguments.of(null, pratchett, "coAuthor", Optional.empty(), Optional.of(pratchett)));
+  private static Stream<Arguments> differentBookWithOptionalCoAuthors() {
+    BookWithOptionalCoAuthor pratchett = new BookWithOptionalCoAuthor("Terry Pratchett");
+    BookWithOptionalCoAuthor georgeMartin = new BookWithOptionalCoAuthor("George Martin");
+
+    return Stream.of(Arguments.of(pratchett, georgeMartin,
+                                  "coAuthor.value.name", "Terry Pratchett", "George Martin"),
+                     Arguments.of(pratchett, new BookWithOptionalCoAuthor(null),
+                                  "coAuthor", Optional.of(new Author("Terry Pratchett")), Optional.empty()),
+                     Arguments.of(new BookWithOptionalCoAuthor(null), pratchett,
+                                  "coAuthor", Optional.empty(), Optional.of(new Author("Terry Pratchett"))),
+                     Arguments.of(new BookWithOptionalCoAuthor("Terry Pratchett", 1, 2L, 3.0),
+                                  new BookWithOptionalCoAuthor("Terry Pratchett", 2, 2L, 3.0),
+                                  "numberOfPages", OptionalInt.of(1), OptionalInt.of(2)),
+                     Arguments.of(new BookWithOptionalCoAuthor("Terry Pratchett", 1, 2L, 3.0),
+                                  new BookWithOptionalCoAuthor("Terry Pratchett", 1, 4L, 3.0),
+                                  "bookId", OptionalLong.of(2L), OptionalLong.of(4L)),
+                     Arguments.of(new BookWithOptionalCoAuthor("Terry Pratchett", 1, 2L, 3.0),
+                                  new BookWithOptionalCoAuthor("Terry Pratchett", 1, 2L, 6.0),
+                                  "price", OptionalDouble.of(3.0), OptionalDouble.of(6.0)));
   }
 
   @Test
   public void should_fail_when_comparing_non_optional_expected_field_with_optional_actual_field() {
     // GIVEN
     Author pratchett = new Author("Terry Pratchett");
-    BookWithOptionalCoAuthor actual = new BookWithOptionalCoAuthor(pratchett);
+    BookWithOptionalCoAuthor actual = new BookWithOptionalCoAuthor(pratchett.name);
     BookWithCoAuthor expected = new BookWithCoAuthor(pratchett);
     // WHEN
     compareRecursivelyFailsAsExpected(actual, expected);
     // THEN
     verifyShouldBeEqualByComparingFieldByFieldRecursivelyCall(actual, expected,
-                                                              diff("coAuthor", Optional.of(pratchett), pratchett));
+                                                              diff("bookId", null, 0l),
+                                                              diff("coAuthor", Optional.of(pratchett), pratchett),
+                                                              diff("numberOfPages", null, 0),
+                                                              diff("price", null, 0.0));
   }
 
   @Test
@@ -81,25 +97,38 @@ public class RecursiveComparisonAssert_isEqualTo_with_optional_Test extends Recu
     // GIVEN
     Author pratchett = new Author("Terry Pratchett");
     BookWithCoAuthor actual = new BookWithCoAuthor(pratchett);
-    BookWithOptionalCoAuthor expected = new BookWithOptionalCoAuthor(pratchett);
+    BookWithOptionalCoAuthor expected = new BookWithOptionalCoAuthor(pratchett.name);
     // WHEN
     compareRecursivelyFailsAsExpected(actual, expected);
     // THEN
     verifyShouldBeEqualByComparingFieldByFieldRecursivelyCall(actual, expected,
+                                                              diff("bookId", 0l, null),
                                                               diff("coAuthor", pratchett, Optional.of(pratchett),
-                                                                   "expected field is an Optional but actual field is not (org.assertj.core.api.recursive.comparison.Author)"));
+                                                                   "expected field is an Optional but actual field is not (org.assertj.core.api.recursive.comparison.Author)"),
+                                                              diff("numberOfPages", 0, null),
+                                                              diff("price", 0.0, null));
   }
 
   static class BookWithOptionalCoAuthor {
 
     Optional<Author> coAuthor;
+    OptionalInt numberOfPages;
+    OptionalLong bookId;
+    OptionalDouble price;
 
-    public BookWithOptionalCoAuthor(Author author) {
-      this.coAuthor = Optional.ofNullable(author);
+    public BookWithOptionalCoAuthor(String coAuthor) {
+      this.coAuthor = Optional.ofNullable(coAuthor == null ? null : new Author(coAuthor));
     }
 
     public BookWithOptionalCoAuthor() {
       this.coAuthor = null;
+    }
+
+    public BookWithOptionalCoAuthor(String coAuthor, int numberOfPages, long bookId, double price) {
+      this.coAuthor = Optional.ofNullable(coAuthor == null ? null : new Author(coAuthor));
+      this.numberOfPages = OptionalInt.of(numberOfPages);
+      this.bookId = OptionalLong.of(bookId);
+      this.price = OptionalDouble.of(price);
     }
 
     public Optional<Author> getCoAuthor() {
@@ -110,6 +139,9 @@ public class RecursiveComparisonAssert_isEqualTo_with_optional_Test extends Recu
   static class BookWithCoAuthor {
 
     Author coAuthor;
+    int numberOfPages;
+    long bookId;
+    double price;
 
     public BookWithCoAuthor(Author author) {
       this.coAuthor = author;
