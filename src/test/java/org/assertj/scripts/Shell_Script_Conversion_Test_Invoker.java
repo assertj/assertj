3c9498d6eb4abe967f8e9913468f2da978db490f
Eveneko <huyubin0203@gmail.com>
2020-04-22 01:05:42 +0800

    fix script permission

--- src/test/java/org/assertj/scripts/Shell_Script_Conversion_Test_Invoker.java
+++ src/test/java/org/assertj/scripts/Shell_Script_Conversion_Test_Invoker.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2020 the original author or authors.
+ */
 package org.assertj.scripts;
 
 import org.junit.Assert;
@@ -48,7 +60,7 @@ public class Shell_Script_Conversion_Test_Invoker {
   }
 
   private void Execute_Shell() throws Exception {
-    String shellCommand = "cd " + root + "/" + fileDirectory + " && " + root + "/" + testedShellPath + " \"" + fileName + "\"";
+    String shellCommand = "cd " + root + "/" + fileDirectory + " && " + "chmod +x " + root + "/" + testedShellPath + " && "  + root + "/" + testedShellPath + " \"" + fileName + "\"";
     Runtime runtime = Runtime.getRuntime();
     Process pro = runtime.exec(new String[] { shellProgramLocation, "-c", shellCommand }, null, null);
     int status = pro.waitFor();
