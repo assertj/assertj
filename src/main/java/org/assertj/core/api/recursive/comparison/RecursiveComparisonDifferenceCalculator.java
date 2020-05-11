07d283960828906c24f4bdb094ee3067f360ab25
phx1999 <phx1999@icloud.com>
2020-05-11 16:36:33 +0800

    solve conflic

--- src/main/java/org/assertj/core/api/recursive/comparison/RecursiveComparisonDifferenceCalculator.java
+++ src/main/java/org/assertj/core/api/recursive/comparison/RecursiveComparisonDifferenceCalculator.java
@@ -146,16 +146,13 @@ public class RecursiveComparisonDifferenceCalculator {
       // parent -> set{child} with child having a reference back to parent
       // it occurs to unordered collection where we compare all possible combination of the collection elements recursively
       // --
-      // Don't use removeAll which uses equals to compare values, comparison must be done by reference otherwise we could remove
-      // values too agressively, that occurs when we add a duplicate of a value already visited.
-      // Example:
-      // - a and a are duplicates but are not the same object, i.e. a equals a' but a' != a
-      // - visitedDualValues = {a , b , c}
-      // - dualValuesToCompare = {a'}
-      // dualValuesToCompare.removeAll(visitedDualValues) would remove it which is incorrect
-      // If we compare references then a' won't be removed from dualValuesToCompare
-      visitedDualValues
-        .forEach(visited -> dualValuesToCompare.removeIf(toCompare -> toCompare == visited));
+      // remove visited values one by one, DualValue.equals correctly compare respective actual and expected fields by reference
+      visitedDualValues.forEach(visitedDualValue -> {
+        dualValuesToCompare.stream()
+                           .filter(dualValueToCompare -> dualValueToCompare.equals(visitedDualValue))
+                           .findFirst()
+                           .ifPresent(dualValuesToCompare::remove);
+      });
     }
 
     private boolean mustCompareFieldsRecursively(boolean isRootObject, DualValue dualValue) {
