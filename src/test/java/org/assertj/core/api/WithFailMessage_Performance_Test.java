9f94d7904fd5851f890de28ca835f6559dcdba9c
Eveneko <huyubin0203@gmail.com>
2020-05-19 20:00:51 +0800

    add withFailmessage_Test

--- src/test/java/org/assertj/core/api/WithFailMessage_Performance_Test.java
+++ src/test/java/org/assertj/core/api/WithFailMessage_Performance_Test.java
@@ -11,11 +11,23 @@ public class WithFailMessage_Performance_Test {
 
   @Test
   public void withFailMessage_performance_test() {
-    String error = "ssss";
+    String error = "hyb";
     assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> {
       AtomicReference<String> actual = new AtomicReference<>("foo");
       assertThat(actual).withFailMessage(error).hasValue("bar");
     }).withMessageContaining(error);
+  }
+
+  @Test
+  public void test1() {
+    long startTime = System.nanoTime();
+    try {
+      String error = "hyb";
+      assertThat(error).withFailMessage("my error").isEqualTo("wy");
+    }catch (AssertionError e){
+      long endTime = System.nanoTime();
+      System.out.println("程序运行时间： " + (endTime - startTime)/1000000 + "ms");
+    }
   }
 
 }
