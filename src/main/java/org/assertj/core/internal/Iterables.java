8dd41b91c0680b3d6968dd71318cdac4ec95d405
Ting Sun <11710108@mail.sustech.edu.cn>
2020-06-12 19:25:09 +0800

    Update src/main/java/org/assertj/core/internal/Iterables.java
    Co-authored-by: mgrafl <michael.florian.grafl@gmail.com>

--- src/main/java/org/assertj/core/internal/Iterables.java
+++ src/main/java/org/assertj/core/internal/Iterables.java
@@ -1127,20 +1127,16 @@ public class Iterables {
     for(Consumer<? super E> consumer: consumers)
       requireNonNull(consumer, "The element in consumers must not be null");
 
-    List<E>[] satisfiedElementsLists = new ArrayList[consumers.length];
-    for (int i = 0; i < consumers.length; i++) {
-      Consumer<? super E> consumer = consumers[i];
-      satisfiedElementsLists[i] = stream(actual).filter(element -> {
-        try {
-          consumer.accept(element);
-        } catch (AssertionError ex) {
-          return false;
-        }
-        return true;
-      }).collect(toList());
-    }
+    List<E>[] satisfiedElementsLists = Arrays.stream(consumers)
+                                             .map(listFilteredBySatisfiedElements(actual))
+                                             .<List<E>>toArray(List[]::new);
+    
     if (!isSatisfied(satisfiedElementsLists, 0))
       throw failures.failure(info, shouldSatisfy(actual));
+  }
+  
+  private <E> Function<? super Consumer<? super E>, List<E>> listFilteredBySatisfiedElements(Iterable<? extends E> actual) {
+    return consumer -> stream(actual).filter(byPassingAssertions(consumer)).collect(toList());
   }
 
   private static <E> boolean isSatisfied(List<E>[] satisfiedElementsLists, int begin) {
