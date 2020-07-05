ad3a61222acb7165bea7a9512b22db1057d49e84
rpolton <richaerd>
2020-07-05 17:15:23 +0100

    assertContainsSequence: if "actual" is an infinite sequence, or only singly-traversable, then we need to perform the check differently so ... instead of looking ahead, store a ring buffer containing the last N elements visited and compare that. Also the standard internal assertJ validations need to be performed differently because, again, the "actual" can't guarantee being able to create a new iterator.

--- src/test/java/org/assertj/core/internal/iterables/Iterables_assertContainsExactly_Test.java
+++ src/test/java/org/assertj/core/internal/iterables/Iterables_assertContainsExactly_Test.java
@@ -12,6 +12,14 @@
  */
 package org.assertj.core.internal.iterables;
 
+import org.assertj.core.api.AssertionInfo;
+import org.assertj.core.internal.Iterables;
+import org.assertj.core.internal.IterablesBaseTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -27,14 +35,6 @@ import static org.assertj.core.util.Arrays.asList;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
 import static org.assertj.core.util.Lists.newArrayList;
 import static org.mockito.Mockito.verify;
-
-import java.util.List;
-import java.util.Iterator;
-
-import org.assertj.core.api.AssertionInfo;
-import org.assertj.core.internal.Iterables;
-import org.assertj.core.internal.IterablesBaseTest;
-import org.junit.jupiter.api.Test;
 
 /**
  * Tests for <code>{@link Iterables#assertContainsExactly(AssertionInfo, Iterable, Object[])}</code>.
