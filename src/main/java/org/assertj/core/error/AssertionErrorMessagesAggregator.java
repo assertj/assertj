f170c0a1f31cbd43182b6831858e8099bf5d637e
Ahmad M <ahmadmoawad2@yahoo.com>
2020-08-06 14:32:19 +0200

    Deprecate AssertionErrorMessagesAggregrator and create AssertionErrorMessagesAggregator

--- src/main/java/org/assertj/core/error/AssertionErrorMessagesAggregator.java
+++ src/main/java/org/assertj/core/error/AssertionErrorMessagesAggregator.java
@@ -14,13 +14,9 @@ package org.assertj.core.error;
 
 import java.util.List;
 
-/**
- * @deprecated use {@link AssertionErrorMessagesAggregator} instead
- */
-@Deprecated
-public class AssertionErrorMessagesAggregrator {
+public class AssertionErrorMessagesAggregator {
 
-  public static String aggregrateErrorMessages(List<String> errors) {
+  public static String aggregateErrorMessages(List<String> errors) {
     StringBuilder msg = new StringBuilder("%nThe following ");
     countAssertions(errors, msg);
     msg.append(" failed:%n");
