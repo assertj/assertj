54779bb06fa7046aebba2d34e78ddfdaff1044de
BJ Hargrave <bj@bjhargrave.com>
2020-08-26 15:13:05 -0400

    Add OSGi integration testing to build
    We define a simple test class to verify basic operations and a
    test case to verify class loading of soft proxies for custom assertions
    from OSGi bundles. The latter tests has been a problem area for which
    regular testing will help avoid regressions.
    
    Fixes https://github.com/joel-costigliola/assertj-core/issues/1888
    
    Signed-off-by: BJ Hargrave <bj@bjhargrave.com>

---
+++ src/test/java/org/assertj/core/osgi/soft/CustomSoftAssertionTest.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2020 the original author or authors.
+ */
+package org.assertj.core.osgi.soft;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.assertj.core.api.AbstractListAssert;
+import org.assertj.core.api.AbstractMapAssert;
+import org.assertj.core.api.AbstractSoftAssertions;
+import org.assertj.core.api.ObjectAssert;
+import org.assertj.core.api.ProxyableListAssert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
+
+// Need PR #1980 for Java 9+
+@DisabledForJreRange(min = JRE.JAVA_9)
+public class CustomSoftAssertionTest {
+
+  @Test
+  void custom_soft_assertions_success() {
+    Map<String, String> map = new HashMap<>();
+    map.put("key1", "value1");
+    map.put("key2", "value2");
+
+    TestSoftAssertions softly = new TestSoftAssertions();
+    softly.assertThat(map).containsKeys("key1", "key1").containsValues("value1", "value2");
+    softly.assertAll();
+  }
+
+  @Test
+  void custom_soft_assertions_failure() {
+    Map<String, String> map = new HashMap<>();
+    map.put("key1", "value1");
+    map.put("key2", "value2");
+
+    TestSoftAssertions softly = new TestSoftAssertions();
+    softly.assertThat(map).containsKeys("key1", "key3").containsValues("value3", "value2");
+    assertThat(softly.wasSuccess()).isFalse();
+    assertThat(softly.errorsCollected()).hasSize(2);
+  }
+
+  public static class TestProxyableMapAssert<KEY, VALUE>
+      extends AbstractMapAssert<TestProxyableMapAssert<KEY, VALUE>, Map<KEY, VALUE>, KEY, VALUE> {
+
+    public TestProxyableMapAssert(Map<KEY, VALUE> actual) {
+      super(actual, TestProxyableMapAssert.class);
+    }
+
+    @Override
+    protected <ELEMENT> AbstractListAssert<?, List<? extends ELEMENT>, ELEMENT, ObjectAssert<ELEMENT>> newListAssertInstance(
+        List<? extends ELEMENT> newActual) {
+      return new ProxyableListAssert<>(newActual);
+    }
+  }
+
+  public static class TestSoftAssertions extends AbstractSoftAssertions {
+    public <K, V> TestProxyableMapAssert<K, V> assertThat(Map<K, V> actual) {
+      @SuppressWarnings("unchecked")
+      TestProxyableMapAssert<K, V> softly = proxy(TestProxyableMapAssert.class, Map.class, actual);
+      return softly;
+    }
+  }
+
+}
