e99ea67b8d113787cf85eb5d167d31b4a3a8a89b
Stefano Cordio <stefano.cordio@gmail.com>
2020-10-08 17:14:47 +0200

    Disable MacOS flaky tests
    They work locally, but fail when running on GitHub Actions workflow.

--- src/test/java/org/assertj/core/api/future/CompletableFutureAssert_failsWithin_Test.java
+++ src/test/java/org/assertj/core/api/future/CompletableFutureAssert_failsWithin_Test.java
@@ -17,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.junit.jupiter.api.condition.OS.MAC;
 
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
@@ -25,6 +26,7 @@ import java.util.concurrent.TimeoutException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 
 @DisplayName("FutureAssert failsWithin")
 class CompletableFutureAssert_failsWithin_Test {
@@ -67,6 +69,7 @@ class CompletableFutureAssert_failsWithin_Test {
                       .withMessage(null);
   }
 
+  @DisabledOnOs(MAC) // Fails only on the Mac runner of GitHub Actions, but succeeds locally
   @Test
   void should_fail_if_future_completes_within_given_timeout() {
     // GIVEN
@@ -77,6 +80,7 @@ class CompletableFutureAssert_failsWithin_Test {
     then(assertionError).hasMessageContainingAll("Completed", "to have failed within 500L MILLISECONDS.");
   }
 
+  @DisabledOnOs(MAC) // Fails only on the Mac runner of GitHub Actions, but succeeds locally
   @Test
   void should_fail_if_future_completes_within_given_timeout_Duration() {
     // GIVEN
