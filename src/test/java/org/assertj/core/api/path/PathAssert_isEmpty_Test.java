3d4543ea7b935d1866169d3b40cfe1f1c0a52203
Omar Morales <morales.ortega.omar@gmail.com>
2020-12-03 21:27:51 -0600

    Unit tests added for isEmpty() and isNotEmpty() #2049

--- src/test/java/org/assertj/core/api/path/PathAssert_isEmpty_Test.java
+++ src/test/java/org/assertj/core/api/path/PathAssert_isEmpty_Test.java
@@ -1,8 +1,19 @@
 package org.assertj.core.api.path;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
 import org.assertj.core.api.PathAssert;
 import org.assertj.core.api.PathAssertBaseTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.error.ShouldBeEmpty.shouldBeEmpty;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+import static org.assertj.core.util.TempFileUtil.createTempPathWithContent;
+import static org.assertj.core.util.TempFileUtil.createTempPathWithNoContent;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 import static org.mockito.Mockito.verify;
 
@@ -20,6 +31,24 @@ class PathAssert_isEmpty_Test extends PathAssertBaseTest {
   @Override
   protected void verify_internal_effects() {
     verify(paths).assertIsEmptyFile(getInfo(assertions), getActual(assertions));
+  }
+
+  @Test
+  void should_pass_if_file_has_no_content() throws IOException {
+    //GIVEN
+    Path actual = createTempPathWithNoContent();
+    //WHEN/THEN
+    then(actual).isEmpty();
+  }
+
+  @Test
+  void should_fail_if_file_has_content() throws IOException {
+    //GIVEN
+    Path actual = createTempPathWithContent("Coffee", defaultCharset);
+    //WHEN
+    AssertionError assertionError = expectAssertionError(() -> assertThat(actual).isEmpty());
+    // THEN
+    then(assertionError).hasMessage(shouldBeEmpty(actual).create());
   }
 
 }
--- src/test/java/org/assertj/core/api/path/PathAssert_isNotEmpty_Test.java
+++ src/test/java/org/assertj/core/api/path/PathAssert_isNotEmpty_Test.java
@@ -3,7 +3,18 @@ package org.assertj.core.api.path;
 import org.assertj.core.api.PathAssert;
 import org.assertj.core.api.PathAssertBaseTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldBeEmpty.shouldBeEmpty;
+import static org.assertj.core.error.ShouldNotBeEmpty.shouldNotBeEmpty;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+import static org.assertj.core.util.TempFileUtil.createTempPathWithContent;
+import static org.assertj.core.util.TempFileUtil.createTempPathWithNoContent;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -20,6 +31,24 @@ class PathAssert_isNotEmpty_Test extends PathAssertBaseTest {
   @Override
   protected void verify_internal_effects() {
     verify(paths).assertIsNotEmptyFile(getInfo(assertions), getActual(assertions));
+  }
+
+  @Test
+  void should_pass_if_file_has_content() throws IOException {
+    //GIVEN
+    Path actual = createTempPathWithContent("Coffee", defaultCharset);
+    //WHEN/THEN
+    then(actual).isNotEmpty();
+  }
+
+  @Test
+  void should_fail_if_file_has_no_content() throws IOException {
+    //GIVEN
+    Path actual = createTempPathWithNoContent();
+    //WHEN
+    AssertionError assertionError = expectAssertionError(() -> assertThat(actual).isNotEmpty());
+    // THEN
+    then(assertionError).hasMessage(shouldNotBeEmpty(actual.toFile()).create());
   }
 
 }
