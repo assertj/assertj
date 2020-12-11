a7d2f09f2e6b5b8e9aa1fa8a4b7a40f56db73c4f
mayralgr <ml17ft@gmail.com>
2020-12-10 21:15:31 -0600

    Add test for cointains with Character array

---
+++ src/test/java/org/assertj/core/api/chararray/CharArrayAssert_contains_with_Character_array_Test.java
@@ -0,0 +1,43 @@
+package org.assertj.core.api.chararray;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldNotBeNull.shouldNotBeNull;
+import static org.assertj.core.test.CharArrays.arrayOf;
+
+import org.assertj.core.api.CharArrayAssert;
+import org.assertj.core.api.CharArrayAssertBaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.verify;
+
+/**
+ * Tests for <code>{@link CharArrayAssert#contains(char[])}</code>.
+ *
+ * @author Lucero Garcia
+ */
+@DisplayName("CharacterArrayAssert contains(Character[])")
+public class CharArrayAssert_contains_with_Character_array_Test extends CharArrayAssertBaseTest {
+
+  @Test
+  void should_fail_if_values_is_null() {
+    // GIVEN
+    Character[] values = null;
+    // WHEN
+    Throwable thrown = catchThrowable(() -> assertions.contains(values));
+    // THEN
+    then(thrown).isInstanceOf(NullPointerException.class)
+      .hasMessage(shouldNotBeNull("values").create());
+  }
+
+  @Override
+  protected CharArrayAssert invoke_api_method() {
+    return assertions.contains(new Character[]{'a', 'b'});
+  }
+
+  @Override
+  protected void verify_internal_effects() {
+    verify(arrays).assertContains(getInfo(assertions), getActual(assertions), arrayOf('a', 'b'));
+  }
+}
