4b9f5ef46de68509751c21e79ab98cf7f71a2ab0
Himadri Mandal <m.himadri@hotmail.com>
2021-01-08 13:22:06 +0530

    Remove <> from ElementsShouldHaveAtLeast and ElementsShouldHaveExactly

--- src/test/java/org/assertj/core/error/ElementsShouldHaveAtLeast_create_Test.java
+++ src/test/java/org/assertj/core/error/ElementsShouldHaveAtLeast_create_Test.java
@@ -37,7 +37,7 @@ class ElementsShouldHaveAtLeast_create_Test {
     // WHEN
     String message = factory.create(new TextDescription("Test"), STANDARD_REPRESENTATION);
     // THEN
-    then(message).isEqualTo(format("[Test] %nExpecting elements:%n<[\"Yoda\", \"Solo\", \"Leia\"]>%n to have at least 2 times <Jedi power>"));
+    then(message).isEqualTo(format("[Test] %nExpecting elements:%n[\"Yoda\", \"Solo\", \"Leia\"]%n to have at least 2 times Jedi power"));
   }
 
 }
