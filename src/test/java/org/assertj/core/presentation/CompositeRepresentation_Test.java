efa4504f0708374723a3874192646a2b5d2f9c12
Annette0127 <992401257@qq.com>
2021-04-20 16:46:09 +0800

    Some improvements have been made according to the first round of comments.

--- src/test/java/org/assertj/core/presentation/CompositeRepresentation_Test.java
+++ src/test/java/org/assertj/core/presentation/CompositeRepresentation_Test.java
@@ -1,62 +1,46 @@
 package org.assertj.core.presentation;
 
+import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.presentation.BinaryRepresentation.BINARY_REPRESENTATION;
-import static org.assertj.core.presentation.HexadecimalRepresentation.HEXA_REPRESENTATION;
-import static org.assertj.core.presentation.UnicodeRepresentation.UNICODE_REPRESENTATION;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CompositeRepresentation_Test extends AbstractBaseRepresentationTest {
   @Test
-  void should_return_toString_of_default_representation() {
-    CompositeRepresentation composite = new CompositeRepresentation();
+  void should_return_toString_of_Standard_representation() {
+    // GIVEN
+    Map<String, Representation> representations = new HashMap<>();
+    representations.put("std", new StandardRepresentation());
+    representations.put("hex", new HexadecimalRepresentation());
+    CompositeRepresentation composite = new CompositeRepresentation(representations);
+    // WHEN
     Object longNumber = 123L;
-    assertThat(composite.toStringOf(longNumber)).isEqualTo("123L");
-  }
-
-  @Test
-  void should_return_toString_of_specific_representation() {
-    CompositeRepresentation composite = new CompositeRepresentation();
-    Object longNumber = 123L;
-    composite.addRepresentation("hex", HEXA_REPRESENTATION);
-    assertThat(composite.toStringOf(longNumber, "hex")).isEqualTo("0x0000_0000_0000_007B");
-    assertThat(composite.toStringOf(longNumber, "standard")).isNull();
+    // THEN
+    then(composite.toStringOf(longNumber)).isEqualTo("123L");
   }
 
   @Test
   void should_return_null() {
-    CompositeRepresentation composite = new CompositeRepresentation();
+    // GIVEN
+    Map<String, Representation> representations = new HashMap<>();
+    CompositeRepresentation composite = new CompositeRepresentation(representations);
+    // WHEN
     Object nullObject = null;
-    assertThat(composite.toStringOf(nullObject)).isNull();
-  }
-
-
-  @Test
-  void should_represent_differently_according_to_the_representation() {
-    Map<String, Representation> representations = new HashMap<>();
-    representations.put("binary", BINARY_REPRESENTATION);
-    representations.put("unicode", UNICODE_REPRESENTATION);
-    CompositeRepresentation composite = new CompositeRepresentation(representations);
-    Object string = "你好！";
-    Object longNumber = 123L;
-    assertThat(composite.toStringOf(longNumber)).isEqualTo("0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01111011");
-    assertThat(composite.toStringOf(string, "unicode")).isEqualTo("\\u4f60\\u597d\\uff01");
+    // THEN
+    then(composite.toStringOf(nullObject)).isNull();
   }
 
   @Test
-  void should_be_able_to_add_or_delete_representations() {
-    Map<String, Representation> representations = new HashMap<>();
-    representations.put("binary", BINARY_REPRESENTATION);
-    representations.put("unicode", UNICODE_REPRESENTATION);
-    CompositeRepresentation composite = new CompositeRepresentation(representations);
-    Object longNumber = 123L;
-    composite.addRepresentation("hex", HEXA_REPRESENTATION);
-    assertThat(composite.getRepresentations().size()).isEqualTo(3);
-    composite.deleteRepresentation("binary");
-    assertThat(composite.toStringOf(longNumber, "binary")).isNull();
+  void should_throw_IllegalArgumentException() {
+    // GIVEN
+    Map<String, Representation> representations = null;
+    // WHEN and THEN
+    assertThrows(IllegalArgumentException.class, () -> {
+      new CompositeRepresentation(null);
+    });
   }
 }
