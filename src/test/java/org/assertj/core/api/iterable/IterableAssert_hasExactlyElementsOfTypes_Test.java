33c76e681f963747f9378590e9f4d28c8dbdae28
mct <11812226@mail.sustech.edu.cn>
2021-04-23 20:57:08 +0800

    revise two tests

--- src/test/java/org/assertj/core/api/iterable/IterableAssert_hasExactlyElementsOfTypes_Test.java
+++ src/test/java/org/assertj/core/api/iterable/IterableAssert_hasExactlyElementsOfTypes_Test.java
@@ -1,22 +1,23 @@
 package org.assertj.core.api.iterable;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
+import org.assertj.core.api.*;
 
-import java.util.List;
-
-import org.assertj.core.internal.ObjectArrays;
-import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.verify;
 
 /**
- * Only make one test since assertion is delegated to {@link ObjectArrays} which has its own tests.
+ * Tests for <code>{@link AbstractIterableAssert#hasExactlyElementsOfTypes(Class...)}</code>.
  */
-class IterableAssert_hasExactlyElementsOfTypes_Test
+class IterableAssert_hasExactlyElementsOfTypes_Test extends ObjectArrayAssertBaseTest
 {
-  @Test
-  void should_pass_if_actual_has_elements_of_the_expected_types_in_order()
+  @Override
+  protected ObjectArrayAssert<Object> invoke_api_method()
   {
-    List<Object> list = newArrayList(1, "a", 1.00);
-    assertThat(list).hasExactlyElementsOfTypes(Integer.class, String.class, Double.class);
+    return assertions.hasExactlyElementsOfTypes(Integer.class, Double.class);
+  }
+
+  @Override
+  protected void verify_internal_effects()
+  {
+    verify(arrays).assertHasExactlyElementsOfTypes(getInfo(assertions), getActual(assertions), Integer.class, Double.class);
   }
 }
--- src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertHasExactlyElementsOfTypes_Test.java
+++ src/test/java/org/assertj/core/internal/objectarrays/ObjectArrays_assertHasExactlyElementsOfTypes_Test.java
@@ -1,9 +1,6 @@
 package org.assertj.core.internal.objectarrays;
 
-import java.util.LinkedList;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.error.ShouldContainExactly.elementsDifferAtIndex;
 import static org.assertj.core.error.ShouldContainExactly.shouldContainExactly;
 import static org.assertj.core.test.TestData.someInfo;
@@ -11,7 +8,6 @@ import static org.assertj.core.util.Arrays.asList;
 import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
 import static org.assertj.core.util.Lists.newArrayList;
-import static org.mockito.Mockito.verify;
 
 import org.assertj.core.api.AssertionInfo;
 import org.assertj.core.internal.ObjectArrays;
@@ -19,17 +15,19 @@ import org.assertj.core.internal.ObjectArraysBaseTest;
 import org.assertj.core.internal.StandardComparisonStrategy;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 /**
  * Tests for <code>{@link ObjectArrays#assertHasExactlyElementsOfTypes(AssertionInfo, Object[], Class...)}</code>.
  */
 class ObjectArrays_assertHasExactlyElementsOfTypes_Test extends ObjectArraysBaseTest
 {
-  private static final Object[] arrayOfObjects = {"a", 'b', new LinkedList<>(), 10L};
+  private static final Object[] arrayOfObjects = {"a", new LinkedList<>(),10L};
 
   @Test
   void should_pass_if_actual_has_exactly_elements_of_the_expected_types_in_order()
   {
-    arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class, Character.class, LinkedList.class, Long.class);
+    arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class, LinkedList.class, Long.class);
   }
 
   @Test
@@ -37,60 +35,93 @@ class ObjectArrays_assertHasExactlyElementsOfTypes_Test extends ObjectArraysBase
   {
     // GIVEN
     Object[] array = null;
-    // GIVEN
-    AssertionError error = expectAssertionError(() -> arrays.assertHasExactlyElementsOfTypes(someInfo(), array, String.class));
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arrays.assertHasExactlyElementsOfTypes(someInfo(), array, String.class));
     // THEN
-    assertThat(error).hasMessage(actualIsNull());
+    then(error).hasMessage(actualIsNull());
   }
 
   @Test
   void should_fail_if_one_element_in_actual_does_not_belong_to_the_expected_type() {
-    // GIVEN
-    AssertionError error = expectAssertionError(() -> arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects,
-      String.class, Character.class, LinkedList.class, Double.class));
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class, LinkedList.class, Double.class));
     // THEN
-    Object[] expected = {String.class, Character.class, LinkedList.class, Double.class};
-    assertThat(error).hasMessage(shouldContainExactly(arrayOfObjects, asList(expected), newArrayList(Double.class),newArrayList(Long.class)).create());
+    Class<?>[] expected = {String.class, LinkedList.class, Double.class};
+    then(error).hasMessage(shouldContainExactly(arrayOfObjects, asList(expected), newArrayList(Double.class),newArrayList(Long.class)).create());
   }
 
   @Test
   void should_fail_if_types_of_elements_are_not_in_the_same_order_as_expected()
   {
-    AssertionInfo info = someInfo();
-
-    Throwable error = catchThrowable(() -> arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects,
-      Character.class, String.class, LinkedList.class, Long.class));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-
-    verify(failures).failure(info, elementsDifferAtIndex(String.class, Character.class, 0));
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, LinkedList.class, String.class, Long.class));
+    // THEN
+    then(error).hasMessage(elementsDifferAtIndex(String.class, LinkedList.class,0).create());
   }
 
   @Test
   void should_fail_if_actual_has_more_elements_than_expected()
   {
-    AssertionInfo info = someInfo();
-
-    Throwable error = catchThrowable(() -> arrays.assertHasExactlyElementsOfTypes(info, arrayOfObjects,String.class, Character.class));
-    Object[] expected = {String.class, Character.class};
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldContainExactly(arrayOfObjects, asList(expected),
-      newArrayList(), newArrayList(LinkedList.class, Long.class),
-      StandardComparisonStrategy.instance()));
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class));
+    // THEN
+    Class<?>[] expected = {String.class};
+    then(error).hasMessage(shouldContainExactly(arrayOfObjects, asList(expected), newArrayList(), newArrayList(LinkedList.class, Long.class)).create());
   }
 
   @Test
   void should_fail_if_actual_contains_all_types_but_has_less_elements_than_expected()
   {
-    AssertionInfo info = someInfo();
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arrays.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class, LinkedList.class, Long.class, Long.class));
+    // THEN
+    Class<?>[] expected = {String.class, LinkedList.class, Long.class, Long.class};
+    then(error).hasMessage(shouldContainExactly(arrayOfObjects, asList(expected), newArrayList(Long.class), newArrayList()).create());
+  }
 
-    Throwable error = catchThrowable(() ->
-      arrays.assertHasExactlyElementsOfTypes(info, arrayOfObjects,String.class, Character.class,LinkedList.class, Long.class, Long.class));
-    Object[] expected = {String.class, Character.class,LinkedList.class, Long.class, Long.class};
-    assertThat(error).isInstanceOf(AssertionError.class);
+  // ------------------------------------------------------------------------------------------------------------------
+  // tests using a custom comparison strategy
+  // ------------------------------------------------------------------------------------------------------------------
 
-    verify(failures).failure(info, shouldContainExactly(arrayOfObjects, asList(expected),
-      newArrayList(Long.class), newArrayList(),
-      StandardComparisonStrategy.instance()));
+  @Test
+  void should_pass_if_actual_has_exactly_elements_of_the_expected_types_in_order_according_to_custom_comparison_strategy()
+  {
+    arraysWithCustomComparisonStrategy.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class, LinkedList.class, Long.class);
+  }
+
+  @Test
+  void should_fail_if_one_element_in_actual_does_not_belong_to_the_expected_type_according_to_custom_comparison_strategy() {
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arraysWithCustomComparisonStrategy.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class, LinkedList.class, Double.class));
+    // THEN
+    Class<?>[] expected = {String.class, LinkedList.class, Double.class};
+    then(error).hasMessage(shouldContainExactly(arrayOfObjects, asList(expected), newArrayList(Double.class), newArrayList(Long.class), StandardComparisonStrategy.instance()).create());
+  }
+
+  @Test
+  void should_fail_if_types_of_elements_are_not_in_the_same_order_as_expected_according_to_custom_comparison_strategy()
+  {
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arraysWithCustomComparisonStrategy.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, LinkedList.class, String.class, Long.class));
+    // THEN
+    then(error).hasMessage(elementsDifferAtIndex(String.class, LinkedList.class,0, StandardComparisonStrategy.instance()).create());
+  }
+
+  @Test
+  void should_fail_if_actual_contains_all_types_but_has_less_elements_than_expected_according_to_custom_comparison_strategy()
+  {
+    // WHEN
+    AssertionError error = expectAssertionError(() ->
+      arraysWithCustomComparisonStrategy.assertHasExactlyElementsOfTypes(someInfo(), arrayOfObjects, String.class, LinkedList.class, Long.class, Long.class));
+    // THEN
+    Class<?>[] expected = {String.class, LinkedList.class, Long.class, Long.class};
+    then(error).hasMessage(shouldContainExactly(arrayOfObjects, asList(expected), newArrayList(Long.class), newArrayList(), StandardComparisonStrategy.instance()).create());
   }
 }
