assertj/assertj

commit ae1c70e70d16f5bee969e7f6925fee9777e29025

Honor map key comparison semantics with `containsOnly` assertions (#2167)
* Honor map key comparison semantics
Fixes #2159

Co-authored-by: Filip Hrisafov <filip.hrisafov@flowable.com>
Co-authored-by: Stefano Cordio <stefano.cordio@gmail.com>

src/main/java/org/assertj/core/internal/Maps.java
@@ -12,6 +12,7 @@
  */
 package org.assertj.core.internal;
 
+import static java.util.Objects.deepEquals;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.data.MapEntry.entry;
@@ -53,6 +54,7 @@ import static org.assertj.core.util.IterableUtil.toArray;
 import static org.assertj.core.util.Objects.areEqual;
 import static org.assertj.core.util.Preconditions.checkArgument;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -66,6 +68,7 @@ import java.util.function.Consumer;
 
 import org.assertj.core.api.AssertionInfo;
 import org.assertj.core.api.Condition;
+import org.assertj.core.data.MapEntry;
 import org.assertj.core.error.UnsatisfiedRequirement;
 import org.assertj.core.util.VisibleForTesting;
 
@@ -323,12 +326,7 @@ public class Maps {
 
   public <K, V> void assertContainsKeys(AssertionInfo info, Map<K, V> actual, K[] keys) {
     assertNotNull(info, actual);
-    Set<K> notFound = new LinkedHashSet<>();
-    for (K key : keys) {
-      if (!actual.containsKey(key)) {
-        notFound.add(key);
-      }
-    }
+    Set<K> notFound = getNotFoundKeys(actual, keys);
     if (notFound.isEmpty()) return;
     throw failures.failure(info, shouldContainKeys(actual, notFound));
   }
@@ -344,12 +342,8 @@ public class Maps {
 
   public <K, V> void assertDoesNotContainKeys(AssertionInfo info, Map<K, V> actual, K[] keys) {
     assertNotNull(info, actual);
-    Set<K> found = new LinkedHashSet<>();
-    for (K key : keys) {
-      if (key != null && actual.containsKey(key)) {
-        found.add(key);
-      }
-    }
+    requireNonNull(keys, keysToLookForIsNull("array of keys"));
+    Set<K> found = getFoundKeys(actual, keys);
     if (!found.isEmpty()) throw failures.failure(info, shouldNotContainKeys(actual, found));
   }
 
@@ -364,19 +358,74 @@ public class Maps {
 
   private <K, V> void assertContainsOnlyKeys(AssertionInfo info, Map<K, V> actual, String placeholderForErrorMessages, K[] keys) {
     assertNotNull(info, actual);
-    failIfNull(keys, keysToLookForIsNull(placeholderForErrorMessages));
+    requireNonNull(keys, keysToLookForIsNull(placeholderForErrorMessages));
     if (actual.isEmpty() && keys.length == 0) {
       return;
     }
     failIfEmpty(keys, keysToLookForIsEmpty(placeholderForErrorMessages));
 
-    Set<K> notFound = new LinkedHashSet<>();
-    Set<K> notExpected = new LinkedHashSet<>();
-
-    compareActualMapAndExpectedKeys(actual, keys, notExpected, notFound);
+    Set<K> notFound = getNotFoundKeys(actual, keys);
+    Set<K> notExpected = getNotExpectedKeys(actual, keys);
 
     if (!notFound.isEmpty() || !notExpected.isEmpty())
       throw failures.failure(info, shouldContainOnlyKeys(actual, keys, notFound, notExpected));
+  }
+
+  private static <K> Set<K> getFoundKeys(Map<K, ?> actual, K[] expectedKeys) {
+    // Stream API avoided for performance reasons
+    Set<K> found = new LinkedHashSet<>();
+    for (K expectedKey : expectedKeys) {
+      if (actual.containsKey(expectedKey)) found.add(expectedKey);
+    }
+    return found;
+  }
+
+  private static <K> Set<K> getNotFoundKeys(Map<K, ?> actual, K[] expectedKeys) {
+    // Stream API avoided for performance reasons
+    Set<K> notFound = new LinkedHashSet<>();
+    for (K expectedKey : expectedKeys) {
+      if (!actual.containsKey(expectedKey)) notFound.add(expectedKey);
+    }
+    return notFound;
+  }
+
+  private static <K> Set<K> getNotExpectedKeys(Map<K, ?> actual, K[] expectedKeys) {
+    // Stream API avoided for performance reasons
+    try {
+      Map<K, ?> clonedMap = clone(actual);
+      for (K expectedKey : expectedKeys) {
+        clonedMap.remove(expectedKey);
+      }
+      return clonedMap.keySet();
+    } catch (NoSuchMethodException | UnsupportedOperationException e) {
+      // actual cannot be cloned or is unmodifiable, falling back to LinkedHashMap
+      Map<K, ?> copiedMap = new LinkedHashMap<>(actual);
+      for (K expectedKey : expectedKeys) {
+        copiedMap.remove(expectedKey);
+      }
+      return copiedMap.keySet();
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <K, V> Map<K, V> clone(Map<K, V> map) throws NoSuchMethodException {
+    try {
+      if (map instanceof Cloneable) {
+        return (Map<K, V>) map.getClass().getMethod("clone").invoke(map);
+      }
+
+      try {
+        // try with copying constructor
+        return map.getClass().getConstructor(Map.class).newInstance(map);
+      } catch (NoSuchMethodException e) {
+        // try with default constructor
+        Map<K, V> newMap = map.getClass().getConstructor().newInstance();
+        newMap.putAll(map);
+        return newMap;
+      }
+    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+      throw new IllegalStateException(e);
+    }
   }
 
   public <K, V> void assertContainsValue(AssertionInfo info, Map<K, V> actual, V value) {
@@ -406,11 +455,49 @@ public class Maps {
     if (actual.isEmpty() && entries.length == 0) return;
     failIfEntriesIsEmptyEmptySinceActualIsNotEmpty(info, actual, entries);
 
-    Set<Entry<? extends K, ? extends V>> notFound = new LinkedHashSet<>();
-    Set<Entry<? extends K, ? extends V>> notExpected = new LinkedHashSet<>();
-    compareActualMapAndExpectedEntries(actual, entries, notExpected, notFound);
-    if (!notFound.isEmpty() || !notExpected.isEmpty())
+    Set<Entry<? extends K, ? extends V>> notFound = getNotFoundEntries(actual, entries);
+    Set<Entry<K, V>> notExpected = getNotExpectedEntries(actual, entries);
+
+    if (!(notFound.isEmpty() && notExpected.isEmpty()))
       throw failures.failure(info, shouldContainOnly(actual, entries, notFound, notExpected));
+  }
+
+  private static <K, V> Set<Entry<? extends K, ? extends V>> getNotFoundEntries(Map<K, V> actual,
+                                                                                Entry<? extends K, ? extends V>[] entries) {
+    // Stream API avoided for performance reasons
+    Set<Entry<? extends K, ? extends V>> notFound = new LinkedHashSet<>();
+    for (Entry<? extends K, ? extends V> entry : entries) {
+      if (!containsEntry(actual, entry)) notFound.add(entry);
+    }
+    return notFound;
+  }
+
+  private static <K, V> Set<Entry<K, V>> getNotExpectedEntries(Map<K, V> actual, Entry<? extends K, ? extends V>[] entries) {
+    // Stream API avoided for performance reasons
+    Set<Entry<K, V>> notExpected = new LinkedHashSet<>();
+    for (Entry<K, V> entry : mapWithoutExpectedEntries(actual, entries).entrySet()) {
+      MapEntry<K, V> mapEntry = entry(entry.getKey(), entry.getValue());
+      notExpected.add(mapEntry);
+    }
+    return notExpected;
+  }
+
+  private static <K, V> Map<K, V> mapWithoutExpectedEntries(Map<K, V> actual, Entry<? extends K, ? extends V>[] expectedEntries) {
+    // Stream API avoided for performance reasons
+    try {
+      Map<K, V> clonedMap = clone(actual);
+      for (Entry<? extends K, ? extends V> expectedEntry : expectedEntries) {
+        clonedMap.remove(expectedEntry.getKey(), expectedEntry.getValue());
+      }
+      return clonedMap;
+    } catch (NoSuchMethodException | UnsupportedOperationException e) {
+      // actual cannot be cloned or is unmodifiable, falling back to LinkedHashMap
+      Map<K, V> copiedMap = new LinkedHashMap<>(actual);
+      for (Entry<? extends K, ? extends V> expectedEntry : expectedEntries) {
+        copiedMap.remove(expectedEntry.getKey(), expectedEntry.getValue());
+      }
+      return copiedMap;
+    }
   }
 
   public <K, V> void assertContainsExactly(AssertionInfo info, Map<K, V> actual, Entry<? extends K, ? extends V>[] entries) {
@@ -439,22 +526,6 @@ public class Maps {
     }
 
     throw failures.failure(info, shouldContainExactly(actual, asList(entries), notFound, notExpected));
-  }
-
-  private <K, V> void compareActualMapAndExpectedKeys(Map<K, V> actual, K[] keys, Set<K> notExpected, Set<K> notFound) {
-
-    Map<K, V> actualEntries = new LinkedHashMap<>(actual);
-    for (K key : keys) {
-      if (actualEntries.containsKey(key)) {
-        // this is an expected key
-        actualEntries.remove(key);
-      } else {
-        // this is a not found key
-        notFound.add(key);
-      }
-    }
-    // All remaining keys from actual copy are not expected entries.
-    notExpected.addAll(actualEntries.keySet());
   }
 
   private <K, V> void compareActualMapAndExpectedEntries(Map<K, V> actual, Entry<? extends K, ? extends V>[] entries,
@@ -512,10 +583,6 @@ public class Maps {
     failIfEmpty(entries);
   }
 
-  private static <K> void failIfNull(K[] keys, String errorMessage) {
-    requireNonNull(keys, errorMessage);
-  }
-
   private static <K, V> void failIfNull(Entry<? extends K, ? extends V>[] entries) {
     requireNonNull(entries, ErrorMessages.entriesToLookForIsNull());
   }
@@ -524,9 +591,9 @@ public class Maps {
     requireNonNull(map, ErrorMessages.mapOfEntriesToLookForIsNull());
   }
 
-  private <K, V> boolean containsEntry(Map<K, V> actual, Entry<? extends K, ? extends V> entry) {
+  private static <K, V> boolean containsEntry(Map<K, V> actual, Entry<? extends K, ? extends V> entry) {
     requireNonNull(entry, ErrorMessages.entryToLookForIsNull());
-    return actual.containsKey(entry.getKey()) && areEqual(actual.get(entry.getKey()), entry.getValue());
+    return actual.containsKey(entry.getKey()) && deepEquals(actual.get(entry.getKey()), entry.getValue());
   }
 
   private void assertNotNull(AssertionInfo info, Map<?, ?> actual) {

src/test/java/org/assertj/core/test/Maps.java
@@ -13,7 +13,9 @@
 package org.assertj.core.test;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.TreeMap;
+import java.util.function.Supplier;
 
 import org.assertj.core.data.MapEntry;
 
@@ -24,18 +26,19 @@ public final class Maps {
 
   @SafeVarargs
   public static <K, V> LinkedHashMap<K, V> mapOf(MapEntry<K, V>... entries) {
-    LinkedHashMap<K, V> map = new LinkedHashMap<>();
-    for (MapEntry<K, V> entry : entries) {
-      map.put(entry.key, entry.value);
-    }
-    return map;
+    return mapOf(LinkedHashMap::new, entries);
   }
 
   @SafeVarargs
   public static <K extends Comparable<? super K>, V> TreeMap<K, V> treeMapOf(MapEntry<K, V>... entries) {
-    TreeMap<K, V> map = new TreeMap<>();
-    for (MapEntry<K, V> entry : entries) {
-      map.put(entry.key, entry.value);
+    return mapOf(TreeMap::new, entries);
+  }
+
+  @SafeVarargs
+  public static <K, V, M extends Map<K, V>> M mapOf(Supplier<M> supplier, Map.Entry<K, V>... entries) {
+    M map = supplier.get();
+    for (Map.Entry<K, V> entry : entries) {
+      map.put(entry.getKey(), entry.getValue());
     }
     return map;
   }

pom.xml
@@ -159,6 +159,12 @@
       <scope>test</scope>
     </dependency>
     <dependency>
+      <groupId>org.apache.commons</groupId>
+      <artifactId>commons-collections4</artifactId>
+      <version>4.4</version>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
       <groupId>commons-io</groupId>
       <artifactId>commons-io</artifactId>
       <version>2.8.0</version>
@@ -174,6 +180,12 @@
       <groupId>org.eclipse.platform</groupId>
       <artifactId>org.eclipse.osgi</artifactId>
       <version>3.16.200</version>
+      <scope>test</scope>
+    </dependency>
+    <dependency>
+      <groupId>org.springframework</groupId>
+      <artifactId>spring-core</artifactId>
+      <version>5.3.6</version>
       <scope>test</scope>
     </dependency>
   </dependencies>
@@ -234,6 +246,7 @@
         <executions>
           <!-- STEP 2: execute maven shade to rename bytebuddy -->
           <execution>
+            <id>bytebuddy</id>
             <phase>package</phase>
             <goals>
               <goal>shade</goal>
@@ -692,7 +705,9 @@
         <jdk>[16,)</jdk>
       </activation>
       <properties>
-        <argLine>-Dnet.bytebuddy.experimental=true --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.math=ALL-UNNAMED --add-opens=java.base/sun.nio.fs=ALL-UNNAMED</argLine>
+        <argLine>-Dnet.bytebuddy.experimental=true --add-opens=java.base/java.lang=ALL-UNNAMED
+          --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.math=ALL-UNNAMED
+          --add-opens=java.base/sun.nio.fs=ALL-UNNAMED</argLine>
       </properties>
     </profile>
     <profile>

src/main/java/org/assertj/core/api/AbstractMapAssert.java
@@ -1136,7 +1136,11 @@ public abstract class AbstractMapAssert<SELF extends AbstractMapAssert<SELF, ACT
 
   /**
    * Verifies that the actual map contains only the given keys and nothing else, in any order.
-   *
+   * <p>
+   * The verification tries to honor the key comparison semantic of the underlying map implementation.
+   * The map under test has to be cloned to identify unexpected elements, but depending on the map implementation
+   * this may not always be possible. In case it is not possible, a regular map is used and the key comparison strategy
+   * may not be the same as the map under test.
    * <p>
    * Examples :
    * <pre><code class='java'> Map&lt;Ring, TolkienCharacter&gt; ringBearers = new HashMap&lt;&gt;();
@@ -1173,7 +1177,11 @@ public abstract class AbstractMapAssert<SELF extends AbstractMapAssert<SELF, ACT
 
   /**
    * Verifies that the actual map contains only the given keys and nothing else, in any order.
-   *
+   * <p>
+   * The verification tries to honor the key comparison semantic of the underlying map implementation.
+   * The map under test has to be cloned to identify unexpected elements, but depending on the map implementation
+   * this may not always be possible. In case it is not possible, a regular map is used and the key comparison strategy
+   * may not be the same as the map under test.
    * <p>
    * Examples :
    * <pre><code class='java'> Map&lt;Ring, TolkienCharacter&gt; ringBearers = new HashMap&lt;&gt;();
@@ -1298,7 +1306,11 @@ public abstract class AbstractMapAssert<SELF extends AbstractMapAssert<SELF, ACT
 
   /**
    * Verifies that the actual map contains only the given entries and nothing else, in any order.
-   *
+   * <p>
+   * The verification tries to honor the key comparison semantic of the underlying map implementation.
+   * The map under test has to be cloned to identify unexpected elements, but depending on the map implementation
+   * this may not always be possible. In case it is not possible, a regular map is used and the key comparison strategy
+   * may not be the same as the map under test.
    * <p>
    * Examples :
    * <pre><code class='java'> Map&lt;Ring, TolkienCharacter&gt; ringBearers = new HashMap&lt;&gt;();

src/main/java/org/assertj/core/util/Lists.java
@@ -20,13 +20,19 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * Utility methods related to {@code java.util.List}s.
+ * Utility methods related to {@link List}s.
  *
  * @author Yvonne Wang
  * @author Alex Ruiz
  * @author Joel Costigliola
  */
 public final class Lists {
+
+  @SafeVarargs
+  public static <T> List<T> list(T... elements) {
+    return newArrayList(elements);
+  }
+
   /**
    * Creates a <em>mutable</em> {@link ArrayList} containing the given elements.
    *
@@ -36,16 +42,6 @@ public final class Lists {
    */
   @SafeVarargs
   public static <T> ArrayList<T> newArrayList(T... elements) {
-    if (elements == null) {
-      return null;
-    }
-    ArrayList<T> list = newArrayList();
-    Collections.addAll(list, elements);
-    return list;
-  }
-
-  @SafeVarargs
-  public static <T> List<T> list(T... elements) {
     if (elements == null) {
       return null;
     }
@@ -100,6 +96,5 @@ public final class Lists {
     return Collections.emptyList();
   }
 
-  private Lists() {
-  }
+  private Lists() {}
 }

src/main/java/org/assertj/core/util/Sets.java
@@ -22,9 +22,22 @@ import java.util.TreeSet;
 /**
  * Utility methods related to {@link Set}s.
  *
- * @author alruiz
+ * @author Alex Ruiz
  */
 public final class Sets {
+
+  /**
+   * Creates a <em>mutable</em> {@link HashSet} containing the given elements.
+   *
+   * @param <T> the generic type of the {@code HashSet} to create.
+   * @param elements the elements to store in the {@code HashSet}.
+   * @return the created {@code HashSet}, or {@code null} if the given array of elements is {@code null}.
+   */
+  @SafeVarargs
+  public static <T> Set<T> set(T... elements) {
+    return newLinkedHashSet(elements);
+  }
+
   /**
    * Creates a <em>mutable</em> {@code HashSet}.
    *

src/test/java/org/assertj/core/internal/MapsBaseTest.java
@@ -24,7 +24,6 @@ import org.assertj.core.data.MapEntry;
 import org.assertj.core.test.WithPlayerData;
 import org.junit.jupiter.api.BeforeEach;
 
-
 /**
  * Base class for {@link Maps} unit tests
  * <p>
@@ -42,7 +41,7 @@ public class MapsBaseTest extends WithPlayerData {
   protected AssertionInfo info;
 
   @BeforeEach
-  public void setUp() {
+  protected void setUp() {
     actual = mapOf(entry("name", "Yoda"), entry("color", "green"));
     failures = spy(new Failures());
     maps = new Maps();
@@ -54,8 +53,9 @@ public class MapsBaseTest extends WithPlayerData {
   protected static MapEntry[] emptyEntries() {
     return new MapEntry[0];
   }
-  
+
   protected static String[] emptyKeys() {
     return new String[0];
   }
+
 }

src/test/java/org/assertj/core/internal/maps/Maps_assertContainsKey_Test.java
@@ -33,7 +33,7 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Tests for <code>{@link Maps#assertContainsKey(AssertionInfo, Map, Object)}</code>.
- * 
+ *
  * @author Nicolas François
  * @author Joel Costigliola
  */
@@ -72,4 +72,5 @@ class Maps_assertContainsKey_Test extends MapsBaseTest {
     assertThat(error).isInstanceOf(AssertionError.class);
     verify(failures).failure(info, shouldContainKeys(actual, newLinkedHashSet(key)));
   }
+
 }

commit bc359bc57161c415615b7c40e59ec6380aa5d107

Map entry values handle deep equality comparison again
This appears to have broken in PR #2167 specifically with the use of
`Map.remove(Object, Object)` that does not perform deep equality
checking on the map entry value (e.g. arrays).

src/main/java/org/assertj/core/internal/Maps.java
@@ -485,19 +485,23 @@ public class Maps {
   private static <K, V> Map<K, V> mapWithoutExpectedEntries(Map<K, V> actual, Entry<? extends K, ? extends V>[] expectedEntries) {
     // Stream API avoided for performance reasons
     try {
-      Map<K, V> clonedMap = clone(actual);
-      for (Entry<? extends K, ? extends V> expectedEntry : expectedEntries) {
-        clonedMap.remove(expectedEntry.getKey(), expectedEntry.getValue());
-      }
-      return clonedMap;
+      return removeExpectedEntries(clone(actual), expectedEntries);
     } catch (NoSuchMethodException | UnsupportedOperationException e) {
       // actual cannot be cloned or is unmodifiable, falling back to LinkedHashMap
-      Map<K, V> copiedMap = new LinkedHashMap<>(actual);
-      for (Entry<? extends K, ? extends V> expectedEntry : expectedEntries) {
-        copiedMap.remove(expectedEntry.getKey(), expectedEntry.getValue());
-      }
-      return copiedMap;
+      return removeExpectedEntries(new LinkedHashMap<K, V>(actual), expectedEntries);
     }
+  }
+
+  private static <K, V> Map<K, V> removeExpectedEntries(Map<K, V> map, Entry<? extends K, ? extends V>[] expectedEntries) {
+    for (Entry<? extends K, ? extends V> expectedEntry : expectedEntries) {
+      K key = expectedEntry.getKey();
+      // must perform deep equals comparison on values as Map.remove(Object, Object) relies on
+      // Objects.equals which does not handle deep equality (e.g. arrays in map entry values)
+      if (map.containsKey(key) && deepEquals(map.get(key), expectedEntry.getValue())) {
+        map.remove(key);
+      }
+    }
+    return map;
   }
 
   public <K, V> void assertContainsExactly(AssertionInfo info, Map<K, V> actual, Entry<? extends K, ? extends V>[] entries) {

src/test/java/org/assertj/core/api/map/MapAssert_containsExactlyInAnyOrderEntriesOf_Test.java
@@ -52,4 +52,15 @@ class MapAssert_containsExactlyInAnyOrderEntriesOf_Test extends MapAssertBaseTes
     assertThat(actualMap).containsExactlyInAnyOrderEntriesOf(expectedMap);
   }
 
+  @Test
+  void invoke_api_like_user_with_value_array() {
+
+    // GIVEN
+    Map<String, byte[]> actualMap = mapOf(entry("key1", new byte[]{1, 2}),
+      entry("key2", new byte[]{3, 4, 5}));
+    Map<String, byte[]> expectedMap = mapOf(entry("key2", new byte[]{3, 4, 5}),
+      entry("key1", new byte[]{1, 2}));
+    // THEN
+    assertThat(actualMap).containsExactlyInAnyOrderEntriesOf(expectedMap);
+  }
 }