EugeneLesnov/assertj-core

Remove `MockPathsBaseTest`

src/test/java/org/assertj/core/internal/paths/Paths_assertHasDigest_with_MessageDigest_and_Byte_array_Test.java
@@ -1,0 +1,166 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2021 the original author or authors.
+ */
+package org.assertj.core.internal.paths;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldBeReadable.shouldBeReadable;
+import static org.assertj.core.error.ShouldBeRegularFile.shouldBeRegularFile;
+import static org.assertj.core.error.ShouldExist.shouldExist;
+import static org.assertj.core.error.ShouldHaveDigest.shouldHaveDigest;
+import static org.assertj.core.internal.Digests.toHex;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.assertj.core.internal.DigestDiff;
+import org.assertj.core.internal.PathsBaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+
+/**
+ * @author Valeriy Vyrva
+ */
+class Paths_assertHasDigest_with_MessageDigest_and_Byte_array_Test extends PathsBaseTest {
+
+  @BeforeEach
+  void setUpNioFilesWrapper() throws IOException {
+    given(nioFilesWrapper.newInputStream(any())).willCallRealMethod();
+  }
+
+  @Test
+  void should_fail_if_digest_is_null() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    MessageDigest digest = null;
+    byte[] expected = {};
+    // WHEN
+    Throwable thrown = catchThrowable(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(thrown).isInstanceOf(NullPointerException.class)
+                .hasMessage("The message digest algorithm should not be null");
+  }
+
+  @Test
+  void should_fail_if_expected_is_null() throws Exception {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = null;
+    // WHEN
+    Throwable thrown = catchThrowable(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(thrown).isInstanceOf(NullPointerException.class)
+                .hasMessage("The binary representation of digest to compare to should not be null");
+  }
+
+  @Test
+  void should_fail_if_actual_is_null() throws NoSuchAlgorithmException {
+    // GIVEN
+    Path actual = null;
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(actualIsNull());
+  }
+
+  @Test
+  void should_fail_if_actual_does_not_exist() throws NoSuchAlgorithmException {
+    // GIVEN
+    Path actual = tempDir.resolve("non-existent");
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldExist(actual).create());
+  }
+
+  @Test
+  void should_fail_if_actual_is_not_a_regular_file() throws Exception {
+    // GIVEN
+    Path actual = Files.createDirectory(tempDir.resolve("directory"));
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldBeRegularFile(actual).create());
+  }
+
+  @Test
+  @DisabledOnOs(WINDOWS)
+  void should_fail_on_unix_if_actual_is_not_readable() throws Exception {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    actual.toFile().setReadable(false);
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldBeReadable(actual).create());
+  }
+
+  @Test
+  void should_rethrow_IOException_as_UncheckedIOException() throws Exception {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    IOException cause = new IOException("boom!");
+    given(nioFilesWrapper.newInputStream(actual)).willThrow(cause);
+    // WHEN
+    Throwable thrown = catchThrowable(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(thrown).isInstanceOf(UncheckedIOException.class)
+                .hasCause(cause);
+  }
+
+  @Test
+  void should_fail_if_actual_does_not_have_expected_digest() throws Exception {
+    // GIVEN
+    Path actual = Files.write(tempDir.resolve("actual"), "Content".getBytes());
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = digest.digest("Another content".getBytes());
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldHaveDigest(actual, new DigestDiff(toHex(digest.digest(Files.readAllBytes(actual))),
+                                                                   toHex(expected),
+                                                                   digest)).create());
+  }
+
+  @Test
+  void should_pass_if_actual_has_expected_digest() throws Exception {
+    // GIVEN
+    Path actual = Files.write(tempDir.resolve("actual"), "Content".getBytes());
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = digest.digest(Files.readAllBytes(actual));
+    // WHEN/THEN
+    paths.assertHasDigest(info, actual, digest, expected);
+  }
+
+}

src/test/java/org/assertj/core/internal/paths/Paths_assertIsExecutable_Test.java
@@ -12,50 +12,58 @@
  */
 package org.assertj.core.internal.paths;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.error.ShouldBeExecutable.shouldBeExecutable;
 import static org.assertj.core.error.ShouldExist.shouldExist;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.assertj.core.internal.PathsBaseTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 
-class Paths_assertIsExecutable_Test extends MockPathsBaseTest {
+class Paths_assertIsExecutable_Test extends PathsBaseTest {
 
   @Test
   void should_fail_if_actual_is_null() {
-    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> paths.assertIsExecutable(info, null))
-                                                   .withMessage(actualIsNull());
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsExecutable(info, null));
+    // THEN
+    then(error).hasMessage(actualIsNull());
   }
 
   @Test
-  void should_fail_with_should_exist_error_if_actual_does_not_exist() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsExecutable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldExist(actual));
+  void should_fail_if_actual_does_not_exist() {
+    // GIVEN
+    Path actual = tempDir.resolve("non-existent");
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsExecutable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldExist(actual).create());
   }
 
   @Test
-  void should_fail_if_actual_exists_but_is_not_executable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isExecutable(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsExecutable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldBeExecutable(actual));
+  @DisabledOnOs(WINDOWS)
+  void should_fail_on_unix_if_actual_is_not_executable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsExecutable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldBeExecutable(actual).create());
   }
 
   @Test
-  void should_succeed_if_actual_exist_and_is_executable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isExecutable(actual)).thenReturn(true);
+  void should_pass_if_actual_is_executable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    actual.toFile().setExecutable(true);
+    // WHEN/THEN
     paths.assertIsExecutable(info, actual);
   }
 

src/test/java/org/assertj/core/internal/paths/Paths_assertIsReadable_Test.java
@@ -12,50 +12,59 @@
  */
 package org.assertj.core.internal.paths;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.error.ShouldBeReadable.shouldBeReadable;
+import static org.assertj.core.error.ShouldBeWritable.shouldBeWritable;
 import static org.assertj.core.error.ShouldExist.shouldExist;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.assertj.core.internal.PathsBaseTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 
-class Paths_assertIsReadable_Test extends MockPathsBaseTest {
+class Paths_assertIsReadable_Test extends PathsBaseTest {
 
   @Test
   void should_fail_if_actual_is_null() {
-    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> paths.assertIsReadable(info, null))
-                                                   .withMessage(actualIsNull());
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsReadable(info, null));
+    // THEN
+    then(error).hasMessage(actualIsNull());
   }
 
   @Test
-  void should_fail_with_should_exist_error_if_actual_does_not_exist() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsReadable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldExist(actual));
+  void should_fail_if_actual_does_not_exist() {
+    // GIVEN
+    Path actual = tempDir.resolve("non-existent");
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsReadable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldExist(actual).create());
   }
 
   @Test
-  void should_fail_if_actual_exists_but_is_not_readable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isReadable(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsReadable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldBeReadable(actual));
+  @DisabledOnOs(WINDOWS)
+  void should_fail_on_unix_if_actual_is_not_readable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    actual.toFile().setReadable(false);
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsReadable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldBeReadable(actual).create());
   }
 
   @Test
-  void should_succeed_if_actual_exist_and_is_readable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isReadable(actual)).thenReturn(true);
+  void should_pass_if_actual_is_readable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    // WHEN/THEN
     paths.assertIsReadable(info, actual);
   }
 

assertj/assertj

src/test/java/org/assertj/core/internal/paths/Paths_assertHasDigest_with_MessageDigest_and_Byte_array_Test.java
@@ -1,0 +1,167 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2021 the original author or authors.
+ */
+package org.assertj.core.internal.paths;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldBeReadable.shouldBeReadable;
+import static org.assertj.core.error.ShouldBeRegularFile.shouldBeRegularFile;
+import static org.assertj.core.error.ShouldExist.shouldExist;
+import static org.assertj.core.error.ShouldHaveDigest.shouldHaveDigest;
+import static org.assertj.core.internal.Digests.toHex;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.assertj.core.internal.DigestDiff;
+import org.assertj.core.internal.PathsBaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+
+/**
+ * @author Valeriy Vyrva
+ */
+class Paths_assertHasDigest_with_MessageDigest_and_Byte_array_Test extends PathsBaseTest {
+
+  @BeforeEach
+  void setUpNioFilesWrapper() throws IOException {
+    given(nioFilesWrapper.newInputStream(any())).willCallRealMethod();
+  }
+
+  @Test
+  void should_fail_if_digest_is_null() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    MessageDigest digest = null;
+    byte[] expected = {};
+    // WHEN
+    Throwable thrown = catchThrowable(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(thrown).isInstanceOf(NullPointerException.class)
+                .hasMessage("The message digest algorithm should not be null");
+  }
+
+  @Test
+  void should_fail_if_expected_is_null() throws Exception {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = null;
+    // WHEN
+    Throwable thrown = catchThrowable(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(thrown).isInstanceOf(NullPointerException.class)
+                .hasMessage("The binary representation of digest to compare to should not be null");
+  }
+
+  @Test
+  void should_fail_if_actual_is_null() throws NoSuchAlgorithmException {
+    // GIVEN
+    Path actual = null;
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(actualIsNull());
+  }
+
+  @Test
+  void should_fail_if_actual_does_not_exist() throws NoSuchAlgorithmException {
+    // GIVEN
+    Path actual = tempDir.resolve("non-existent");
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldExist(actual).create());
+  }
+
+  @Test
+  void should_fail_if_actual_is_not_a_regular_file() throws Exception {
+    // GIVEN
+    Path actual = Files.createDirectory(tempDir.resolve("directory"));
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldBeRegularFile(actual).create());
+  }
+
+  @Test
+  @DisabledOnOs(value = WINDOWS, disabledReason = "gh-FIXME")
+  void should_fail_if_actual_is_not_readable() throws Exception {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    actual.toFile().setReadable(false);
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldBeReadable(actual).create());
+  }
+
+  @Test
+  @DisabledOnOs(value = WINDOWS, disabledReason = "gh-FIXME")
+  void should_rethrow_IOException_as_UncheckedIOException() throws Exception {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = {};
+    IOException cause = new IOException("boom!");
+    given(nioFilesWrapper.newInputStream(actual)).willThrow(cause);
+    // WHEN
+    Throwable thrown = catchThrowable(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(thrown).isInstanceOf(UncheckedIOException.class)
+                .hasCause(cause);
+  }
+
+  @Test
+  void should_fail_if_actual_does_not_have_expected_digest() throws Exception {
+    // GIVEN
+    Path actual = Files.write(tempDir.resolve("actual"), "Content".getBytes());
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = digest.digest("Another content".getBytes());
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertHasDigest(info, actual, digest, expected));
+    // THEN
+    then(error).hasMessage(shouldHaveDigest(actual, new DigestDiff(toHex(digest.digest(Files.readAllBytes(actual))),
+                                                                   toHex(expected),
+                                                                   digest)).create());
+  }
+
+  @Test
+  void should_pass_if_actual_has_expected_digest() throws Exception {
+    // GIVEN
+    Path actual = Files.write(tempDir.resolve("actual"), "Content".getBytes());
+    MessageDigest digest = MessageDigest.getInstance("MD5");
+    byte[] expected = digest.digest(Files.readAllBytes(actual));
+    // WHEN/THEN
+    paths.assertHasDigest(info, actual, digest, expected);
+  }
+
+}

src/test/java/org/assertj/core/internal/paths/Paths_assertIsExecutable_Test.java
@@ -12,50 +12,58 @@
  */
 package org.assertj.core.internal.paths;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.error.ShouldBeExecutable.shouldBeExecutable;
 import static org.assertj.core.error.ShouldExist.shouldExist;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.assertj.core.internal.PathsBaseTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 
-class Paths_assertIsExecutable_Test extends MockPathsBaseTest {
+class Paths_assertIsExecutable_Test extends PathsBaseTest {
 
   @Test
   void should_fail_if_actual_is_null() {
-    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> paths.assertIsExecutable(info, null))
-                                                   .withMessage(actualIsNull());
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsExecutable(info, null));
+    // THEN
+    then(error).hasMessage(actualIsNull());
   }
 
   @Test
-  void should_fail_with_should_exist_error_if_actual_does_not_exist() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsExecutable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldExist(actual));
+  void should_fail_if_actual_does_not_exist() {
+    // GIVEN
+    Path actual = tempDir.resolve("non-existent");
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsExecutable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldExist(actual).create());
   }
 
   @Test
-  void should_fail_if_actual_exists_but_is_not_executable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isExecutable(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsExecutable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldBeExecutable(actual));
+  @DisabledOnOs(value = WINDOWS, disabledReason = "gh-FIXME")
+  void should_fail_if_actual_is_not_executable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsExecutable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldBeExecutable(actual).create());
   }
 
   @Test
-  void should_succeed_if_actual_exist_and_is_executable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isExecutable(actual)).thenReturn(true);
+  void should_pass_if_actual_is_executable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    actual.toFile().setExecutable(true);
+    // WHEN/THEN
     paths.assertIsExecutable(info, actual);
   }
 

src/test/java/org/assertj/core/internal/paths/Paths_assertIsReadable_Test.java
@@ -12,50 +12,59 @@
  */
 package org.assertj.core.internal.paths;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.error.ShouldBeReadable.shouldBeReadable;
+import static org.assertj.core.error.ShouldBeWritable.shouldBeWritable;
 import static org.assertj.core.error.ShouldExist.shouldExist;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
 import static org.assertj.core.util.FailureMessages.actualIsNull;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.assertj.core.internal.PathsBaseTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
 
-class Paths_assertIsReadable_Test extends MockPathsBaseTest {
+class Paths_assertIsReadable_Test extends PathsBaseTest {
 
   @Test
   void should_fail_if_actual_is_null() {
-    assertThatExceptionOfType(AssertionError.class).isThrownBy(() -> paths.assertIsReadable(info, null))
-                                                   .withMessage(actualIsNull());
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsReadable(info, null));
+    // THEN
+    then(error).hasMessage(actualIsNull());
   }
 
   @Test
-  void should_fail_with_should_exist_error_if_actual_does_not_exist() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsReadable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldExist(actual));
+  void should_fail_if_actual_does_not_exist() {
+    // GIVEN
+    Path actual = tempDir.resolve("non-existent");
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsReadable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldExist(actual).create());
   }
 
   @Test
-  void should_fail_if_actual_exists_but_is_not_readable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isReadable(actual)).thenReturn(false);
-
-    Throwable error = catchThrowable(() -> paths.assertIsReadable(info, actual));
-
-    assertThat(error).isInstanceOf(AssertionError.class);
-    verify(failures).failure(info, shouldBeReadable(actual));
+  @DisabledOnOs(value = WINDOWS, disabledReason = "gh-FIXME")
+  void should_fail_if_actual_is_not_readable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    actual.toFile().setReadable(false);
+    // WHEN
+    AssertionError error = expectAssertionError(() -> paths.assertIsReadable(info, actual));
+    // THEN
+    then(error).hasMessage(shouldBeReadable(actual).create());
   }
 
   @Test
-  void should_succeed_if_actual_exist_and_is_readable() {
-    when(nioFilesWrapper.exists(actual)).thenReturn(true);
-    when(nioFilesWrapper.isReadable(actual)).thenReturn(true);
+  void should_pass_if_actual_is_readable() throws IOException {
+    // GIVEN
+    Path actual = Files.createFile(tempDir.resolve("actual"));
+    // WHEN/THEN
     paths.assertIsReadable(info, actual);
   }
 