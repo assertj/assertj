ca3ba6311bc3fbb96243f15de9da2bfaa24bbfb3
Stefano Cordio <stefano.cordio@gmail.com>
2021-08-18 22:19:34 +0200

    Fix javadoc warning

--- src/main/java/org/assertj/core/api/WithAssumptions.java
+++ src/main/java/org/assertj/core/api/WithAssumptions.java
@@ -206,6 +206,7 @@ public interface WithAssumptions {
   /**
    * Creates a new instance of <code>{@link ThrowableAssert}</code> assumption.
    *
+   * @param <T> the type of the actual value.
    * @param actual the actual value.
    * @return the created assumption for assertion object.
    * @since 2.9.0 / 3.9.0
