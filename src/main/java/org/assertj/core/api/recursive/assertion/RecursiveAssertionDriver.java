e90a6c298808484fc4e2f11cd82c444a773bc9d8
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-11-01 13:01:22 +0100

    Added some basic recursion support with cycle detection

--- src/main/java/org/assertj/core/api/recursive/assertion/RecursiveAssertionDriver.java
+++ src/main/java/org/assertj/core/api/recursive/assertion/RecursiveAssertionDriver.java
@@ -1,5 +1,18 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2021 the original author or authors.
+ */
 package org.assertj.core.api.recursive.assertion;
 
+import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.util.Lists.list;
 import static org.assertj.core.util.Sets.newHashSet;
 
@@ -9,6 +22,8 @@ import java.util.Set;
 import java.util.function.Predicate;
 
 import org.assertj.core.api.recursive.FieldLocation;
+import org.assertj.core.internal.Objects;
+import org.assertj.core.util.introspection.PropertyOrFieldSupport;
 
 public class RecursiveAssertionDriver {
 
@@ -27,11 +42,18 @@ public class RecursiveAssertionDriver {
 
   private void assertRecursively(Predicate<Object> predicate, Object node, FieldLocation fieldLocation) {
     // TODO 0: Add object id to black set
+    boolean nodeWasAlreadyBlack = markNodeAsBlack(node);
+    if (nodeWasAlreadyBlack) return;
+    
     // TODO 1: Check conditions that should cause us to ignore this field (ignore by name, type, null...)
     doTheActualAssertionAndRegisterInCaseOfFailure(predicate, node, fieldLocation);
     // TODO 3: Check recursive conditions
     // TODO 4: Check for map/collections/arrays/optionals
     // TODO 5: Make the recursive call for all applicable fields
+    Set<String> namesOfFieldsInNode = Objects.getFieldsNames(node.getClass());
+    namesOfFieldsInNode.stream()
+    .map(name -> tuple(name, PropertyOrFieldSupport.EXTRACTION.getSimpleValue(name, node)))
+    .forEach(tuple -> assertRecursively(predicate, tuple.getByIndexAndType(1, Object.class), fieldLocation.field(tuple.getByIndexAndType(0, String.class))));
   }
 
   private void doTheActualAssertionAndRegisterInCaseOfFailure(Predicate<Object> predicate, Object node, FieldLocation fieldLocation) {
@@ -39,5 +61,31 @@ public class RecursiveAssertionDriver {
       fieldsThatFailedTheAssertion.add(fieldLocation);
     }
   }
+  
+  private boolean markNodeAsBlack(Object node) {
+    String objectId = identityToString(node);
+    boolean nodeWasAlreadyBlack = !markedBlackSet.add(objectId);
+    return nodeWasAlreadyBlack;
+  }
+  
+  /*
+   * This is taken verbatim from org.apache.commons.lang3.ObjectUtils .
+   * 
+   * It would be much cleaner if we would be allowed to use ObjectUtils directly.
+   */
+  private String identityToString(final Object object) {
+    if (object == null) {
+        return null;
+    }
+    final String name = object.getClass().getName();
+    final String hexString = Integer.toHexString(System.identityHashCode(object));
+    final StringBuilder builder = new StringBuilder(name.length() + 1 + hexString.length());
+    // @formatter:off
+    builder.append(name)
+          .append('@')
+          .append(hexString);
+    // @formatter:on
+    return builder.toString();
+}
   
 }
