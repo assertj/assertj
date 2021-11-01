66cbc5ae64e8f9af1933e6adcf92fa278b59a74d
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-11-01 22:40:47 +0100

    Added handling for primitive fields

---
+++ src/test/java/org/assertj/core/api/recursive/assertion/PrimitiveReflectionTest.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2021 the original author or authors.
+ */
+package org.assertj.core.api.recursive.assertion;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class PrimitiveReflectionTest {
+  
+  @Test
+  public void testPrimitiveReflectionRules() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    class MyClass {
+      public int m = 0;
+      private float f = 0.0F;
+      public String s = "Hello World";
+    }
+    
+    MyClass mc = new MyClass();
+    Field mField = MyClass.class.getField("m");
+    Object object = mField.get(mc);
+    System.out.println(mField.getType().isPrimitive());
+    System.out.println(mField.getType().getName());
+    Class<?> oClass = object.getClass();
+    System.out.println(oClass.isPrimitive());
+    System.out.println(oClass.getName());
+  }
+}
