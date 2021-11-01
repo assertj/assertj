d737112acd8a8613812461bfae98ce6aea20f15b
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-11-01 22:40:47 +0100

    Added handling for primitive fields

--- src/main/java/org/assertj/core/api/recursive/assertion/RecursiveAssertionDriver.java
+++ src/main/java/org/assertj/core/api/recursive/assertion/RecursiveAssertionDriver.java
@@ -16,6 +16,7 @@ import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.util.Lists.list;
 import static org.assertj.core.util.Sets.newHashSet;
 
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -23,6 +24,7 @@ import java.util.function.Predicate;
 
 import org.assertj.core.api.recursive.FieldLocation;
 import org.assertj.core.internal.Objects;
+import org.assertj.core.util.introspection.FieldSupport;
 import org.assertj.core.util.introspection.PropertyOrFieldSupport;
 
 public class RecursiveAssertionDriver {
@@ -30,21 +32,21 @@ public class RecursiveAssertionDriver {
   private final Set<String> markedBlackSet = newHashSet();
   private final List<FieldLocation> fieldsThatFailedTheAssertion = list();
   private final RecursiveAssertionConfiguration configuration;
-  
+
   public RecursiveAssertionDriver(RecursiveAssertionConfiguration configuration) {
     this.configuration = configuration;
   }
 
   public List<FieldLocation> assertOverObjectGraph(Predicate<Object> predicate, Object graphNode) {
-    assertRecursively(predicate, graphNode, FieldLocation.rootFieldLocation());
-    return Collections.unmodifiableList(fieldsThatFailedTheAssertion); 
+    assertRecursively(predicate, graphNode, graphNode.getClass(), FieldLocation.rootFieldLocation());
+    return Collections.unmodifiableList(fieldsThatFailedTheAssertion);
   }
 
-  private void assertRecursively(Predicate<Object> predicate, Object node, FieldLocation fieldLocation) {
-    // TODO 0: Add object id to black set
+  private void assertRecursively(Predicate<Object> predicate, Object node, Class<?> nodeType, FieldLocation fieldLocation) {
     boolean nodeWasAlreadyBlack = markNodeAsBlack(node);
     if (nodeWasAlreadyBlack) return;
-    
+    if (nodeType.isPrimitive() && !configuration.getAssertOverPrimitiveFields()) return;
+
     // TODO 1: Check conditions that should cause us to ignore this field (ignore by name, type, null...)
     doTheActualAssertionAndRegisterInCaseOfFailure(predicate, node, fieldLocation);
     // TODO 3: Check recursive conditions
@@ -52,22 +54,29 @@ public class RecursiveAssertionDriver {
     // TODO 5: Make the recursive call for all applicable fields
     Set<String> namesOfFieldsInNode = Objects.getFieldsNames(node.getClass());
     namesOfFieldsInNode.stream()
-    .map(name -> tuple(name, PropertyOrFieldSupport.EXTRACTION.getSimpleValue(name, node)))
-    .forEach(tuple -> assertRecursively(predicate, tuple.getByIndexAndType(1, Object.class), fieldLocation.field(tuple.getByIndexAndType(0, String.class))));
+                       .map(name -> tuple(name, PropertyOrFieldSupport.EXTRACTION.getSimpleValue(name, node),
+                                          getFieldType(name, node)))
+                       .forEach(tuple -> {
+                         String fieldName = tuple.getByIndexAndType(0, String.class);
+                         Object nextNodeValue = tuple.getByIndexAndType(1, Object.class);
+                         Class<?> nextNodeType = tuple.getByIndexAndType(2, Class.class);
+                         assertRecursively(predicate, nextNodeValue, nextNodeType, fieldLocation.field(fieldName));
+                       });
   }
 
-  private void doTheActualAssertionAndRegisterInCaseOfFailure(Predicate<Object> predicate, Object node, FieldLocation fieldLocation) {
+  private void doTheActualAssertionAndRegisterInCaseOfFailure(Predicate<Object> predicate, Object node,
+                                                              FieldLocation fieldLocation) {
     if (!predicate.test(node)) {
       fieldsThatFailedTheAssertion.add(fieldLocation);
     }
   }
-  
+
   private boolean markNodeAsBlack(Object node) {
     String objectId = identityToString(node);
     boolean nodeWasAlreadyBlack = !markedBlackSet.add(objectId);
     return nodeWasAlreadyBlack;
   }
-  
+
   /*
    * This is taken verbatim from org.apache.commons.lang3.ObjectUtils .
    * 
@@ -75,17 +84,28 @@ public class RecursiveAssertionDriver {
    */
   private String identityToString(final Object object) {
     if (object == null) {
-        return null;
+      return null;
     }
     final String name = object.getClass().getName();
     final String hexString = Integer.toHexString(System.identityHashCode(object));
     final StringBuilder builder = new StringBuilder(name.length() + 1 + hexString.length());
     // @formatter:off
     builder.append(name)
-          .append('@')
-          .append(hexString);
+           .append('@')
+           .append(hexString);
     // @formatter:on
     return builder.toString();
-}
-  
+  }
+
+  private Class<?> getFieldType(String name, Object node) {
+    try {
+      return node.getClass().getDeclaredField(name).getType();
+    } catch (NoSuchFieldException | SecurityException e) {
+      throw new IllegalStateException(
+              String.format("Could not find field %s on class %s, even though its name was retrieved from the class earlier", 
+                            name, 
+                            node.getClass().getCanonicalName()), e);
+    }
+  }
+
 }
