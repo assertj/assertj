8679d88d009cc5c081b49d200c6c7f342ddddf5b
Stefano Cordio <stefano.cordio@gmail.com>
2021-12-14 01:55:19 +0100

    Fix test class name

--- src/test/java/org/assertj/core/error/ShouldBeUnmodifiable_create_Test.java
+++ src/test/java/org/assertj/core/error/ShouldBeUnmodifiable_create_Test.java
@@ -20,7 +20,7 @@ import static org.assertj.core.presentation.StandardRepresentation.STANDARD_REPR
 import org.assertj.core.internal.TestDescription;
 import org.junit.jupiter.api.Test;
 
-class ShouldBeUnmodifiableTest {
+class ShouldBeUnmodifiable_create_Test {
 
   @Test
   void should_create_error_message_with_String() {
