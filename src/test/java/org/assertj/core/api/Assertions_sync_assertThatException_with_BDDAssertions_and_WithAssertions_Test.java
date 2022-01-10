ca979b931d0f946f3320347b3dd9950b44c6b605
minami.yoshihiko <nannany@hey.jp>
2022-01-10 15:03:14 +0900

    implement sync test

---
+++ src/test/java/org/assertj/core/api/Assertions_sync_assertThatException_with_BDDAssertions_and_WithAssertions_Test.java
@@ -0,0 +1,28 @@
+package org.assertj.core.api;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Assertions_sync_assertThatException_with_BDDAssertions_and_WithAssertions_Test extends BaseAssertionsTest {
+
+  @Test
+  void Assertions_assertThatException_and_BDDAssertions_thenException_should_be_the_same() {
+    Method[] assertThatExceptionInAssertions = findMethodsWithName(Assertions.class, "assertThatException");
+    Method[] thenExceptionInBDDAssertions = findMethodsWithName(BDDAssertions.class, "thenException");
+
+    assertThat(thenExceptionInBDDAssertions).usingElementComparator(IGNORING_DECLARING_CLASS_AND_METHOD_NAME)
+      .containsExactlyInAnyOrder(assertThatExceptionInAssertions);
+  }
+
+  @Test
+  void Assertions_assertThatException_and_WithAssertions_assertThatException_should_be_the_same() {
+    Method[] assertThatExceptionInAssertions = findMethodsWithName(Assertions.class, "assertThatException");
+    Method[] assertThatExceptionInWithAssertions = findMethodsWithName(WithAssertions.class, "assertThatException");
+
+    assertThat(assertThatExceptionInWithAssertions).usingElementComparator(IGNORING_DECLARING_CLASS_ONLY)
+      .containsExactlyInAnyOrder(assertThatExceptionInAssertions);
+  }
+}
---
+++ src/test/java/org/assertj/core/api/Assertions_sync_assertThatIndexOutOfBoundsException_with_BDDAssertions_and_WithAssertions_Test.java
@@ -0,0 +1,28 @@
+package org.assertj.core.api;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Assertions_sync_assertThatIndexOutOfBoundsException_with_BDDAssertions_and_WithAssertions_Test extends BaseAssertionsTest {
+
+  @Test
+  void Assertions_assertThatIndexOutOfBoundsException_and_BDDAssertions_thenIndexOutOfBoundsException_should_be_the_same() {
+    Method[] assertThatIndexOutOfBoundsExceptionInAssertions = findMethodsWithName(Assertions.class, "assertThatIndexOutOfBoundsException");
+    Method[] thenIndexOutOfBoundsExceptionInBDDAssertions = findMethodsWithName(BDDAssertions.class, "thenIndexOutOfBoundsException");
+
+    assertThat(thenIndexOutOfBoundsExceptionInBDDAssertions).usingElementComparator(IGNORING_DECLARING_CLASS_AND_METHOD_NAME)
+      .containsExactlyInAnyOrder(assertThatIndexOutOfBoundsExceptionInAssertions);
+  }
+
+  @Test
+  void Assertions_assertThatIndexOutOfBoundsException_and_WithAssertions_assertThatIndexOutOfBoundsException_should_be_the_same() {
+    Method[] assertThatIndexOutOfBoundsExceptionInAssertions = findMethodsWithName(Assertions.class, "assertThatIndexOutOfBoundsException");
+    Method[] assertThatIndexOutOfBoundsExceptionInWithAssertions = findMethodsWithName(WithAssertions.class, "assertThatIndexOutOfBoundsException");
+
+    assertThat(assertThatIndexOutOfBoundsExceptionInWithAssertions).usingElementComparator(IGNORING_DECLARING_CLASS_ONLY)
+      .containsExactlyInAnyOrder(assertThatIndexOutOfBoundsExceptionInAssertions);
+  }
+}
