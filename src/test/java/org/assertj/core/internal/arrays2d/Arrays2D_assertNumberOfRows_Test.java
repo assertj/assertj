953c339ab17901aa5ce5a96671e94543cb4833db
Stefano Cordio <stefano_cordio@epam.com>
2022-03-05 23:17:56 +0100

    Add missing license headers

--- src/test/java/org/assertj/core/internal/arrays2d/Arrays2D_assertNumberOfRows_Test.java
+++ src/test/java/org/assertj/core/internal/arrays2d/Arrays2D_assertNumberOfRows_Test.java
@@ -1,16 +1,28 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2022 the original author or authors.
+ */
 package org.assertj.core.internal.arrays2d;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.error.ShouldHaveDimensions.shouldHaveFirstDimension;
 import static org.assertj.core.error.ShouldNotBeNull.shouldNotBeNull;
 import static org.assertj.core.test.TestData.someInfo;
 import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class Arrays2D_assertNumberOfRows_Test extends Arrays2D_BaseTest {
 
--- src/test/java/org/assertj/core/internal/boolean2darrays/Boolean2DArrays_assertNumberOfRows_Test.java
+++ src/test/java/org/assertj/core/internal/boolean2darrays/Boolean2DArrays_assertNumberOfRows_Test.java
@@ -1,9 +1,21 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2022 the original author or authors.
+ */
 package org.assertj.core.internal.boolean2darrays;
+
+import static org.mockito.Mockito.verify;
 
 import org.assertj.core.internal.Boolean2DArraysBaseTest;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.Mockito.verify;
 
 class Boolean2DArrays_assertNumberOfRows_Test extends Boolean2DArraysBaseTest {
 
