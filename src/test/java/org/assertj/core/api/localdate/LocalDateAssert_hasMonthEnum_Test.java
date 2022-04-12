cd0c6f74f1fc2e92a55e59c534e05f95b2060127
nith2001 <nith2001@uw.edu>
2022-04-12 13:20:27 -0700

    Wrote hasMonth(enum) tests

---
+++ src/test/java/org/assertj/core/api/localdate/LocalDateAssert_hasMonthEnum_Test.java
@@ -0,0 +1,54 @@
+package org.assertj.core.api.localdate;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldHaveDateField.shouldHaveDateField;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+
+public class LocalDateAssert_hasMonthEnum_Test {
+
+  @Test
+  void should_pass_if_actual_is_in_given_month() {
+    // GIVEN
+    LocalDate actual = LocalDate.of(2021, 2, 22);
+    // WHEN/THEN
+    then(actual).hasMonth(Month.FEBRUARY);
+  }
+
+  @Test
+  void should_pass_if_actual_is_in_given_month_from_localdate() {
+    // GIVEN
+    LocalDate actual = LocalDate.of(2021, 2, 22);
+    LocalDate other = LocalDate.of(2022, 2, 21);
+    // WHEN/THEN
+    then(actual).hasMonth(other.getMonth());
+  }
+
+  @Test
+  void should_fail_if_actual_is_not_in_given_month() {
+    // GIVEN
+    LocalDate actual = LocalDate.of(2022, 1, 1);
+    Month wrongMonth = Month.DECEMBER;
+    // WHEN
+    AssertionError assertionError = expectAssertionError(() -> assertThat(actual).hasMonth(wrongMonth));
+    // THEN
+    then(assertionError).hasMessage(shouldHaveDateField(actual, "month", wrongMonth.getValue()).create());
+  }
+
+  @Test
+  void should_fail_if_actual_is_not_in_given_month() {
+    // GIVEN
+    LocalDate actual = LocalDate.of(2022, 1, 1);
+    LocalDate other = LocalDate.of(2022, 2, 1);
+    // WHEN
+    AssertionError assertionError = expectAssertionError(() -> assertThat(actual).hasMonth(other.getMonth()));
+    // THEN
+    then(assertionError).hasMessage(shouldHaveDateField(actual, "month", other.getMonth().getValue()).create());
+  }
+
+}
---
+++ src/test/java/org/assertj/core/api/localdate/LocalDateAssert_hasMonthInt_Test.java
@@ -0,0 +1,4 @@
+package org.assertj.core.api.localdate;
+
+public class LocalDateAssert_hasMonthInt_Test {
+}
