7dcfcef16a7d0f872b409d2f6bba0f848253936d
nutelllia <natalia.struharova@icloud.com>
2022-04-28 16:16:36 +0200

    Increase test coverage for internal and api package Fix #2543

---
+++ src/test/java/org/assertj/core/internal/booleanarrays/BooleanArrays_assertContainsSubsequence_Test.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2022 the original author or authors.
+ */
+package org.assertj.core.internal.booleanarrays;
+
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldContainSubsequence.shouldContainSubsequence;
+import static org.assertj.core.internal.ErrorMessages.valuesToLookForIsNull;
+import static org.assertj.core.test.BooleanArrays.arrayOf;
+import static org.assertj.core.test.BooleanArrays.emptyArray;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+import static org.assertj.core.util.FailureMessages.actualIsNull;
+import static org.mockito.Mockito.verify;
+
+import org.assertj.core.api.AssertionInfo;
+import org.assertj.core.internal.BooleanArrays;
+import org.assertj.core.internal.BooleanArraysBaseTest;
+import org.assertj.core.internal.StandardComparisonStrategy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for <code>{@link BooleanArrays#assertContainsSubsequence(AssertionInfo, boolean[], boolean[])}</code>.
+ *
+ * @author Natália Struharová
+ */
+public class BooleanArrays_assertContainsSubsequence_Test extends BooleanArraysBaseTest {
+
+  @Override
+  @BeforeEach
+  public void setUp() {
+    super.setUp();
+    actual = arrayOf(true, false, false, true);
+  }
+
+  @Test
+  void should_fail_if_actual_is_null() {
+    // WHEN
+    AssertionError assertionError = expectAssertionError(() -> arrays.assertContainsSubsequence(INFO, null, arrayOf(true)));
+    // THEN
+    then(assertionError).hasMessage(actualIsNull());
+  }
+
+  @Test
+  void should_throw_error_if_subsequence_is_null() {
+    // WHEN/THEN
+    assertThatNullPointerException().isThrownBy(() -> arrays.assertContainsSubsequence(INFO, actual, null))
+                                    .withMessage(valuesToLookForIsNull());
+  }
+
+  @Test
+  void should_pass_if_actual_and_given_values_are_empty() {
+    // GIVEN
+    actual = emptyArray();
+    // WHEN/THEN
+    arrays.assertContainsSubsequence(INFO, actual, emptyArray());
+  }
+
+  @Test
+  void should_pass_if_actual_contains_given_subsequence() {
+    // GIVEN
+    boolean[] subsequence = arrayOf(true, false);
+    // WHEN/THEN
+    arrays.assertContainsSubsequence(INFO, actual, subsequence);
+  }
+
+  @Test
+  void should_fail_if_actual_contains_first_elements_of_subsequence_but_not_whole_subsequence() {
+    // GIVEN
+    actual = arrayOf(false, false);
+    boolean[] subsequence = { false, true };
+    // WHEN
+    expectAssertionError(() -> arrays.assertContainsSubsequence(INFO, actual, subsequence));
+    // THEN
+    verify(failures).failure(INFO, shouldContainSubsequence(actual, subsequence, 1, StandardComparisonStrategy.instance()));
+  }
+}
---
+++ src/test/java/org/assertj/core/internal/iterators/Iterators_assertHasNext_Test.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2022 the original author or authors.
+ */
+package org.assertj.core.internal.iterators;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldHaveNext.shouldHaveNext;
+import static org.assertj.core.error.ShouldNotBeNull.shouldNotBeNull;
+import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
+import static org.assertj.core.util.Lists.list;
+import static org.mockito.Mockito.verify;
+
+import java.util.Iterator;
+
+import org.assertj.core.api.AssertionInfo;
+import org.assertj.core.internal.Iterators;
+import org.assertj.core.internal.IteratorsBaseTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for <code>{@link Iterators#assertHasNext(AssertionInfo, Iterator)}</code>.
+ *
+ * @author Natália Struharová
+ */
+public class Iterators_assertHasNext_Test extends IteratorsBaseTest {
+
+  @Test
+  void should_pass_if_iterator_has_next() {
+    // WHEN
+    Iterator<String> actual = list("Luke", "Yoda", "Leia").iterator();
+    // THEN
+    iterators.assertHasNext(INFO, actual);
+  }
+
+  @Test
+  void should_fail_if_iterator_has_no_next() {
+    // GIVEN
+    Iterator<String> actual = list("Luke").iterator();
+    actual.next();
+    // WHEN
+    expectAssertionError(() -> iterators.assertHasNext(INFO, actual));
+    // THEN
+    verify(failures).failure(INFO, shouldHaveNext());
+  }
+
+  @Test
+  void should_fail_if_actual_is_null() {
+    // GIVEN
+    Iterator<String> actual = null;
+    // WHEN
+    AssertionError error = expectAssertionError(() -> iterators.assertHasNext(INFO, actual));
+    // THEN
+    then(error).hasMessage(shouldNotBeNull().create());
+  }
+
+}
