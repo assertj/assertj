d3c2e26dde62a372dcb9105d1e3f0bc3337ebdaa
Stefano Cordio <stefano_cordio@epam.com>
2022-05-14 22:22:22 +0200

    Keep `ShouldBeGreater` binary compatible

--- src/main/java/org/assertj/core/error/ShouldBeGreater.java
+++ src/main/java/org/assertj/core/error/ShouldBeGreater.java
@@ -31,7 +31,7 @@ public class ShouldBeGreater extends BasicErrorMessageFactory {
    * @param other the value used in the failed assertion to compare the actual value to.
    * @return the created {@code ErrorMessageFactory}.
    */
-  public static <T> ErrorMessageFactory shouldBeGreater(T actual, T other) {
+  public static <T> ErrorMessageFactory shouldBeGreater(Comparable<? super T> actual, Comparable<? super T> other) {
     return new ShouldBeGreater(actual, other, StandardComparisonStrategy.instance());
   }
 
@@ -44,11 +44,13 @@ public class ShouldBeGreater extends BasicErrorMessageFactory {
    * @param comparisonStrategy the {@link ComparisonStrategy} used to evaluate assertion.
    * @return the created {@code ErrorMessageFactory}.
    */
-  public static <T> ErrorMessageFactory shouldBeGreater(T actual, T other, ComparisonStrategy comparisonStrategy) {
+  public static <T> ErrorMessageFactory shouldBeGreater(Comparable<? super T> actual, Comparable<? super T> other,
+                                                        ComparisonStrategy comparisonStrategy) {
     return new ShouldBeGreater(actual, other, comparisonStrategy);
   }
 
-  private <T> ShouldBeGreater(T actual, T other, ComparisonStrategy comparisonStrategy) {
+  private <T> ShouldBeGreater(Comparable<? super T> actual, Comparable<? super T> other, ComparisonStrategy comparisonStrategy) {
     super("%nExpecting actual:%n  %s%nto be greater than:%n  %s%n%s", actual, other, comparisonStrategy);
   }
+
 }
