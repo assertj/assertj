43058a516cf4a2c0ef1affd0e01f78e03e907caa
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-06-05 12:01:43 +0100

    Fix spurious Future tests failure on Windows runners

--- src/test/java/org/assertj/core/api/future/AbstractFutureTest.java
+++ src/test/java/org/assertj/core/api/future/AbstractFutureTest.java
@@ -12,23 +12,63 @@
  */
 package org.assertj.core.api.future;
 
+import static java.lang.String.format;
+
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 abstract class AbstractFutureTest {
-
   protected ExecutorService executorService;
+  private final ThreadFactory factory;
+
+  AbstractFutureTest() {
+    factory = new FutureTestThreadFactory();
+  }
 
   @BeforeEach
   void beforeEach() {
-    executorService = Executors.newSingleThreadExecutor();
+    executorService = Executors.newSingleThreadExecutor(factory);
   }
 
   @AfterEach
   void afterEach() {
     executorService.shutdownNow();
+  }
+
+  private class FutureTestThreadFactory implements ThreadFactory, UncaughtExceptionHandler {
+    private final Logger logger;
+    private final AtomicInteger count;
+
+    private FutureTestThreadFactory() {
+      logger = LoggerFactory.getLogger(AbstractFutureTest.this.getClass());
+      count = new AtomicInteger(0);
+    }
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+      Thread thread = new Thread(runnable);
+      thread.setName(AbstractFutureTest.this.getClass().getName() + "." + count.incrementAndGet());
+      thread.setDaemon(true);
+      thread.setPriority(Thread.MAX_PRIORITY);
+      thread.setUncaughtExceptionHandler(this);
+      thread.setContextClassLoader(Thread.currentThread().getContextClassLoader());
+      return thread;
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+      logger.info(
+        ex,
+        () -> format("Thread %s [%s] threw an exception", thread.getName(), thread.getId())
+      );
+    }
   }
 }
