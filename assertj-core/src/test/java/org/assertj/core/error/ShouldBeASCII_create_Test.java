88e252e5a9267b6e0a3c0e0d521d065a59618039
Joel Costigliola <joel.costigliola@gmail.com>
2022-08-06 22:38:49 +1200

    Add additional assertions for `CharSequence` (#2731)
    Add additional assertions for CharSequence: isAlphabetic, isAlphanumeric, isASCII, isHexadecimal, isVisible, isPrintable
    
    Fix #2472
    
    Co-authored-by: neshka <neshka.dantinor@gmail.com>

---
+++ assertj-core/src/test/java/org/assertj/core/error/ShouldBeASCII_create_Test.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2022 the original author or authors.
+ */
+package org.assertj.core.error;
+
+
+import static java.lang.String.format;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.ShouldBeASCII.shouldBeASCII;
+import static org.assertj.core.presentation.StandardRepresentation.STANDARD_REPRESENTATION;
+
+import org.assertj.core.description.TextDescription;
+import org.junit.jupiter.api.Test;
+
+class ShouldBeASCII_create_Test {
+
+  @Test
+  void should_create_error_message_for_non_ASCII_character() {
+    // WHEN
+    String message = shouldBeASCII("\u2303").create(new TextDescription("Test"), STANDARD_REPRESENTATION);
+    // THEN
+    then(message).isEqualTo(format("[Test] %nExpecting \"\u2303\" to be ASCII"));
+  }
+
+  @Test
+  void should_create_error_message_for_strings_with_ASCII_character() {
+    // WHEN
+    String message = shouldBeASCII("123\u230300abc").create(new TextDescription("Test"), STANDARD_REPRESENTATION);
+    // THEN
+    then(message).isEqualTo(format("[Test] %nExpecting \"123\u230300abc\" to be ASCII"));
+  }
+}
