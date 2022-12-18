dee396bb2bd6004cbc15550d191ca87dd9a3a36b
Dirk Toewe <DirkToewe@GoogleMail.com>
2022-12-18 08:53:55 +0100

    Comparables::assertIsBetween String alloc. removed
    Comparables::checkBoundsValidity, which is used by Comparables::assertIsBetween, does build an error message String even if the assertion does not fail. This results in noticeable overhead. The JVM can hardly eliminate the String building since `start.toString()`, `operator.toString()` and `end.toString()` may have side effects. This commit moves the String building inside a `Supplier<String>` which is only evaluated on failure.

--- assertj-core/src/test/java/org/assertj/core/util/Preconditions_checkArgument_Test.java
+++ assertj-core/src/test/java/org/assertj/core/util/Preconditions_checkArgument_Test.java
@@ -12,9 +12,13 @@
  */
 package org.assertj.core.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
 class Preconditions_checkArgument_Test {
 
@@ -28,5 +32,23 @@ class Preconditions_checkArgument_Test {
   @Test
   void should_not_throw_if_expression_is_true() {
     Preconditions.checkArgument(true, "Invalid parameter %s", "foo");
+  }
+
+  @Test
+  void should_call_message_supplier_once_if_expression_is_false() {
+    AtomicInteger callCount = new AtomicInteger();
+    String str = "Super secret message!!!";
+    Supplier<String> msg = () -> {
+      int nCalls = callCount.incrementAndGet();
+      assertThat(nCalls).isEqualTo(1);
+      return str;
+    };
+    assertThatIllegalArgumentException().isThrownBy(() -> Preconditions.checkArgument(false, msg)).withMessage(str);
+    assertThat(callCount.get()).isEqualTo(1);
+  }
+
+  @Test
+  void should_never_call_message_supplier_if_expression_is_true() {
+    Preconditions.checkArgument(true, () -> {throw new AssertionError();});
   }
 }
