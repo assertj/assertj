e8bfbd62a8ec0126b8b8036b0f74f64b73301fe1
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2023-01-09 17:48:33 +0000

    Fixes

--- assertj-core/src/test/java/org/assertj/core/api/keystore/KeyStoreAssert_containsAlias_Test.java
+++ assertj-core/src/test/java/org/assertj/core/api/keystore/KeyStoreAssert_containsAlias_Test.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2023 the original author or authors.
+ */
 package org.assertj.core.api.keystore;
 
 import org.junit.jupiter.api.Test;
@@ -7,7 +19,7 @@ import java.security.KeyStoreSpi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
-import static org.assertj.core.api.keystore.KeyStoreFactory.newKeyStore;
+import org.assertj.core.test.KeyStores;
 import static org.assertj.core.error.KeyStoreShouldContainAlias.shouldContainAlias;
 import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -22,7 +34,7 @@ public class KeyStoreAssert_containsAlias_Test {
     KeyStoreSpi keyStoreSpiMock = mock(KeyStoreSpi.class);
     when(keyStoreSpiMock.engineContainsAlias(anyString())).thenReturn(false);
     // AND
-    KeyStore actual = newKeyStore(keyStoreSpiMock);
+    KeyStore actual = KeyStores.newKeyStore(keyStoreSpiMock);
     // WHEN
     AssertionError assertionError = expectAssertionError(() -> assertThat(actual).containsAlias("foo"));
     // THEN
@@ -35,7 +47,7 @@ public class KeyStoreAssert_containsAlias_Test {
     KeyStoreSpi keyStoreSpiMock = mock(KeyStoreSpi.class);
     when(keyStoreSpiMock.engineContainsAlias(anyString())).thenReturn(true);
     // AND
-    KeyStore actual = newKeyStore(keyStoreSpiMock);
+    KeyStore actual = KeyStores.newKeyStore(keyStoreSpiMock);
     // WHEN/THEN
     then(actual).containsAlias("foo");
   }
