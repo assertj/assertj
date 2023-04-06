c577e5efeadd555fb31460eb5496331aa12e7b85
Stefano Cordio <stefano_cordio@epam.com>
2023-04-06 09:42:57 +0200

    Add license header

--- assertj-core/src/test/java/org/assertj/core/error/ShouldHaveRecordComponents_create_Test.java
+++ assertj-core/src/test/java/org/assertj/core/error/ShouldHaveRecordComponents_create_Test.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2023 the original author or authors.
+ */
 package org.assertj.core.error;
 
 import static java.lang.String.format;
@@ -20,9 +32,9 @@ class ShouldHaveRecordComponents_create_Test {
     then(message).isEqualTo(format("[TEST] %n" +
                                    "Expecting%n" +
                                    "  java.lang.Object%n" +
-                                   "to have the following record components:\n" +
-                                   "  [\"component1\", \"component2\"]\n" +
-                                   "but it doesn't have:\n" +
+                                   "to have the following record components:%n" +
+                                   "  [\"component1\", \"component2\"]%n" +
+                                   "but it doesn't have:%n" +
                                    "  [\"component2\"]"));
   }
 
