b380f396e20c607358104e04b9c2d856bedf96d2
Harry Dent <harry.j.e.dent@gmail.com>
2023-04-28 22:22:26 +0100

    Add group methods to MatcherAssert
    Fix #3042

---
+++ assertj-core/src/test/java/org/assertj/core/error/MatcherShouldHaveGroup_create_Test.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.assertj.core.error;
+
+import static java.lang.String.format;
+import static java.util.regex.Pattern.compile;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.error.MatcherShouldHaveGroup.shouldHaveGroup;
+import static org.assertj.core.presentation.StandardRepresentation.STANDARD_REPRESENTATION;
+
+import java.util.regex.Matcher;
+
+import org.assertj.core.internal.TestDescription;
+import org.junit.jupiter.api.Test;
+
+class MatcherShouldHaveGroup_create_Test {
+
+  @Test
+  void should_create_error_message_with_int_group_identifier() {
+    // GIVEN
+    Matcher actual = compile("a*").matcher("abc");
+    // WHEN
+    String message = shouldHaveGroup(actual, 1).create(new TestDescription("Test"), STANDARD_REPRESENTATION);
+    // THEN
+    then(message).isEqualTo(format("[Test] %n"
+                                   + "Expecting java.util.regex.Matcher[pattern=a* region=0,3 lastmatch=] to have group 1"));
+  }
+
+  @Test
+  void should_create_error_message_with_string_group_identifier() {
+    // GIVEN
+    Matcher actual = compile("a*").matcher("abc");
+    // WHEN
+    String message = shouldHaveGroup(actual, "foo").create(new TestDescription("Test"), STANDARD_REPRESENTATION);
+    // THEN
+    then(message).isEqualTo(format("[Test] %n"
+                                   + "Expecting java.util.regex.Matcher[pattern=a* region=0,3 lastmatch=] to have group \"foo\""));
+  }
+}
