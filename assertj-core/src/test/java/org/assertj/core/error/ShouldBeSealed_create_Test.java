6f12045c5ff8710beb274355c54d5c2e07d5ccae
Stefano Cordio <stefano_cordio@epam.com>
2023-06-14 13:41:09 +0200

    Add `isSealed` and `isNotSealed` to `Class` assertions (#3080)

--- assertj-core/src/test/java/org/assertj/core/error/ShouldBeSealed_create_Test.java
+++ assertj-core/src/test/java/org/assertj/core/error/ShouldBeSealed_create_Test.java
@@ -14,41 +14,37 @@ package org.assertj.core.error;
 
 import static java.lang.String.format;
 import static org.assertj.core.api.BDDAssertions.then;
-import static org.assertj.core.error.ShouldHaveSuperclass.shouldHaveSuperclass;
+import static org.assertj.core.error.ShouldBeSealed.shouldBeSealed;
+import static org.assertj.core.error.ShouldBeSealed.shouldNotBeSealed;
 import static org.assertj.core.presentation.StandardRepresentation.STANDARD_REPRESENTATION;
 
 import org.assertj.core.internal.TestDescription;
 import org.junit.jupiter.api.Test;
 
-class ShouldHaveSuperclass_create_Test {
+class ShouldBeSealed_create_Test {
 
   @Test
-  void should_create_error_message_if_actual_has_superclass() {
+  void should_create_error_message_with_shouldBeSealed() {
     // WHEN
-    String message = shouldHaveSuperclass(String.class, Integer.class).create(new TestDescription("TEST"),
-                                                                              STANDARD_REPRESENTATION);
+    String message = shouldBeSealed(String.class).create(new TestDescription("TEST"),
+                                                         STANDARD_REPRESENTATION);
     // THEN
     then(message).isEqualTo(format("[TEST] %n" +
                                    "Expecting%n" +
                                    "  java.lang.String%n" +
-                                   "to have superclass:%n" +
-                                   "  java.lang.Integer%n" +
-                                   "but had:%n" +
-                                   "  java.lang.Object"));
+                                   "to be sealed"));
   }
 
   @Test
-  void should_create_error_message_if_actual_has_no_superclass() {
+  void should_create_error_message_with_shouldNotBeSealed() {
     // WHEN
-    String message = shouldHaveSuperclass(Object.class, Integer.class).create(new TestDescription("TEST"),
-                                                                              STANDARD_REPRESENTATION);
+    String message = shouldNotBeSealed(String.class).create(new TestDescription("TEST"),
+                                                            STANDARD_REPRESENTATION);
     // THEN
     then(message).isEqualTo(format("[TEST] %n" +
                                    "Expecting%n" +
-                                   "  java.lang.Object%n" +
-                                   "to have superclass:%n" +
-                                   "  java.lang.Integer%n" +
-                                   "but had none."));
+                                   "  java.lang.String%n" +
+                                   "not to be sealed"));
   }
 
 }
