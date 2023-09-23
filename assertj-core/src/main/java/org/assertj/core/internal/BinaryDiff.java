5768f1d3b671197eb15ed1a93adb33c33be43f06
Matthias Kraaz <matthias.kraaz@gmx.de>
2023-09-23 05:21:51 +0000

    Speed up for BinaryDiff used by hasSameBinaryContentAs for example. Fixes #3193.

--- assertj-core/src/main/java/org/assertj/core/internal/BinaryDiff.java
+++ assertj-core/src/main/java/org/assertj/core/internal/BinaryDiff.java
@@ -12,6 +12,7 @@
  */
 package org.assertj.core.internal;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -36,7 +37,7 @@ public class BinaryDiff {
 
   @VisibleForTesting
   public BinaryDiffResult diff(Path actual, byte[] expected) throws IOException {
-    try (InputStream actualStream = Files.newInputStream(actual)) {
+    try(InputStream actualStream = new BufferedInputStream(Files.newInputStream(actual))) {
       return diff(actualStream, expected);
     }
   }
