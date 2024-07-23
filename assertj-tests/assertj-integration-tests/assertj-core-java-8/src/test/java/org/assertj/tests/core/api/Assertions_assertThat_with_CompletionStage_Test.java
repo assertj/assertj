96ae7c10f62e8720d79be7ddc363b79cf489714c
Stefano Cordio <stefano_cordio@epam.com>
2024-07-23 11:42:05 +0200

    Start moving `assertj-core` tests to separate module

---
+++ assertj-tests/assertj-integration-tests/assertj-core-java-8/src/test/java/org/assertj/tests/core/api/Assertions_assertThat_with_CompletionStage_Test.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2024 the original author or authors.
+ */
+package org.assertj.tests.core.api;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.CompletionStage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Assertions_assertThat_with_CompletionStage_Test {
+
+  private CompletionStage<String> completionStage;
+
+  @BeforeEach
+  void setup() {
+    completionStage = completedFuture("done");
+  }
+
+  @Test
+  void should_create_Assert() {
+    Object assertions = assertThat(completionStage);
+    assertThat(assertions).isNotNull();
+  }
+
+}
