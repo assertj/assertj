e1279390f577d31b30095181e203127f04a31632
Jean de Leeuw <jeandeleeuw@gmail.com>
2024-09-26 08:00:19 +0200

    Add tests for capacity, limit, position, remaining and marked

--- assertj-core/src/test/java/org/assertj/core/api/buffer/Buffer_hasPosition_Test.java
+++ assertj-core/src/test/java/org/assertj/core/api/buffer/Buffer_hasPosition_Test.java
@@ -1,3 +1,15 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2024 the original author or authors.
+ */
 package org.assertj.core.api.buffer;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,7 +35,7 @@ public class Buffer_hasPosition_Test {
   public void should_fail_when_expected_position_mismatches() {
     ByteBuffer buffer = ByteBuffer.allocate(10);
     assertThatThrownBy(() -> assertThat(buffer).hasPosition(1))
-      .isInstanceOf(AssertionError.class)
-      .hasMessage(shouldHavePosition(1, 0, buffer).create());
+                                                               .isInstanceOf(AssertionError.class)
+                                                               .hasMessage(shouldHavePosition(1, 0, buffer).create());
   }
 }
