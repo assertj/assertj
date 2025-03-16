ff696a9fda54dc6030853186160a7af655c9bd89
Joel Costigliola <382613+joel-costigliola@users.noreply.github.com>
2025-03-16 18:09:26 +0100

    BREAKING CHANGE: Remove deprecated AbstractDateAssert.isEqualToIgnoringMillis

--- assertj-core/src/main/java/org/assertj/core/api/AbstractDateAssert.java
+++ assertj-core/src/main/java/org/assertj/core/api/AbstractDateAssert.java
@@ -13,7 +13,6 @@
 package org.assertj.core.api;
 
 import static java.util.Objects.requireNonNull;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.util.DateUtil.newIsoDateFormat;
 import static org.assertj.core.util.DateUtil.newIsoDateTimeFormat;
@@ -175,106 +174,6 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
    */
   public SELF isEqualTo(Instant instant) {
     return isEqualTo(dateFrom(instant));
-  }
-
-  /**
-   * Same assertion as {@link AbstractDateAssert#isEqualToIgnoringMillis(Date)} but given Date is represented as String
-   * either with one of the default supported date formats or user custom date format (set with method
-   * {@link #withDateFormat(DateFormat)}).
-   * <p>
-   * User custom date format takes precedence over the default ones.
-   * <p>
-   * Unless specified otherwise, beware that the default formats are expressed in the current local timezone.
-   * <p>
-   * Example:
-   * <pre><code class='java'> Date date1 = parseDatetimeWithMs("2003-04-26T13:01:35.998");
-   *
-   * // OK : all dates fields are the same up to milliseconds excluded
-   * assertThat().isEqualToIgnoringMillis("2003-04-26T13:01:35.997");
-   *
-   * // KO : fail as seconds fields differ
-   * assertThat("2003-04-26T13:01:35.998").isEqualToIgnoringMinutes("2003-04-26T13:01:36.998")</code></pre>
-   * <p>
-   * Defaults date format (expressed in the local time zone unless specified otherwise) are:
-   * <ul>
-   * <li><code>yyyy-MM-dd'T'HH:mm:ss.SSSX</code> (in ISO Time zone)</li>
-   * <li><code>yyyy-MM-dd'T'HH:mm:ss.SSS</code></li>
-   * <li><code>yyyy-MM-dd HH:mm:ss.SSS</code></li>
-   * <li><code>yyyy-MM-dd'T'HH:mm:ssX</code> (in ISO Time zone)</li>
-   * <li><code>yyyy-MM-dd'T'HH:mm:ss</code></li>
-   * <li><code>yyyy-MM-dd</code></li>
-   * </ul>
-   * <p>
-   * Example of valid string date representations:
-   * <ul>
-   * <li><code>2003-04-26T03:01:02.758+00:00</code></li>
-   * <li><code>2003-04-26T03:01:02.999</code></li>
-   * <li><code>2003-04-26 03:01:02.999</code></li>
-   * <li><code>2003-04-26T03:01:02+00:00</code></li>
-   * <li><code>2003-04-26T13:01:02</code></li>
-   * <li><code>2003-04-26</code></li>
-   * </ul>
-   * <p>
-   * If you are getting an {@code IllegalArgumentException} with <i>"Unknown pattern character 'X'"</i> message (some Android versions don't support it),
-   * you can explicitly specify the date format to use so that the default ones are bypassed.
-   *
-   * @param dateAsString the given Date represented as String in default or custom date format.
-   * @return this assertion object.
-   * @throws AssertionError if actual and given Date represented as String are not equal ignoring milliseconds.
-   * @throws AssertionError if the given date as String could not be converted to a Date.
-   * @deprecated Use {@link #isCloseTo(Date, long)} instead, although not exactly the same semantics,
-   * this is the right way to compare with a given precision.
-   */
-  @Deprecated(since = "3", forRemoval = true)
-  public SELF isEqualToIgnoringMillis(String dateAsString) {
-    return isEqualToIgnoringMillis(parse(dateAsString));
-  }
-
-  /**
-   * Same assertion as {@link AbstractDateAssert#isEqualToIgnoringMillis(Date)} but given Date is represented as
-   * an {@code java.time.Instant}.
-   * <p>
-   * Example:
-   * <pre><code class='java'> assertThat(new Date()).isEqualToIgnoringMillis(Instant.now());</code></pre>
-   *
-   * @param instant the given {@code Instant}.
-   * @return this assertion object.
-   * @throws AssertionError if actual {@code Date} and given {@code Instant} are not equal ignoring milliseconds.
-   * @since 3.19.0
-   * @deprecated Use {@link #isCloseTo(Instant, long)} instead, although not exactly the same semantics,
-   * this is the right way to compare with a given precision.
-   */
-  @Deprecated(since = "3", forRemoval = true)
-  public SELF isEqualToIgnoringMillis(Instant instant) {
-    return isEqualToIgnoringMillis(dateFrom(instant));
-  }
-
-  /**
-   * Same assertion as {@link AbstractAssert#isEqualTo(Object)} but given Date should not take care of milliseconds
-   * precision.
-   * <p>
-   * Example:
-   * <pre><code class='java'> Date date1 = parseDatetimeAndMs("2003-04-26T13:01:35.001");
-   * Date date2 = parseDatetimeAndMs("2003-04-26T13:01:35.002");
-   * Date date3 = parseDatetimeAndMs("2003-04-26T14:01:36.001");
-   *
-   * // OK : all dates fields are the same up to milliseconds excluded
-   * assertThat(date1).isEqualToIgnoringMillis(date2);
-   *
-   * // KO : fail as second fields differ
-   * assertThat(date1).isEqualToIgnoringMillis(date3);</code></pre>
-   *
-   * @param date the given Date represented as String in default or custom date format.
-   * @return this assertion object.
-   * @throws AssertionError if actual and given Date represented as String are not equal ignoring milliseconds.
-   * @throws AssertionError if the given date as String could not be converted to a Date.
-   * @deprecated Use {@link #isCloseTo(Date, long)} instead, although not exactly the same semantics,
-   * this is the right way to compare with a given precision.
-   */
-  @Deprecated(since = "3", forRemoval = true)
-  public SELF isEqualToIgnoringMillis(Date date) {
-    dates.assertIsEqualWithPrecision(info, actual, date, MILLISECONDS);
-    return myself;
   }
 
   /**
@@ -2740,8 +2639,6 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
    * using {@link org.assertj.core.util.DateUtil#secondOf(Date)} to get the second of a given Date.
    * <p>
    * Note that using a {@link #usingComparator(Comparator) custom comparator}  has no effect on this assertion.
-   * <p>
-   * This assertion is logically equivalent to {@link #isEqualToIgnoringMillis(Date)}.
    *
    * @param other the given {@code Date} to compare actual {@code Date} to.
    * @return this assertion object.
@@ -2787,8 +2684,6 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
    * <p>
    * If you are getting an {@code IllegalArgumentException} with <i>"Unknown pattern character 'X'"</i> message (some Android versions don't support it),
    * you can explicitly specify the date format to use so that the default ones are bypassed.
-   * <p>
-   * This assertion is logically equivalent to {@link #isEqualToIgnoringMillis(String)}.
    *
    * @param dateAsString the given Date represented as String.
    * @return this assertion object.
