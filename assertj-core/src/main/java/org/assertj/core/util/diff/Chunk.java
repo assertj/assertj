740340a26b49a955a282658bf1756c38d0c4d5bb
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2025-03-25 23:15:37 +0700

    Fix Javadoc typos (#3809)

--- assertj-core/src/main/java/org/assertj/core/util/diff/Chunk.java
+++ assertj-core/src/main/java/org/assertj/core/util/diff/Chunk.java
@@ -23,7 +23,7 @@ import java.util.List;
  * 
  * <p>
  * Text is represented as <code>Object[]</code> because the diff engine is
- * capable of handling more than plain ascci. In fact, arrays or lists of any
+ * capable of handling more than plain ascii. In fact, arrays or lists of any
  * type that implements {@link Object#hashCode hashCode()} and
  * {@link Object#equals equals()} correctly can be subject to
  * differencing using this library.
