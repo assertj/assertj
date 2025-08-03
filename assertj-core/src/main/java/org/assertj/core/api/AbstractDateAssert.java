assertj/assertj

commit 97b642a0b462fe0ad25255800f9531eaa44c4c15

Rebuild default date formats used to parse string as dates when the default timezone or the lenient flag changes.
Fix #3382

assertj-core/src/main/java/org/assertj/core/api/AbstractDateAssert.java
@@ -38,9 +38,11 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
+import org.assertj.core.configuration.Configuration;
 import org.assertj.core.configuration.ConfigurationProvider;
 import org.assertj.core.internal.ComparatorBasedComparisonStrategy;
 import org.assertj.core.internal.Dates;
@@ -70,19 +72,33 @@ import org.assertj.core.util.VisibleForTesting;
  */
 public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> extends AbstractAssert<SELF, Date> {
 
+  private static final String DATE_FORMAT_PATTERN_SHOULD_NOT_BE_NULL = "Given date format pattern should not be null";
+  private static final String DATE_FORMAT_SHOULD_NOT_BE_NULL = "Given date format should not be null";
+
   /**
    * the default DateFormat used to parse any String date representation.
    */
-  @VisibleForTesting
-  static final List<DateFormat> DEFAULT_DATE_FORMATS = list(newIsoDateTimeWithMsAndIsoTimeZoneFormat(),
-                                                            newIsoDateTimeWithMsFormat(),
-                                                            newTimestampDateFormat(),
-                                                            newIsoDateTimeWithIsoTimeZoneFormat(),
-                                                            newIsoDateTimeFormat(),
-                                                            newIsoDateFormat());
+  private static List<DateFormat> DEFAULT_DATE_FORMATS = defaultDateFormats();
+  private static boolean lenientParsing = Configuration.LENIENT_DATE_PARSING;
 
-  private static final String DATE_FORMAT_PATTERN_SHOULD_NOT_BE_NULL = "Given date format pattern should not be null";
-  private static final String DATE_FORMAT_SHOULD_NOT_BE_NULL = "Given date format should not be null";
+  @VisibleForTesting
+  static List<DateFormat> defaultDateFormats() {
+    if (DEFAULT_DATE_FORMATS == null || defaultDateFormatMustBeRecreated()) {
+      DEFAULT_DATE_FORMATS = list(newIsoDateTimeWithMsAndIsoTimeZoneFormat(lenientParsing),
+                                  newIsoDateTimeWithMsFormat(lenientParsing),
+                                  newTimestampDateFormat(lenientParsing),
+                                  newIsoDateTimeWithIsoTimeZoneFormat(lenientParsing),
+                                  newIsoDateTimeFormat(lenientParsing),
+                                  newIsoDateFormat(lenientParsing));
+    }
+    return DEFAULT_DATE_FORMATS;
+  }
+
+  private static boolean defaultDateFormatMustBeRecreated() {
+    // check default timezone or lenient flag changes, only check one date format since all are configured the same way
+    DateFormat dateFormat = DEFAULT_DATE_FORMATS.get(0);
+    return !dateFormat.getTimeZone().getID().equals(TimeZone.getDefault().getID()) || dateFormat.isLenient() != lenientParsing;
+  }
 
   /**
    * Used in String based Date assertions - like {@link #isAfter(String)} - to convert input date represented as string
@@ -3392,13 +3408,10 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
    *
    * To revert to default strict date parsing, call {@code setLenientDateParsing(false)}.
    *
-   * @param value whether lenient parsing mode should be enabled or not
+   * @param lenientDateParsing whether lenient parsing mode should be enabled or not
    */
-  public static void setLenientDateParsing(boolean value) {
-    ConfigurationProvider.loadRegisteredConfiguration();
-    for (DateFormat defaultDateFormat : DEFAULT_DATE_FORMATS) {
-      defaultDateFormat.setLenient(value);
-    }
+  public static void setLenientDateParsing(boolean lenientDateParsing) {
+    lenientParsing = lenientDateParsing;
   }
 
   /**
@@ -3564,7 +3577,7 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
   }
 
   /**
-   * Thread safe utility method to parse a Date with {@link #userDateFormats} first, then {@link #DEFAULT_DATE_FORMATS}.
+   * Thread safe utility method to parse a Date with {@link #userDateFormats} first, then {@link #defaultDateFormats()}.
    * <p>
    * Returns <code>null</code> if dateAsString parameter is <code>null</code>.
    *
@@ -3587,15 +3600,13 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
                                            info.representation().toStringOf(dateFormatsInOrderOfUsage())));
   }
 
-  private Date parseDateWithDefaultDateFormats(final String dateAsString) {
-    synchronized (DEFAULT_DATE_FORMATS) {
-      return parseDateWith(dateAsString, DEFAULT_DATE_FORMATS);
-    }
+  private synchronized Date parseDateWithDefaultDateFormats(final String dateAsString) {
+    return parseDateWith(dateAsString, defaultDateFormats());
   }
 
   private List<DateFormat> dateFormatsInOrderOfUsage() {
     List<DateFormat> allDateFormatsInOrderOfUsage = newArrayList(userDateFormats.get());
-    allDateFormatsInOrderOfUsage.addAll(DEFAULT_DATE_FORMATS);
+    allDateFormatsInOrderOfUsage.addAll(defaultDateFormats());
     return allDateFormatsInOrderOfUsage;
   }
 

assertj-core/src/main/java/org/assertj/core/util/DateUtil.java
@@ -52,7 +52,7 @@ public class DateUtil {
    * @return a {@code yyyy-MM-dd} {@link DateFormat}
    */
   public static DateFormat newIsoDateFormat() {
-    return strictDateFormatForPattern("yyyy-MM-dd");
+    return newIsoDateFormat(false);
   }
 
   /**
@@ -61,7 +61,7 @@ public class DateUtil {
    * @return a {@code yyyy-MM-dd'T'HH:mm:ssX} {@link DateFormat}
    */
   public static DateFormat newIsoDateTimeWithIsoTimeZoneFormat() {
-    return strictDateFormatForPattern("yyyy-MM-dd'T'HH:mm:ssX");
+    return newIsoDateTimeWithIsoTimeZoneFormat(false);
   }
 
   /**
@@ -69,7 +69,7 @@ public class DateUtil {
    * @return a {@code yyyy-MM-dd'T'HH:mm:ss} {@link DateFormat}
    */
   public static DateFormat newIsoDateTimeFormat() {
-    return strictDateFormatForPattern("yyyy-MM-dd'T'HH:mm:ss");
+    return newIsoDateTimeFormat(false);
   }
 
   /**
@@ -78,7 +78,7 @@ public class DateUtil {
    * @return a {@code yyyy-MM-dd'T'HH:mm:ss.SSS} {@link DateFormat}
    */
   public static DateFormat newIsoDateTimeWithMsFormat() {
-    return strictDateFormatForPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+    return newIsoDateTimeWithMsFormat(false);
   }
 
   /**
@@ -87,7 +87,7 @@ public class DateUtil {
    * @return a {@code yyyy-MM-dd'T'HH:mm:ss.SSSX} {@link DateFormat}
    */
   public static DateFormat newIsoDateTimeWithMsAndIsoTimeZoneFormat() {
-    return strictDateFormatForPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+    return newIsoDateTimeWithMsAndIsoTimeZoneFormat(false);
   }
 
   /**
@@ -96,12 +96,70 @@ public class DateUtil {
    * @return a {@code yyyy-MM-dd HH:mm:ss.SSS} {@link DateFormat}
    */
   public static DateFormat newTimestampDateFormat() {
-    return strictDateFormatForPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    return newTimestampDateFormat(false);
   }
 
-  private static DateFormat strictDateFormatForPattern(String pattern) {
+  /**
+   * ISO 8601 date format (yyyy-MM-dd), example : <code>2003-04-23</code>
+   * @param lenientParsing whether or not parsing the date is lenient
+   * @return a {@code yyyy-MM-dd} {@link DateFormat}
+   */
+  public static DateFormat newIsoDateFormat(boolean lenientParsing) {
+    return dateFormatForPattern("yyyy-MM-dd", lenientParsing);
+  }
+
+  /**
+   * ISO 8601 date-time format with ISO time zone (yyyy-MM-dd'T'HH:mm:ssX), example :
+   * <code>2003-04-26T03:01:02+00:00</code>
+   * @param lenientParsing whether or not parsing the date is lenient
+   * @return a {@code yyyy-MM-dd'T'HH:mm:ssX} {@link DateFormat}
+   */
+  public static DateFormat newIsoDateTimeWithIsoTimeZoneFormat(boolean lenientParsing) {
+    return dateFormatForPattern("yyyy-MM-dd'T'HH:mm:ssX", lenientParsing);
+  }
+
+  /**
+   * ISO 8601 date-time format (yyyy-MM-dd'T'HH:mm:ss), example : <code>2003-04-26T13:01:02</code>
+   * @param lenientParsing whether or not parsing the date is lenient
+   * @return a {@code yyyy-MM-dd'T'HH:mm:ss} {@link DateFormat}
+   */
+  public static DateFormat newIsoDateTimeFormat(boolean lenientParsing) {
+    return dateFormatForPattern("yyyy-MM-dd'T'HH:mm:ss", lenientParsing);
+  }
+
+  /**
+   * ISO 8601 date-time format with millisecond (yyyy-MM-dd'T'HH:mm:ss.SSS), example :
+   * <code>2003-04-26T03:01:02.999</code>
+   * @param lenientParsing whether or not parsing the date is lenient
+   * @return a {@code yyyy-MM-dd'T'HH:mm:ss.SSS} {@link DateFormat}
+   */
+  public static DateFormat newIsoDateTimeWithMsFormat(boolean lenientParsing) {
+    return dateFormatForPattern("yyyy-MM-dd'T'HH:mm:ss.SSS", lenientParsing);
+  }
+
+  /**
+   * ISO 8601 date-time format with millisecond and ISO time zone (yyyy-MM-dd'T'HH:mm:ss.SSSX), example :
+   * <code>2003-04-26T03:01:02.758+00:00</code>
+   * @param lenientParsing whether or not parsing the date is lenient
+   * @return a {@code yyyy-MM-dd'T'HH:mm:ss.SSSX} {@link DateFormat}
+   */
+  public static DateFormat newIsoDateTimeWithMsAndIsoTimeZoneFormat(boolean lenientParsing) {
+    return dateFormatForPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX", lenientParsing);
+  }
+
+  /**
+   * {@link java.sql.Timestamp} date-time format with millisecond (yyyy-MM-dd HH:mm:ss.SSS), example :
+   * <code>2003-04-26 03:01:02.999</code>
+   * @param lenientParsing whether or not parsing the date is lenient
+   * @return a {@code yyyy-MM-dd HH:mm:ss.SSS} {@link DateFormat}
+   */
+  public static DateFormat newTimestampDateFormat(boolean lenientParsing) {
+    return dateFormatForPattern("yyyy-MM-dd HH:mm:ss.SSS", lenientParsing);
+  }
+
+  private static DateFormat dateFormatForPattern(String pattern, boolean lenient) {
     DateFormat dateFormat = new SimpleDateFormat(pattern);
-    dateFormat.setLenient(false);
+    dateFormat.setLenient(lenient);
     return dateFormat;
   }
 

assertj-core/src/test/java/org/assertj/core/api/EntryPointAssertions_setLenientDateParsing_Test.java
@@ -38,7 +38,7 @@ class EntryPointAssertions_setLenientDateParsing_Test extends EntryPointAssertio
     // WHEN
     setLenientDateParsingFunction.accept(true);
     // THEN
-    then(AbstractDateAssert.DEFAULT_DATE_FORMATS).allMatch(DateFormat::isLenient);
+    then(AbstractDateAssert.defaultDateFormats()).allMatch(DateFormat::isLenient);
   }
 
   private static Stream<Consumer<Boolean>> setLenientDateParsingFunctions() {

assertj-core/src/test/java/org/assertj/core/api/date/DateAssert_with_string_based_date_representation_Test.java
@@ -24,17 +24,20 @@ import static org.assertj.core.util.DateUtil.parseDatetimeWithMs;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.Date;
+import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 import org.assertj.core.api.DateAssertBaseTest;
 import org.assertj.core.util.DateUtil;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -44,10 +47,20 @@ import org.junit.jupiter.api.Test;
  */
 class DateAssert_with_string_based_date_representation_Test extends DateAssertBaseTest {
 
+  private TimeZone defaultTimeZone;
+
+  @Override
+  @BeforeEach
+  public void setUp() {
+    super.setUp();
+    defaultTimeZone = TimeZone.getDefault();
+  }
+
   @Override
   @AfterEach
   public void tearDown() {
     useDefaultDateFormatsOnly();
+    TimeZone.setDefault(defaultTimeZone);
   }
 
   @Test
@@ -256,4 +269,15 @@ class DateAssert_with_string_based_date_representation_Test extends DateAssertBa
     assertThat(date).isEqualTo("2003 04 26");
   }
 
+  @Test
+  void default_date_formats_should_support_default_timezone_change() {
+    // GIVEN
+    TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+    // need to call a date assertion to initialize the default date formats before changing the timezone.
+    assertThat(Date.from(Instant.parse("2024-03-01T00:00:00.000+01:00"))).as("In CET time zone").isEqualTo("2024-03-01");
+    // WHEN
+    TimeZone.setDefault(TimeZone.getTimeZone("WET"));
+    // THEN
+    then(Date.from(Instant.parse("2024-03-01T00:00:00.000+00:00"))).as("In WET time zone").isEqualTo("2024-03-01");
+  }
 }

commit 7461b681568b762c8ee8052f2a5558a5bfc99691

Fix thread-safety in `AbstractDateAssert` (#3874)
97b642a0 introduced a regression that broke `AbstractDateAssert`
thread-safety by replacing synchronization on `DEFAULT_DATE_FORMATS`
with synchronization on the method that parses with the default
formats. Such a synchronization cannot work properly with multiple
instances of `AbstractDateAssert` because `DEFAULT_DATE_FORMATS` is
static.

This change fixes the regression by replacing the synchronization on
the method level with a `ThreadLocal` that guards access to the current
`DEFAULT_DATE_FORMATS` content.

assertj-core/src/main/java/org/assertj/core/api/AbstractDateAssert.java
@@ -75,28 +75,29 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
   private static final String DATE_FORMAT_PATTERN_SHOULD_NOT_BE_NULL = "Given date format pattern should not be null";
   private static final String DATE_FORMAT_SHOULD_NOT_BE_NULL = "Given date format should not be null";
 
+  private static boolean lenientParsing = Configuration.LENIENT_DATE_PARSING;
+
   /**
    * the default DateFormat used to parse any String date representation.
    */
-  private static List<DateFormat> DEFAULT_DATE_FORMATS = defaultDateFormats();
-  private static boolean lenientParsing = Configuration.LENIENT_DATE_PARSING;
+  private static final ThreadLocal<List<DateFormat>> DEFAULT_DATE_FORMATS = ThreadLocal.withInitial(() -> list(newIsoDateTimeWithMsAndIsoTimeZoneFormat(lenientParsing),
+                                                                                                               newIsoDateTimeWithMsFormat(lenientParsing),
+                                                                                                               newTimestampDateFormat(lenientParsing),
+                                                                                                               newIsoDateTimeWithIsoTimeZoneFormat(lenientParsing),
+                                                                                                               newIsoDateTimeFormat(lenientParsing),
+                                                                                                               newIsoDateFormat(lenientParsing)));
 
   @VisibleForTesting
   static List<DateFormat> defaultDateFormats() {
-    if (DEFAULT_DATE_FORMATS == null || defaultDateFormatMustBeRecreated()) {
-      DEFAULT_DATE_FORMATS = list(newIsoDateTimeWithMsAndIsoTimeZoneFormat(lenientParsing),
-                                  newIsoDateTimeWithMsFormat(lenientParsing),
-                                  newTimestampDateFormat(lenientParsing),
-                                  newIsoDateTimeWithIsoTimeZoneFormat(lenientParsing),
-                                  newIsoDateTimeFormat(lenientParsing),
-                                  newIsoDateFormat(lenientParsing));
+    if (defaultDateFormatMustBeRecreated()) {
+      DEFAULT_DATE_FORMATS.remove();
     }
-    return DEFAULT_DATE_FORMATS;
+    return DEFAULT_DATE_FORMATS.get();
   }
 
   private static boolean defaultDateFormatMustBeRecreated() {
     // check default timezone or lenient flag changes, only check one date format since all are configured the same way
-    DateFormat dateFormat = DEFAULT_DATE_FORMATS.get(0);
+    DateFormat dateFormat = DEFAULT_DATE_FORMATS.get().get(0);
     return !dateFormat.getTimeZone().getID().equals(TimeZone.getDefault().getID()) || dateFormat.isLenient() != lenientParsing;
   }
 
@@ -3600,7 +3601,7 @@ public abstract class AbstractDateAssert<SELF extends AbstractDateAssert<SELF>> 
                                            info.representation().toStringOf(dateFormatsInOrderOfUsage())));
   }
 
-  private synchronized Date parseDateWithDefaultDateFormats(final String dateAsString) {
+  private Date parseDateWithDefaultDateFormats(final String dateAsString) {
     return parseDateWith(dateAsString, defaultDateFormats());
   }
 

assertj-core/src/test/java/org/assertj/core/api/date/DateAssert_isEqualTo_Test.java
@@ -12,7 +12,8 @@
  */
 package org.assertj.core.api.date;
 
-import static java.time.Instant.parse;
+import static java.time.ZoneId.systemDefault;
+import static java.util.stream.IntStream.range;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.assertj.core.util.AssertionsUtil.expectAssertionError;
@@ -21,9 +22,11 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Date;
-
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -32,6 +35,7 @@ import org.opentest4j.AssertionFailedError;
 
 /**
  * @author Joel Costigliola
+ * @author Niklas Keller
  */
 class DateAssert_isEqualTo_Test {
 
@@ -46,13 +50,11 @@ class DateAssert_isEqualTo_Test {
       assertThat(actual).isEqualTo(expected);
     }
 
-    Arguments[] should_pass() {
-      return new Arguments[] {
-          arguments(Date.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    Date.from(parse("1970-01-01T00:00:00.000000001Z"))),
-          arguments(Date.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    Timestamp.from(parse("1970-01-01T00:00:00.000000001Z")))
-      };
+    Stream<Arguments> should_pass() {
+      return Stream.of(arguments(Date.from(Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                                 Date.from(Instant.parse("1970-01-01T00:00:00.000000001Z"))),
+                       arguments(Date.from(Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                                 Timestamp.from(Instant.parse("1970-01-01T00:00:00.000000001Z"))));
     }
 
     @ParameterizedTest
@@ -64,11 +66,9 @@ class DateAssert_isEqualTo_Test {
       then(error).isInstanceOf(AssertionFailedError.class);
     }
 
-    Arguments[] should_fail() {
-      return new Arguments[] {
-          arguments(Timestamp.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    Date.from(parse("1970-01-01T00:00:00.000000001Z")))
-      };
+    Stream<Arguments> should_fail() {
+      return Stream.of(arguments(Timestamp.from(Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                                 Date.from(Instant.parse("1970-01-01T00:00:00.000000001Z"))));
     }
 
   }
@@ -84,13 +84,11 @@ class DateAssert_isEqualTo_Test {
       assertThat(actual).isEqualTo(expected);
     }
 
-    Arguments[] should_pass() {
-      return new Arguments[] {
-          arguments(Date.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    parse("1970-01-01T00:00:00.000000001Z")),
-          arguments(Timestamp.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    parse("1970-01-01T00:00:00.000000001Z"))
-      };
+    Stream<Arguments> should_pass() {
+      return Stream.of(arguments(Date.from(Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                                 Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                       arguments(Timestamp.from(Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                                 Instant.parse("1970-01-01T00:00:00.000000001Z")));
     }
 
   }
@@ -106,11 +104,8 @@ class DateAssert_isEqualTo_Test {
       assertThat(actual).isEqualTo(expected);
     }
 
-    Arguments[] should_pass() {
-      return new Arguments[] {
-          arguments(Date.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    "1970-01-01T00:00:00.000Z")
-      };
+    Stream<Arguments> should_pass() {
+      return Stream.of(arguments(Date.from(Instant.parse("1970-01-01T00:00:00.000000001Z")), "1970-01-01T00:00:00.000Z"));
     }
 
     @ParameterizedTest
@@ -122,13 +117,20 @@ class DateAssert_isEqualTo_Test {
       then(error).isInstanceOf(AssertionFailedError.class);
     }
 
-    Arguments[] should_fail() {
-      return new Arguments[] {
-          arguments(Date.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    "1970-01-01T00:00:00.000000001Z"),
-          arguments(Timestamp.from(parse("1970-01-01T00:00:00.000000001Z")),
-                    "1970-01-01T00:00:00.000000001Z")
-      };
+    Stream<Arguments> should_fail() {
+      return Stream.of(arguments(Date.from(Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                                 "1970-01-01T00:00:00.000000001Z"),
+                       arguments(Timestamp.from(Instant.parse("1970-01-01T00:00:00.000000001Z")),
+                                 "1970-01-01T00:00:00.000000001Z"));
+    }
+
+    // https://github.com/assertj/assertj/issues/3873
+    @Test
+    void should_pass_concurrently() {
+      // GIVEN
+      Date actual = Date.from(LocalDate.parse("1970-01-01").atStartOfDay(systemDefault()).toInstant());
+      // WHEN/THEN
+      range(0, 1000).parallel().forEach(i -> assertThat(actual).isEqualTo("1970-01-01"));
     }
 
   }