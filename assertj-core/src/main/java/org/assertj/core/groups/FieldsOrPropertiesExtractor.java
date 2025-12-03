7013542cd8e63bb4c0e76fc1aa8d8aa44f6e16a6
chanwon <clw8679@naver.com>
2025-12-03 11:12:35 +0900

    feat: honor maxStackTraceElementsDisplayed in soft assertions (fix #4040)

--- assertj-core/src/main/java/org/assertj/core/groups/FieldsOrPropertiesExtractor.java
+++ assertj-core/src/main/java/org/assertj/core/groups/FieldsOrPropertiesExtractor.java
@@ -15,7 +15,6 @@
  */
 package org.assertj.core.groups;
 
-import static java.util.stream.Collectors.toList;
 import static org.assertj.core.util.IterableUtil.toArray;
 import static org.assertj.core.util.Lists.newArrayList;
 import static org.assertj.core.util.Streams.stream;
@@ -64,7 +63,7 @@ public class FieldsOrPropertiesExtractor {
    */
   public static <F, T> List<T> extract(Iterable<? extends F> objects, Function<? super F, T> extractor) {
     checkObjectToExtractFromIsNotNull(objects);
-    return stream(objects).map(extractor).collect(toList());
+    return stream(objects).map(extractor).toList();
   }
 
   private static void checkObjectToExtractFromIsNotNull(Object object) {
---
+++ assertj-core/src/test/java/org/assertj/core/util/Throwables_describeErrors_Test.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.assertj.core.util;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.InstanceOfAssertFactories.STRING;
+import static org.assertj.core.util.Throwables.describeErrors;
+
+import java.util.List;
+
+import org.assertj.core.presentation.StandardRepresentation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link Throwables#describeErrors(List)}.
+ *
+ * @author chanwonlee
+ */
+class Throwables_describeErrors_Test {
+
+  private static final int DEFAULT_MAX_STACK_TRACE_ELEMENTS = StandardRepresentation.getMaxStackTraceElementsDisplayed();
+
+  @AfterEach
+  void afterEach() {
+    // restore default value
+    StandardRepresentation.setMaxStackTraceElementsDisplayed(DEFAULT_MAX_STACK_TRACE_ELEMENTS);
+  }
+
+  @Test
+  void should_honor_maxStackTraceElementsDisplayed_setting() {
+    // GIVEN
+    StandardRepresentation.setMaxStackTraceElementsDisplayed(10);
+    var cause = new RuntimeException("cause message");
+    var error = new RuntimeException("error message", cause);
+    // WHEN
+    List<String> descriptions = describeErrors(List.of(error));
+    // THEN
+    then(descriptions).singleElement(STRING)
+                      .contains("cause first 10 stack trace elements:");
+    long stackTraceElementCount = descriptions.get(0).lines()
+                                              .filter(line -> line.trim().startsWith("at "))
+                                              .count();
+    then(stackTraceElementCount).isEqualTo(StandardRepresentation.getMaxStackTraceElementsDisplayed());
+  }
+
+  @Test
+  void should_return_message_when_error_has_no_cause() {
+    // GIVEN
+    var error = new RuntimeException("error message without cause");
+    // WHEN
+    List<String> descriptions = describeErrors(List.of(error));
+    // THEN
+    then(descriptions).containsExactly("error message without cause");
+  }
+}
