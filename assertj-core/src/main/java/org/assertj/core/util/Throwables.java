067306d4ab8097cda902e1edd3e984d15853a969
Joel Costigliola <382613+joel-costigliola@users.noreply.github.com>
2026-04-06 15:40:31 +0200

    remove isProxiedAssertionClass as we don't proxy classes anymore since we removed byte buddy usage

--- assertj-core/src/main/java/org/assertj/core/util/Throwables.java
+++ assertj-core/src/main/java/org/assertj/core/util/Throwables.java
@@ -260,8 +260,7 @@ public final class Throwables {
   public static StackTraceElement getFirstStackTraceElementFromTest(StackTraceElement[] stacktrace) {
     for (StackTraceElement element : stacktrace) {
       String className = element.getClassName();
-      if (isProxiedAssertionClass(className)
-          || className.startsWith("sun.reflect")
+      if (className.startsWith("sun.reflect")
           || className.startsWith("jdk.internal.reflect")
           || className.startsWith("java.")
           || className.startsWith("javax.")
@@ -279,10 +278,6 @@ public final class Throwables {
       return element;
     }
     return null;
-  }
-
-  private static boolean isProxiedAssertionClass(String className) {
-    return className.contains("$ByteBuddy$");
   }
 
   private static <T extends Throwable> T addLineNumberToErrorMessage(T error) {
